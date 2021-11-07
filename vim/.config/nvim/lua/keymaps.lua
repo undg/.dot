7150be3d32a1a3893369816eabe93a3f384f9047
und3rdg <und3rdg@gmail.com>
2021-11-07 15:04:15 +0000

    nvim: lua init scaffolding.

---
+++ vim/.config/nvim/lua/keymaps.lua
@@ -0,0 +1,61 @@
+local map = require('utils.map')
+
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
+
+-- Stop that stupid window from popping up!
+map.normal('q:', ':q')
+-- No fcking way! Nope, I don't want ex mode!
+map.normal('Q', '<Nop>')
+
+-- no need for ESC
+map.insert('jk', '<ESC>')
+map.insert('kj', '<ESC>')
+
+-- ZZ like stuff
+map.normal('<LEADER><LEADER>', ':write<CR>')
+map.normal('<LEADER>q', ':q<CR>')
+
+-- duno... I'm using it a lot.
+map.normal('<LEADER>s', ':mksession!<CR>')
+map.normal('<LEADER>cd', ':lcd %:p:h<CR>', {silent = true})
+
+-- remove white spaces on end line
+map.normal('<LEADER>sp', ':%s/\\s\\+$//ge<CR>:echomsg \"white space cleaing\"<cr>')
+
+-- Toggle list (display unprintable characters).
+map.normal('<F8>', ':set list!<CR>')
+map.insert('<F8>', '<esc>:set list!<CR>i')
+
+-- get git branch go into insert mode.
+map.normal('<leader>gb', ':0r!git rev-parse --abbrev-ref HEAD<CR>A:<SPACE>')
+
+
+-- pop-up selection (autocomplete)
+map.insert('<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+map.insert('<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<S-Tab>"', {expr = true})
+
+-- repeat last macro
+map.normal('<CR>', '@@')
+
+-- Yanking/Pasting
+map.normal('<leader>p', '"+p')
+map.visual('<leader>y', '"+y')
+-- Yank to end of line
+map.normal('Y', 'y$')
+-- Only visual, keep same yank in register
+map.visual('<LEADER>p', '"_dP')
+
+-- Keep in center
+map.normal('n', 'nzzzv')
+map.normal('N', 'Nzzzv')
+map.normal('J', 'mzJ`z')
+map.normal('<C-n>', ':cnext<CR>zzzv')
+map.normal('<C-p>', ':cprev<CR>zzzv')
+
+-- Extra break points
+map.insert(',', ',<C-g>u')
+map.insert('.', '.<C-g>u')
+map.insert('!', '!<C-g>u')
+map.insert('?', '?<C-g>u')
+
---
+++ vim/.config/nvim/lua/settings.lua
@@ -0,0 +1,49 @@
+vim.opt.autoread = true
+vim.opt.autoindent = true
+vim.opt.timeout = false
+vim.opt.ttimeout = true
+vim.opt.mouse = 'n'
+vim.opt.history = 900
+
+-- do not wrap long lines, I'll wrap me myself or prettier will do it.
+vim.opt.textwidth = 0
+-- @example ...and if they need to be long, just display them with line break.  ...and if they need to be long, just display them with line break.  ...and if they need to be long, just display them with line break.  ...and if they need to be long, just display them with line break.  ...and if they need to be long, just display them with line break.  ...and if they need to be long, just display them with line break.
+vim.opt.linebreak = true
+vim.opt.breakindent = true
+vim.opt.breakindentopt = 'min:60,shift:0,sbr'
+vim.opt.showbreak = '+++ '
+
+-- Don't redraw while executing macros (good performance config)
+vim.opt.lazyredraw = true
+
+-- For regular expressions turn magic on
+vim.opt.magic = true
+
+
+-- No annoying sound on errors
+vim.opt.errorbells = false
+vim.opt.visualbell = false
+-- vim.opt.t_vb = ''
+vim.opt.tm = 500
+
+vim.opt.encoding = 'utf8'
+
+-- Use Unix as the standard file type
+vim.opt.ffs = 'unix,dos,mac'
+
+-- Turn backup off, since most stuff is in SVN, git et.c anyway...
+vim.opt.backup = false
+vim.opt.wb = false
+vim.opt.swapfile = false
+
+-- vim.opt.to auto read when a file is changed from the outside
+vim.opt.autoread = true
+
+vim.opt.list = false
+vim.opt.listchars = 'eol:¬,tab:>·,trail:~,extends:>,precedes:<,space:·'
+
+vim.opt.path = vim.opt.path + '**'
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'longest:full,full'
+vim.opt.wildoptions = 'pum,tagfile'
+
