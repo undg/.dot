c93470c5d3fc8e129ebf663a824d4b0ea0223d7d
und3rdg <und3rdg@gmail.com>
2021-11-08 11:36:20 +0000

    nvim-lua: fzf, theme, nerdtree, QFEnter

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,8 +1,13 @@
--- source a vimscript file
+-- source a Vimscript file
 -- vim.cmd('source ~/.vim/init.vim')
-require('plugins')
 require('settings')
 require('keymaps')
--- require('colors')
+require('colors')
 
-
+require('plugins')
+require('plugins/treesitter')
+require('plugins/vim-gitgutter')
+require('plugins/nerdtree')
+require('plugins/fzf-lua')
+require('plugins/nvim-web-devicons')
+require('plugins/QFEnter')
---
+++ vim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+-- Tree-sitter based folding. (Technically not a module because it's per windows and not per buffer.)
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr='nvim_treesitter#foldexpr()'
+
+require('nvim-treesitter.configs').setup {
+    highlight = {
+        enable = true,
+        -- custom_captures = {
+        --     -- Highlight the @foo.bar capture group with the "Identifier" highlight group.
+        --     ["foo.bar"] = "Identifier",
+        -- },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = true,
+    },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection = "gnn",
+            node_incremental = "grn",
+            scope_incremental = "grc",
+            node_decremental = "grm",
+        },
+    },
+    indent = {
+        enable = true
+    },
+}
