fa68a2f898c59a3d9560ae34099ffb05a96e2364
und3rdg <und3rdg@gmail.com>
2021-11-14 19:16:18 +0000

    nvim-dot-29: fix tsserver by using defaults, lol.

--- vim/.config/nvim/lua/lsp/tsserver.lua
+++ vim/.config/nvim/lua/lsp/tsserver.lua
@@ -1,9 +1,16 @@
   return {
-    root_dir = function(fname)
-      return nvim_lsp.util.root_pattern("tsconfig.json")(fname) or
-        nvim_lsp.util.root_pattern("package.json", "jsconfig.json", ".git")(
-          fname
-        ) or
-        vim.fn.getcwd()
-    end
-  }
+  -- cmd = { "typescript-language-server", "--stdio" },
+  -- filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
+  -- init_options = {
+  --   hostInfo = "neovim"
+  -- },
+  -- root_dir = root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git")
+  -- OR
+  -- root_dir = function(fname)
+  --   return nvim_lsp.util.root_pattern("tsconfig.json")(fname) or
+  --     nvim_lsp.util.root_pattern("package.json", "jsconfig.json", ".git")(
+  --       fname
+  --     ) or
+  --     vim.fn.getcwd()
+  -- end
+}
