b65499f1fd56b83626394179dbeed79cee0045bd
und3rdg <und3rdg@gmail.com>
2021-11-14 19:16:18 +0000

    nvim-dot-29: fix tsserver by using defaults, lol.

--- vim/.config/nvim/lua/plugins/lsp-installer.lua
+++ vim/.config/nvim/lua/plugins/lsp-installer.lua
@@ -1,11 +1,13 @@
 local lsp_installer = require('nvim-lsp-installer')
-local lsp_opt = {}
 
-lsp_opt.tsserver = require('lsp/tsserver')
-lsp_opt.sumneko_lua = require('lsp/sumneko_lua')
-lsp_opt.yamlls = require('lsp/yamlls')
+-- Table with individual language servers settings.
+local lsp_opt = {}
+lsp_opt.denols = require('lsp/denols')
+lsp_opt.tailwindcss = require('lsp/tailwindcss')
 lsp_opt.jsonls = require('lsp/jsonls')
 
+lsp_opt.sumneko_lua = require('lsp/sumneko_lua')
+lsp_opt.yamlls = require('lsp/yamlls')
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
