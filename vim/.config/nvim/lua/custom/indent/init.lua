79e22d331dd0e68ff191014420ca7b002e076f29
und3rdg <und3rdg@gmail.com>
2021-12-17 03:53:46 +0000

    nvim: goto with telescope and random cleaning.

--- vim/.config/nvim/lua/custom/indent/init.lua
+++ vim/.config/nvim/lua/custom/indent/init.lua
@@ -1,1 +1,1 @@
-vim.cmd [[command ToggleIndent :lua R("custom.indent.indent").toggle()<cr>]]
+vim.cmd [[command! ToggleIndent :lua R("custom.indent.indent").toggle()<cr>]]
--- vim/.config/nvim/lua/custom/json2ts.lua
+++ vim/.config/nvim/lua/custom/json2ts.lua
@@ -1,3 +1,3 @@
 -- @TODO auto install dependencies (`npm i -g json-ts`)
-vim.cmd [[command Json2ts :r!xclip -o | json-ts --stdin<cr>]]
+vim.cmd [[command! Json2ts :r!xclip -o | json-ts --stdin<cr>]]
 
