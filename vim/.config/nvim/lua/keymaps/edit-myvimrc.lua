37cd192264f592ab4ba443036a98e3d4902a46c3
und3rdg <und3rdg@gmail.com>
2022-02-09 13:11:29 +0000

    nvim: add open fzf to edit myvimrc jumper keybinding.

--- vim/.config/nvim/lua/keymaps/edit-myvimrc.lua
+++ vim/.config/nvim/lua/keymaps/edit-myvimrc.lua
@@ -1,6 +1,6 @@
 local map = require'../utils/map'
 
 -- Open vimrc with ',ev'
-map.normal('<leader>ve', '<CMD>tabnew ' .. vim.env.MYVIMRC .. ' | lcd %:p:h<CR>')
+map.normal('<leader>ve', '<CMD>tabnew ' .. vim.env.MYVIMRC .. ' | lcd %:p:h | Telescope find_files hidden=true no_ignore=true<CR> ')
 map.normal('<leader>vc', '<CMD>source %<CR>:echom "SOURCE current file"<CR>')
 
