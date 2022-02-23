a91433b76f270237a6f073971946f3328c18eba8
und3rdg <und3rdg@gmail.com>
2022-02-23 17:46:59 +0000

    nvim: fix config, move it to plugins and remove install script as it is installed from YAY package (nvim-packer-git).

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,18 +1,12 @@
 require('utils.globals')
 
 require('config')
-require('theme')
 
 require('plugins')
-require('plugins._packages')
-require('plugins.treesitter')
-require('plugins.vim-gitgutter')
-require('plugins.nerdtree') -- @TODO ♻ port ♻
-require('plugins.nvim-web-devicons')
-require('plugins.QFEnter')
-require('plugins.lsp-installer')
-require('plugins.nvim-cmp')
-require('plugins.vim-yoink')
+-- require('plugins.vim-gitgutter')
+-- require('plugins.nerdtree') -- @TODO ♻ port ♻
+-- require('plugins.QFEnter')
+-- require('plugins.vim-yoink')
 
 require('custom.statusline')
 require('custom.tabline')
