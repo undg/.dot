318752231a1b26664cac029a54ad7fd3e3b91718
und3rdg <und3rdg@gmail.com>
2022-02-23 18:43:44 +0000

    nvim: remove packer_compiled from repo (.gitignore) and remove some ded code

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,12 +1,13 @@
 require('utils.globals')
+require('theme')
 
-require('config')
 
 require('plugins')
--- require('plugins.vim-gitgutter')
--- require('plugins.nerdtree') -- @TODO ♻ port ♻
--- require('plugins.QFEnter')
--- require('plugins.vim-yoink')
+require('config')
+require('plugins.vim-gitgutter')
+require('plugins.nerdtree')
+require('plugins.QFEnter')
+require('plugins.vim-yoink')
 
 require('custom.statusline')
 require('custom.tabline')
@@ -16,7 +17,6 @@ require('custom.indent')
 require('custom.json2ts')
 require('custom.reload-config')
 require('custom.update')
--- require('custom.auto-highlight-toggle') -- @TODO ♻ port ♻
 
 require('keymaps')
 require('keymaps.lsp')
