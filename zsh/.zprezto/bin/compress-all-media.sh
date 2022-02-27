de5ff73dc1741db1e9f120cfebd571c045978b0a
und3rdg <und3rdg@gmail.com>
2022-02-27 03:40:28 +0000

    zsh: new bash script for compress-all-media in current dir.

---
+++ zsh/.zprezto/bin/compress-all-media.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+echo -e "All files will be renamed and overrides with ffmpeg.\n CTRL+C to break.\n Press ANY key to continue."
+read
+
+
+R='\033[0;31m'   #'0;31' is Red's ANSI color code
+G='\033[0;32m'   #'0;32' is Green's ANSI color code
+Y='\033[1;32m'   #'1;32' is Yellow's ANSI color code
+B='\033[0;34m'   #'0;34' is Blue's ANSI color code
+NC='\033[0m'     #'0'    is back no color
+
+for f in *
+do
+    f_date=$(date -r "$f" +"%Y-%m-%d_%H-%M-%S")
+    f_ext=${f##*.}
+    name="$f_date.$f_ext"
+
+    echo ""
+    echo -e "${B}~~~~~~>${G} $f ${B}===>${G} $name ${NC}" 
+    echo ""
+    ffmpeg -i $f $name && trash $f
+done
+
