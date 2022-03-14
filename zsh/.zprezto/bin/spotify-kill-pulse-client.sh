88fbb8eaac318925ae5c15b694245f82c9d3b35a
und3rdg <und3rdg@gmail.com>
2022-03-14 13:05:15 +0000

    zsh: fix broken spotify fix

--- zsh/.zprezto/bin/spotify-kill-pulse-client.sh
+++ zsh/.zprezto/bin/spotify-kill-pulse-client.sh
@@ -2,36 +2,45 @@
 # source:
 # https://gist.github.com/fatihkaan22/0cad6b53647c5d887c67b22c260568cb
 
-spotifyInstances=()
+# One liner that's works
+pacmd list-clients \
+  | awk '($1 == "index:") {i=$2} ($1 == "application.name" && tolower($0) ~ /spotify/) {print i}' \
+  | head -n-1 \
+  | xargs -n1 pacmd kill-client
 
-while read LINE1; read LINE2; do
-	IFS=':-='
+# # This one is broken in line with unset. EE msg in comment.
+# spotifyInstances=()
 
-	read -ra L1 <<< $LINE1
-	read -ra L2 <<< $LINE2
+# while read LINE1; read LINE2; do
+# 	IFS=':-='
 
-	# trim leading/trailing spaces for error checking
-	INDEX_STR=$(echo "${L1[0]}" | xargs)
-	NAME_STR=$(echo "${L2[0]}" | xargs)
+# 	read -ra L1 <<< $LINE1
+# 	read -ra L2 <<< $LINE2
 
-	if [[ $INDEX_STR != 'index' || $NAME_STR != 'application.name' ]]; then
-		echo 'ERROR'
-		exit
-	fi
+# 	# trim leading/trailing spaces for error checking
+# 	INDEX_STR=$(echo "${L1[0]}" | xargs)
+# 	NAME_STR=$(echo "${L2[0]}" | xargs)
 
-	INDEX=${L1[1]}
-	NAME=${L2[1]}
+# 	if [[ $INDEX_STR != 'index' || $NAME_STR != 'application.name' ]]; then
+# 		echo 'ERROR'
+# 		exit
+# 	fi
 
-	if [[ $NAME == ' "Spotify"' ]]; then
-		spotifyInstances+=( $INDEX )
-	fi
+# 	INDEX=${L1[1]}
+# 	NAME=${L2[1]}
 
-done < <(pacmd list-clients | grep -e 'index:' -e 'application.name')
+# 	if [[ $NAME == ' "Spotify"' ]]; then
+# 		spotifyInstances+=( $INDEX )
+# 	fi
 
-# remove last element (active client)
-unset 'spotifyInstances[${#arr[@]}-1]'
+# done < <(pacmd list-clients | grep -e 'index:' -e 'application.name')
 
-for s in ${spotifyInstances[@]}; do
-	echo "pacmd kill-client $s"
-	pacmd kill-client $s
-done
+# # remove last element (active client)
+# unset 'spotifyInstances[${#arr[@]}-1]'
+# # EE: /home/undg/.zprezto/bin/spotify-kill-pulse-client.sh: line 32: unset: [${#arr[@]}-1]: bad array subscript
+
+# for s in ${spotifyInstances[@]}; do
+# 	echo "pacmd kill-client $s"
+# 	pacmd kill-client $s
+# done
+
