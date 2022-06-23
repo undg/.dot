2a7b1efe18da036272eb7e296b903d7817b50979
undg <und3rdg@gmail.com>
2022-06-23 11:32:09 +0100

    vim: git and telescope quickfix.

--- vim/.config/nvim/lua/plugins/vim-gitgutter.lua
+++ vim/.config/nvim/lua/plugins/vim-gitgutter.lua
@@ -1,10 +1,16 @@
 local map = require('../utils/map')
 
+-- disable default mappings h:gitgutter-mappings
+vim.g.gitgutter_map_keys = 0
+
 -- To change the hunk-jumping maps (defaults shown):
+map.normal('[{', ':GitGutterPrevHunk<CR>zz')
+map.normal(']}', ':GitGutterNextHunk<CR>zz')
+-- Legacy fallback
 map.normal('[g', ':GitGutterPrevHunk<CR>zz')
 map.normal(']g', ':GitGutterNextHunk<CR>zz')
 
--- To change the hunk-staging/undoing/previewing maps (defaults shown):
+-- To change staging/undoing/previewing hunk:
 map.normal('gs', ':GitGutterStageHunk<CR>')
 map.normal('gu', ':GitGutterUndoHunk<CR>')
 map.normal('gp', ':GitGutterPreviewHunk<CR>')
