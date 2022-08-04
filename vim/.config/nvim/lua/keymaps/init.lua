adfbe8858773cdd13a0410099f1a4585fc5fe521
undg <und3rdg@gmail.com>
2022-08-04 01:07:06 +0100

    vim: require missing file with keymaps for telescope

--- vim/.config/nvim/lua/keymaps/init.lua
+++ vim/.config/nvim/lua/keymaps/init.lua
@@ -1,10 +1,12 @@
 require('keymaps.general')
 
-require('keymaps.lsp')
-require('keymaps.spell')
 require('keymaps.edit-myvimrc')
 require('keymaps.harpoon')
-require('keymaps.vim-yoink')
-require('keymaps.trouble')
-require('keymaps.vimux')
+require('keymaps.lsp')
 require('keymaps.reload_config')
+require('keymaps.spell')
+require('keymaps.telescope')
+require('keymaps.trouble')
+require('keymaps.vim-yoink')
+require('keymaps.vimux')
+
