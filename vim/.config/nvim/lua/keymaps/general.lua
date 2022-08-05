fe466b173dafd7c5a01a7cc40d68efae4931c728
undg <und3rdg@gmail.com>
2022-08-05 10:29:36 +0100

    vim: keymap for moving windows around

--- vim/.config/nvim/lua/keymaps/general.lua
+++ vim/.config/nvim/lua/keymaps/general.lua
@@ -87,28 +87,3 @@ map.normal("<leader>/", ":noh<cr>")
 map.visual("v", "<Plug>(expand_region_expand)")
 map.visual("<C-v>", "<Plug>(expand_region_shrink)")
 
--- WINDOWS AND TABS --
-----------------------
-
--- Zoom / Fullscreen
-map.normal("<C-w>z", ":ZoomWinTabToggle<CR>")
-map.normal("<C-w><C-z>", ":ZoomWinTabToggle<CR>")
-
-map.normal("tt", ":tabnew %<CR>")
-map.normal("<C-w>t", ":tabnew %<CR>")
-map.normal("<C-w><C-t>", ":tabnew %<CR>")
-
--- split navigations
-map.normal("<C-h>", "<C-w><C-h>")
-map.normal("<C-j>", "<C-w><C-j>")
-map.normal("<C-k>", "<C-w><C-k>")
-map.normal("<C-l>", "<C-w><C-l>")
-
--- tab change
--- gt <--
-map.normal("<A-h>", "gT")
-map.normal("<A-j>", "gT")
--- gT -->
-map.normal("<A-k>", "gt")
-map.normal("<A-l>", "gt")
-
