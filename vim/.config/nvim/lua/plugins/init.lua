43925f1186b6a14a30e4bd4b7432526ca3aac3a7
undg <und3rdg@gmail.com>
2022-08-09 12:59:37 +0100

    vim: nvim-scrollbar config.

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -180,19 +180,17 @@ packer.startup(function()
     use({
         "petertriho/nvim-scrollbar",
         config = function()
-            require("scrollbar").setup()
-            require("scrollbar.handlers.search").setup()
+            require("plugins.nvim-scrollbar")
         end,
         requires = {
             {
                 "kevinhwang91/nvim-hlslens",
                 config = function()
-                    require('plugins.nvim-hlslens')
+                    require("plugins.nvim-hlslens")
                 end,
             },
         },
     })
-
 
     -- Theme
     use({
@@ -201,6 +199,28 @@ packer.startup(function()
             require("theme")
         end,
     })
+    -- use({
+    --     "ellisonleao/gruvbox.nvim",
+    --     config = function()
+    --         -- setup must be called before loading the colorscheme
+    --         -- Default options:
+    --         require("gruvbox").setup({
+    --             undercurl = true,
+    --             underline = true,
+    --             bold = true,
+    --             italic = true,
+    --             strikethrough = true,
+    --             invert_selection = false,
+    --             invert_signs = false,
+    --             invert_tabline = false,
+    --             invert_intend_guides = false,
+    --             inverse = true, -- invert background for search, diffs, statuslines and errors
+    --             contrast = "", -- can be "hard", "soft" or empty string
+    --             overrides = {},
+    --         })
+    --         vim.cmd("colorscheme gruvbox")
+    --     end,
+    -- })
     use("lukas-reineke/indent-blankline.nvim") -- Add indentation guides even on blank lines
     use({
         "blueyed/vim-diminactive",
