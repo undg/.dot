eb6024623c6d59422d566936e0cc6bcf629ffca9
undg <und3rdg@gmail.com>
2022-08-09 13:13:34 +0100

    vim: remove commented code.

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -199,28 +199,6 @@ packer.startup(function()
             require("theme")
         end,
     })
-    -- use({
-    --     "ellisonleao/gruvbox.nvim",
-    --     config = function()
-    --         -- setup must be called before loading the colorscheme
-    --         -- Default options:
-    --         require("gruvbox").setup({
-    --             undercurl = true,
-    --             underline = true,
-    --             bold = true,
-    --             italic = true,
-    --             strikethrough = true,
-    --             invert_selection = false,
-    --             invert_signs = false,
-    --             invert_tabline = false,
-    --             invert_intend_guides = false,
-    --             inverse = true, -- invert background for search, diffs, statuslines and errors
-    --             contrast = "", -- can be "hard", "soft" or empty string
-    --             overrides = {},
-    --         })
-    --         vim.cmd("colorscheme gruvbox")
-    --     end,
-    -- })
     use("lukas-reineke/indent-blankline.nvim") -- Add indentation guides even on blank lines
     use({
         "blueyed/vim-diminactive",
