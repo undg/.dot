85edd8f6c4173ff4e341c15572d6562b2f76c851
undg <und3rdg@gmail.com>
2022-09-20 12:59:19 +0100

    vim: typescript-nvim keymaps

---
+++ vim/.config/nvim/lua/keymaps/typescript-nvim.lua
@@ -0,0 +1,6 @@
+local map = require('utils.map')
+
+map.normal('<leader>to', ':TypescriptOrganizeImports<cr>')
+map.normal('<leader>tp', ':TypescriptRemoveUnused<cr>')
+map.normal('<leader>ti', ':TypescriptAddMissingImports<cr>')
+
