282042b8660e4ef051ca38289613d98280810a11
undg <und3rdg@gmail.com>
2022-10-06 11:03:56 +0100

    vim: autocmd port to lua

--- vim/.config/nvim/lua/autocmd.lua
+++ vim/.config/nvim/lua/autocmd.lua
@@ -1,1 +1,7 @@
-vim.cmd [[autocmd FileType * set formatoptions-=o]]
+-- vim.cmd([[autocmd FileType * set formatoptions-=o]])
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    callback = function()
+        vim.opt.formatoptions:remove("o")
+    end,
+})
