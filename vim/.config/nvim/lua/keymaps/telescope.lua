5d00144d1c5327914146d0e66bf1279d14b7be9e
undg <und3rdg@gmail.com>
2022-10-07 14:56:13 +0100

    vim: search fucking strings. wip

--- vim/.config/nvim/lua/keymaps/telescope.lua
+++ vim/.config/nvim/lua/keymaps/telescope.lua
@@ -31,3 +31,32 @@ map.normal("<leader>fcb", ":GotoCodeBrowse<cr>")
 map.normal("<leader>fcs", ":GotoCodeGit<cr>")
 map.normal("<leader>fcg", ":GotoCodeGrep<cr>")
 
+function vim.getVisualSelection()
+	vim.cmd('noau normal! "vy"')
+	local text = vim.fn.getreg('v')
+	vim.fn.setreg('v', {})
+
+	text = string.gsub(text, "\n", "")
+	if #text > 0 then
+		return text
+	else
+		return ''
+	end
+end
+
+
+local keymap = vim.keymap.set
+local tb = require('telescope.builtin')
+local opts = { noremap = true, silent = true }
+
+keymap('n', '<space>g', ':Telescope current_buffer_fuzzy_find<cr>', opts)
+keymap('v', '<space>g', function()
+	local text = vim.getVisualSelection()
+	tb.current_buffer_fuzzy_find({ default_text = text })
+end, opts)
+
+keymap('n', '<leader>g', ':Telescope live_grep<cr>', opts)
+keymap('v', '<leader>g', function()
+	local text = vim.getVisualSelection()
+	tb.live_grep({ default_text = text })
+end, opts)
