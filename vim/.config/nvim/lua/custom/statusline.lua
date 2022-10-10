14c7b1263d867dcc853ed6c19d287074ca7f78c8
undg <und3rdg@gmail.com>
2022-10-10 11:51:00 +0100

    vim: statusline update

--- vim/.config/nvim/lua/custom/statusline.lua
+++ vim/.config/nvim/lua/custom/statusline.lua
@@ -11,30 +11,7 @@ vim.cmd[[
     set statusline+=\ %l:%c
     set statusline+=\ %=
     set statusline+=\ %1*\ %.35{getcwd()}\ %*
-    set statusline+=\ %3*\ [%{FugitiveHead()}]\ %*
+    set statusline+=\ %3*\ [%{get(b:,'gitsigns_status','')}]\ %*
     set statusline+=\ %1*\ %f%m\ %*
 ]]
--- " Return current branch name to be used in status line.
--- function! GitFileStatus()
---     return system("[[ -n \"$(git status --porcelain " . shellescape(expand("%")) . " 2>/dev/null )\" ]] && echo -n ✰ ")
--- endfunction
-
--- let s:status = GitFileStatus()
-
--- function! StatuslineGit()
---     let l:branchname = GitBranch()
---     return strlen(l:branchname) > 0?'['.l:branchname.s:status.']':''
--- endfunction
-
--- function! GitBranch()
---     return FugitiveHead()
---     " return FugitiveStatusline()
---     " return system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
--- endfunction
-
-
--- augroup reload_vimrc
---     autocmd!
---     autocmd BufWritePost let s:gitFileStatus = GitFileStatus()
--- augroup END
 
