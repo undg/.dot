8e2f06566a17abcdd56f63020541c7b88a9f6b56
undg <und3rdg@gmail.com>
2022-10-24 22:09:57 +0100

    vim: rearrange require, JSON prettier, re enable keymap override for * and #

---
+++ vim/.config/nvim/lua/custom/init.lua
@@ -0,0 +1,10 @@
+require('custom.statusline')
+require('custom.tabline')
+require('custom.search-selected')
+require('custom.auto-relative-numbers')
+require('custom.indent')
+require('custom.json2ts')
+require('custom.reload-config')
+require('custom.update')
+
+
