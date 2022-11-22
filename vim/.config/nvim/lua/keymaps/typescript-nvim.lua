9e1e9ada933dad9233b0cabe57d51ccd548f8d41
undg <und3rdg@gmail.com>
2022-11-22 10:07:43 +0000

    vim: fix formatter and some TS helpers

--- vim/.config/nvim/lua/keymaps/typescript-nvim.lua
+++ vim/.config/nvim/lua/keymaps/typescript-nvim.lua
@@ -1,6 +1,10 @@
 local map = require('utils.map')
 
-map.normal('<leader>to', ':TypescriptOrganizeImports<cr>')
-map.normal('<leader>tp', ':TypescriptRemoveUnused<cr>')
-map.normal('<leader>ti', ':TypescriptAddMissingImports<cr>')
+map.normal('<leader>to', ':lua require("typescript").actions.organizeImports({sync=true})<cr>')
+map.normal('<leader>tp', ':lua require("typescript").actions.removeUnused({sync = true})<cr>')
+map.normal('<leader>ti', ':lua require("typescript").actions.addMissingImports({sync = true})<cr>')
+
+-- map.normal('<leader>to', ':TypescriptOrganizeImports<cr>')
+-- map.normal('<leader>tp', ':TypescriptRemoveUnused<cr>')
+-- map.normal('<leader>ti', ':TypescriptAddMissingImports<cr>')
 
