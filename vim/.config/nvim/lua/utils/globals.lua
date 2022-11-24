29cf38dcb28ef7ef25870eb1c95c1c2199c7e79d
undg <und3rdg@gmail.com>
2022-11-24 12:10:36 +0000

    vim: add TableConcat helper

--- vim/.config/nvim/lua/utils/globals.lua
+++ vim/.config/nvim/lua/utils/globals.lua
@@ -1,10 +1,25 @@
-R = function (package)
-    require'package'.loaded = nil
-   return require(package)
+R = function(package)
+    require("package").loaded = nil
+    return require(package)
 end
 
-P = function (any)
+P = function(any)
     print(vim.inspect(any))
     return any
 end
 
+---@param t1 {}
+---@param t2 {}
+function TableConcat(t1, t2)
+    local tOut = {}
+
+    for i = 1, #t1 do
+        tOut[i] = t1[i]
+    end
+
+    for i = #t1, #t1 + #t2 do
+        tOut[i] = t2[i]
+    end
+
+    return tOut
+end
