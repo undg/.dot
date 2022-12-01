0ea1b84ae12d04d7da49666cfb69177ff6b7a5fb
undg <und3rdg@gmail.com>
2022-12-01 16:38:24 +0000

    vim: vimux map for deno

--- vim/.config/nvim/lua/keymaps/vimux.lua
+++ vim/.config/nvim/lua/keymaps/vimux.lua
@@ -4,13 +4,10 @@ local map = require'../utils/map'
 map.normal('<Leader>vv', ':call VimuxRunCommand("./" . bufname("%"))<CR>')
 map.normal('<Leader>vt', ':call VimuxRunCommand("tsc " . bufname("%"))<CR>')
 map.normal('<Leader>vn', ':call VimuxRunCommand("node " . bufname("%"))<CR>')
-map.normal('<Leader>vg', ':call VimuxRunCommand("gulp bar")<CR>')
+map.normal('<Leader>vd', ':call VimuxRunCommand("deno run " . bufname("%"))<CR>')
 
 -- Prompt for a command to run
 map.normal('<Leader>vp', ':VimuxPromptCommand<CR>')
-
--- Run last command executed by VimuxRunCommand
-map.normal('<Leader>vl', ':VimuxRunLastCommand<CR>')
 
 -- Inspect runner pane
 map.normal('<Leader>vi', ':VimuxInspectRunner<CR>')
@@ -24,17 +21,9 @@ map.normal('<Leader>vx', ':VimuxInterruptRunner<CR>')
 -- Zoom the runner pane (use <bind-key> z to restore runner pane)
 map.normal('<Leader>vz', ':call VimuxZoomRunner()<CR>')
 
--- RESTART LAST TASK
+-- Run last command executed by VimuxRunCommand
+map.normal('<Leader>vl', ':VimuxRunLastCommand<CR>')
+
+-- !RESTART LAST COMMAND
 map.normal('<Leader>vr', ':call VimuxInterruptRunner() <bar> :call VimuxInterruptRunner() <bar> :call VimuxRunLastCommand() <cr>')
 
-
--- function! VimuxSlime()
---     call VimuxSendText(@v)
---     call VimuxSendKeys("Enter")
--- endfunction
-
--- -- If text is selected, save it in the v buffer and send that buffer it to tmux
--- vmap <Leader>vs "vy :call VimuxSlime()<CR>
-
--- -- Select current paragraph and send it to tmux
--- nmap <Leader>vs vip<LocalLeader>vs<CR>
