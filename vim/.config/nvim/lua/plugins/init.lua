3948f67aeb7946aa0754af42c3280f583f5c9671
undg <und3rdg@gmail.com>
2022-12-24 20:51:11 +0000

    vim: comment with command to install packer.

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -1,3 +1,7 @@
+--[[ 
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+--]]
+
 local packer = require("packer")
 local use = packer.use
 packer.startup(function()
