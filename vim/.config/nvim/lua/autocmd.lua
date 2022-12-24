3b9e92024e63f8f9dd5be83dba217b3fe0105d3c
undg <und3rdg@gmail.com>
2022-12-24 20:45:15 +0000

    vim: auto commands for spelling/tabs

--- vim/.config/nvim/lua/autocmd.lua
+++ vim/.config/nvim/lua/autocmd.lua
@@ -1,7 +1,24 @@
--- vim.cmd([[autocmd FileType * set formatoptions-=o]])
-vim.api.nvim_create_autocmd("FileType", {
-    pattern = "*",
+-- Fix auto comment
+vim.api.nvim_create_autocmd("BufWinEnter", {
     callback = function()
+        -- vim.opt.formatoptions:remove("c")
+        -- vim.opt.formatoptions:remove("r")
         vim.opt.formatoptions:remove("o")
     end,
+})
+
+-- Set wrap and spell in gitcommit and markdown
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = {"gitcommit", "markdown"},
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
+})
+
+-- Highligh on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    callback = function ()
+        vim.highlight.on_yank {higroup = "Visual", timeout = 200}
+    end
 })
