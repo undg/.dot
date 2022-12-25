348c1b71bdf4ae6cc593de7f8c9e0d2b4505a3fd
undg <und3rdg@gmail.com>
2022-12-25 16:24:02 +0000

    vim: move auto commands, fix hide line numbers on Alpha dashboard.

--- vim/.config/nvim/lua/custom/init.lua
+++ vim/.config/nvim/lua/custom/init.lua
@@ -1,7 +1,6 @@
 require('custom.statusline')
 require('custom.tabline')
 require('custom.search-selected')
-require('custom.auto-relative-numbers')
 require('custom.indent')
 require('custom.json2ts')
 require('custom.reload-config')
--- vim/.config/nvim/lua/plugins/alpha.lua
+++ vim/.config/nvim/lua/plugins/alpha.lua
@@ -14,14 +14,15 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-    dashboard.button( "p", "⌨   >  Projects" , ":Telescope project<CR>"),
-    dashboard.button( "r", "   >  Recent"   , ":Telescope oldfiles<CR>"),
-    dashboard.button( "e", "   >  New file" , ":ene <BAR> startinsert <CR>"),
-    dashboard.button( "f", "   >  Find file", ":cd $HOME/ | Telescope find_files<CR>"),
-    dashboard.button( "s", "   >  Settings" , ":e $MYVIMRC | lcd %:p:h | Telescope find_files hidden=true no_ignore=true<CR>"),
-    dashboard.button( "d", "   >  Dot"      , ":cd $HOME/.dot/ | Telescope find_files hidden=true no_ignore=true<CR>"),
-    dashboard.button( "q", "   >  Quit NVIM", ":qa<CR>"),
-    dashboard.button( "<C-c>", "", ":qa<CR>"),
+    dashboard.button( "r", "   >  Recent"     , ":Telescope oldfiles<CR>"),
+    dashboard.button( "c", "   >  Recent here", ":Telescope oldfiles cwd_only=true <CR>"),
+    dashboard.button( "p", "⌨   >  Projects"   , ":Telescope project<CR>"),
+    dashboard.button( "f", "   >  Find file"  , ":Telescope find_files<CR>"),
+    dashboard.button( "s", "   >  Settings"   , ":e $MYVIMRC | lcd %:p:h | Telescope oldfiles cwd_only=true<CR>"),
+    dashboard.button( "d", "   >  Dot"        , ":e $HOME/.dot/README.md | Telescope find_files hidden=true no_ignore=true<CR>"),
+    dashboard.button( "e", "   >  New file"   , ":enew <BAR> startinsert <CR>"),
+    dashboard.button( "q", "   >  Quit NVIM"  , ":qa<CR>"),
+    dashboard.button( "<C-c>", ""              , ":qa<CR>"),
 }
 
 -- Send config to alpha
