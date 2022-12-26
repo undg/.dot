7eac5ff0265c8527e40b6740a004439499b4e7af
undg <und3rdg@gmail.com>
2022-12-26 23:58:42 +0000

    vim: major workflow changes. Moving away from tabs to buffers. Remove many keymaps that will be used for buffers.

--- vim/.config/nvim/lua/keymaps/windows-and-tabs.lua
+++ vim/.config/nvim/lua/keymaps/windows-and-tabs.lua
@@ -2,13 +2,6 @@ local map = require("utils.map")
 
 -- WINDOWS AND TABS --
 ----------------------
---
-map.normal("<A-H>", "<C-w>H")
-map.normal("<A-J>", "<C-w>J")
-map.normal("<A-K>", "<C-w>K")
-map.normal("<A-L>", "<C-w>L")
-
-
 -- Zoom / Fullscreen
 map.normal("<C-w>z", ":ZoomWinTabToggle<CR>")
 map.normal("<C-w><C-z>", ":ZoomWinTabToggle<CR>")
@@ -24,17 +17,16 @@ map.normal("<C-k>", "<C-w><C-k>")
 map.normal("<C-l>", "<C-w><C-l>")
 
 -- resize
-map.normal("<Left>" , ":vertical resize -5<cr>")
-map.normal("<Down>" , ":resize -5<cr>")
-map.normal("<Up>"   , ":resize +5<cr>")
+map.normal("<Up>", ":resize +5<cr>")
+map.normal("<Down>", ":resize -5<cr>")
+
+map.normal("<Left>", ":vertical resize -5<cr>")
 map.normal("<Right>", ":vertical resize +5<cr>")
 
--- tab change
--- gt <--
-map.normal("<A-h>", "gT")
-map.normal("<A-j>", "gT")
--- gT -->
-map.normal("<A-k>", "gt")
-map.normal("<A-l>", "gt")
-
-
+-- -- tab change
+-- -- gt <--
+-- map.normal("<A-h>", "gT")
+-- map.normal("<A-j>", "gT")
+-- -- gT -->
+--map.normal("<A-k>", "gt")
+-- map.normal("<A-l>", "gt")
