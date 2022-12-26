59e2e402ed8d936330c615757b78b44f7d3ca70c
undg <und3rdg@gmail.com>
2022-12-26 03:07:55 +0000

    vim: setup for nvim-tree

--- vim/.config/nvim/lua/keymaps/nvim-tree.lua
+++ vim/.config/nvim/lua/keymaps/nvim-tree.lua
@@ -1,3 +1,8 @@
 local map = require('utils.map')
 
 map.normal('<F2>', ':NvimTreeFindFileToggle<cr>')
+map.normal('<F3>', ':NvimTreeFindFile<cr>')
+map.normal('<F4>', ':NvimTreeClose<cr>')
+
+map.normal('<leader>ff', ':NvimTreeFindFile<cr>')
+map.normal('<leader>fc', ':NvimTreeClose<cr>')
