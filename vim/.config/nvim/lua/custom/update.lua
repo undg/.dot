3ac302d29831583ab644bebb1e53b4449d95b775
undg <und3rdg@gmail.com>
2022-12-27 03:22:16 +0000

    vim: simplify update workflow

--- vim/.config/nvim/lua/custom/update.lua
+++ vim/.config/nvim/lua/custom/update.lua
@@ -1,10 +1,12 @@
 local M = {}
 
 function UpdatePacker()
+    vim.api.nvim_command('source ~/.config/nvim/lua/plugins/init.lua')
+    vim.api.nvim_command('PackerCompile')
     vim.api.nvim_command('PackerSync')
     vim.api.nvim_command('TSUpdate')
     -- vim.cmd [[ :!npm install -g json-ts<cr> ]]
 end
-vim.api.nvim_create_user_command('UpdatePacker', 'lua UpdatePacker()', {})
+vim.api.nvim_create_user_command('Update', 'lua UpdatePacker()', {})
 
 return M
