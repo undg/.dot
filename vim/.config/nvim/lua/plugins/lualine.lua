c01cb299eca65a2a4df3276c7bbecc791b31c430
undg <und3rdg@gmail.com>
2022-12-30 14:11:46 +0000

    vim: styling lualine

--- vim/.config/nvim/lua/plugins/lualine.lua
+++ vim/.config/nvim/lua/plugins/lualine.lua
@@ -28,19 +28,56 @@ local cwd = {
     'filename',
     file_status = false,
     newfile_status = false,
-    color = 'USER1',
+    -- color = 'USER1',
     fmt = function()
         local estimated_space_available = my_window.width() - shorting_target
         return my_path.shorten(my_path.from_home(), '/', estimated_space_available)
     end,
 }
 
+-- suda plugin required
+
+local sudo = false
 local relative_path = {
     'filename',
     file_status = true, -- Displays file status (readonly status, modified status)
     newfile_status = true, -- Display new file status (new file means no write after created)
     path = path_type.relative,
-    color = 'USER1',
+    color = function(section)
+        local bg
+        if sudo then
+            bg = '#551100'
+        else
+            local line
+            if vim.fn.mode() == 'i' then
+                line = 'lualine_a_insert'
+            elseif vim.fn.mode() == 'n' then
+                line = 'lualine_a_normal'
+            else
+                line = 'lualine_a_visual'
+            end
+                bg = line.bg
+        end
+
+        if vim.bo.modified then
+            return { fg = '#89d957', bg = bg }
+        else
+            return { fg = '#fe8019', bg = bg }
+        end
+    end,
+    fmt = function(str)
+        local function starts(String, Start)
+            return string.sub(String, 1, string.len(Start)) == Start
+        end
+
+        if starts(str, 'suda:///') then
+            sudo = true
+        else
+            sudo = false
+        end
+        return str
+    end,
+
     shorting_target = shorting_target, -- Shortens path to leave 40 spaces in the window
     -- for other components. (terrible name, any suggestions?)
     symbols = {
@@ -56,6 +93,13 @@ local branch = {
     color = 'USER3',
     icons_enabled = true,
     separator = { left = '', right = '' },
+    fmt = function(str)
+        if str == '' then
+            return 'noop'
+        else
+            return str
+        end
+    end,
 }
 
 local filetype = {
@@ -74,19 +118,29 @@ local filetype = {
 }
 local fileformat = {
     'fileformat',
-    color = 'USER1',
+    color = 'USER3',
     icons_enabled = true,
     separator = { left = '', right = '' },
 }
 
-local sections = {
-    lualine_a = { cwd },
-    lualine_b = { branch, fileformat },
-    lualine_c = { relative_path },
+-- @TODO (undg) 2022-12-30: merge it, create custom one
+local progress = {
+    'progress',
+    color = 'USER2',
+}
+local location = {
+    'location',
+    color = 'USER2',
+}
 
-    lualine_x = { 'diagnostics', 'diff' },
+local sections = {
+    lualine_a = { location, progress },
+    lualine_b = { branch, fileformat },
+    lualine_c = { cwd, '' },
+
+    lualine_x = { relative_path, 'diagnostics', 'diff' },
     lualine_y = { filetype },
-    lualine_z = { 'progress', 'location' },
+    lualine_z = { 'hostname' },
 }
 
 lualine.setup({
