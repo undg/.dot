71fe8ab33581de6c99ea9174c183744b371ae321
undg <5306983+undg@users.noreply.github.com>
2023-01-13 01:30:52 +0000

    arrange code. (#12)

--- vim/.config/nvim/lua/keymaps/nvim-tree.lua
+++ vim/.config/nvim/lua/keymaps/nvim-tree.lua
@@ -1,14 +1,20 @@
 local map = require('utils.map')
 
+local function nvimTreeToggleFocus()
+    if vim.bo.filetype == 'NvimTree' then
+        vim.api.nvim_input('<C-W><C-P>')
+    else
+        vim.cmd('NvimTreeFindFile')
+    end
+end
+vim.api.nvim_create_user_command('NvimTreeToggleFocus', nvimTreeToggleFocus, {})
+
 map.normal('<F2>', ':NvimTreeFindFileToggle<CR>')
 map.normal('<leader>2', ':NvimTreeFindFileToggle<CR>')
+map.normal('<leader>ff', ':NvimTreeFindFileToggle<cr>')
 
--- @TODO (undg) 2022-12-27: create wrapper for toggling focus between windows.
-map.normal('<F3>', ':NvimTreeFindFile<cr>')
-map.normal('<leader>3', ':NvimTreeFindFile<cr>')
+map.normal('<F3>', ':NvimTreeToggleFocus<cr>')
+map.normal('<leader>3', ':NvimTreeToggleFocus<cr>')
 
 map.normal('<F4>', ':NvimTreeClose<cr>')
 map.normal('<leader>4', ':NvimTreeClose<cr>')
-
-map.normal('<leader>ff', ':NvimTreeFindFileToggle<cr>')
--- map.normal("<leader>fc", ":NvimTreeClose<cr>")
