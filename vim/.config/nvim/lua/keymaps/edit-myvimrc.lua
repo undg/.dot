9a45e6691c4be025f3d9932e5513db8aa3fae419
undg <5306983+undg@users.noreply.github.com>
2023-01-22 16:18:34 +0000

    vim/keymaps-and-alpha
    Update keymaps for jumping to vim config and reorganize alpha

--- vim/.config/nvim/lua/keymaps/edit-myvimrc.lua
+++ vim/.config/nvim/lua/keymaps/edit-myvimrc.lua
@@ -1,8 +1,10 @@
 local map = require("utils.map")
 
+-- note: moved to Telescope keymaps
+--
 -- Open vimrc with ',ev'
-map.normal(
-    "<leader>ve",
-    "<CMD>tabnew " .. vim.env.MYVIMRC .. " | lcd %:p:h | Telescope find_files hidden=true no_ignore=true<CR>"
-)
+-- map.normal(
+--     "<leader>ve",
+--     "<CMD>tabnew " .. vim.env.MYVIMRC .. " | lcd %:p:h | Telescope find_files hidden=true no_ignore=true<CR>"
+-- )
 map.normal("<leader>vc", ':w<cr>:source %<CR>:echom "SOURCE current file"<CR>')
--- vim/.config/nvim/lua/plugins/alpha.lua
+++ vim/.config/nvim/lua/plugins/alpha.lua
@@ -22,16 +22,15 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-    dashboard.button('sl', '   >  Last session', ':SessionManager load_last_session<CR>'),
-    dashboard.button('oo', '   >  Old files', ':Telescope oldfiles<CR>'),
-    dashboard.button('oo', '   >  Old files', ':Telescope oldfiles cwd_only=true<CR>'),
-    dashboard.button('ss', '   >  Sessions list', ':SessionManager load_session<CR>'),
     dashboard.button('sc', '鬒  >  Session (current dir)', ':SessionManager load_current_dir_session<CR>'),
-    dashboard.button('fp', '⌨   >  Projects', ':Telescope project<CR>'),
+    dashboard.button('sl', '   >  Last session', ':SessionManager load_last_session<CR>'),
+    dashboard.button('ss', '   >  Sessions list', ':SessionManager load_session<CR>'),
+    dashboard.button('pp', '⌨   >  Projects', ':Telescope project<CR>'),
     dashboard.button('ff', '   >  Find files', ':Telescope find_files<CR>'),
-    dashboard.button('fv', '   >  Nvim find files', ':cd ~/.config/nvim/ | Telescope find_files cwd_only=true<CR>'),
-    dashboard.button('ov', '   >  Nvim old files', ':cd ~/.config/nvim/ | Telescope oldfiles cwd_only=true<CR>'),
     dashboard.button('fd', '   >  Dot find files', ':cd ~/.dot/ |  Telescope find_files cwd_only=true<CR>'),
+    dashboard.button('fv', '   >  Nvim find files', ':cd ~/.config/nvim/ | Telescope find_files cwd_only=true<CR>'),
+    dashboard.button('oo', '   >  Old files', ':Telescope oldfiles cwd_only=true<CR>'),
+    dashboard.button('ov', '   >  Nvim old files', ':cd ~/.config/nvim/ | Telescope oldfiles cwd_only=true<CR>'),
     dashboard.button('od', '   >  Dot old files', ':cd ~/.dot/ |  Telescope oldfiles cwd_only=true<CR>'),
     dashboard.button( 'e', '   >  New file', ':enew <BAR> startinsert <CR>'),
     dashboard.button( 'q', '   >  Quit', ':qa<CR>'),
