ba755fdaad43c19b212dfefebec2894515f072bf
undg <und3rdg@gmail.com>
2023-01-29 13:46:37 +0000

    vim/tmux: remap moving between windows/panes/buffers

--- vim/.config/nvim/lua/keymaps/windows-and-tabs.lua
+++ vim/.config/nvim/lua/keymaps/windows-and-tabs.lua
@@ -7,14 +7,12 @@ map.normal('<C-w>z', ':ZoomWinTabToggle<CR>')
 map.normal('<C-w><C-z>', ':ZoomWinTabToggle<CR>')
 
 map.normal('tt', ':tab split<CR>')
-map.normal('<C-w>t', ':tabnew %<CR>')
-map.normal('<C-w><C-t>', ':tabnew %<CR>')
 
 -- split navigations
-map.normal('<C-h>', '<C-w><C-h>')
-map.normal('<C-j>', '<C-w><C-j>')
-map.normal('<C-k>', '<C-w><C-k>')
-map.normal('<C-l>', '<C-w><C-l>')
+map.normal('<A-h>', '<C-w><C-h>')
+map.normal('<A-j>', '<C-w><C-j>')
+map.normal('<A-k>', '<C-w><C-k>')
+map.normal('<A-l>', '<C-w><C-l>')
 
 -- resize
 map.normal('<Up>', ':resize +5<cr>')
