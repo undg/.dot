8ff9b8c2966d774338006e2f079e1fd32c34af17
undg <und3rdg@gmail.com>
2023-02-01 21:47:27 +0000

    zsh: update $PATH. Add go lang, remove obsolete nonsense, explain convoluted part.

--- zsh/.config/zsh/path.zsh
+++ zsh/.config/zsh/path.zsh
@@ -4,20 +4,12 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.deno/bin:$PATH"
+export PATH="$PATH:$(go env GOBIN):$(go env GOPATH)/bin"
 
-
-# @TODO (undg) 2023-01-01: check that nonsens
-# leftover from larbs
-# Set the list of directories that Zsh searches for programs.
-path=(
-    /usr/local/{bin,sbin}
-    $HOME/.config/i3/scripts/
-    # $HOME/bin
-    $path
-)
-
-export PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
 # Adds `~/.config/i3/scripts` and all subdirectories to $PATH
 if [[ -d "$HOME/.config/i3/scripts/" ]]; then
     export PATH="$(du "$HOME/.config/i3/scripts/" | cut -f2 | tr '\n' ':')$PATH"
 fi
+
+# debloate WSL from m$ windows paths (fix lagging in terminal)
+export PATH=$(/usr/bin/printenv PATH | /usr/bin/perl -ne 'print join(":", grep { !/\/mnt\/[a-z]/ } split(/:/));')
