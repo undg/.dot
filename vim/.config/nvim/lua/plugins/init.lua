4e3027aa888085f6e39af625535ee1fc036bd344
undg <und3rdg@gmail.com>
2023-02-07 09:23:34 +0000

    vim: md preview pluging

--- vim/.config/nvim/lua/plugins/init.lua
+++ vim/.config/nvim/lua/plugins/init.lua
@@ -73,6 +73,14 @@ packer.startup(function(use)
             require('plugins.luapad')
         end,
     }) -- lua scratch pad
+    use({
+        'iamcco/markdown-preview.nvim',
+        run = 'cd app && npm install',
+        setup = function()
+            vim.g.mkdp_filetypes = { 'markdown' }
+        end,
+        ft = { 'markdown' },
+    })
 
     -- Git
     use({
