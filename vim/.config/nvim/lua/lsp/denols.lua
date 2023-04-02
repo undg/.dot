316e88ceff5941598d1cf551779e4ba6db0248a6
undg <und3rdg@gmail.com>
2023-04-02 20:32:49 +0100

    vim: cleanig plugins and LSP

--- vim/.config/nvim/lua/lsp/denols.lua
+++ vim/.config/nvim/lua/lsp/denols.lua
@@ -1,5 +1,11 @@
 local nvim_lsp = require("lspconfig")
 
+local ok_twoslash, twoslash = pcall(require, 'twoslash-queries')
+if not ok_twoslash then
+    print('lua/lsp/tsserver.lua: twoslash-queries fail')
+    return
+end
+
 return {
     root_dir = nvim_lsp.util.root_pattern("deno.json"),
     -- autostart = false,
@@ -7,4 +13,7 @@ return {
     init_options = {
         lint = true,
     },
+    on_attach = function(client, bufnr)
+        twoslash.attach(client, bufnr)
+    end,
 }
