80b9702e685babc55ff01d3161539df3a6858bb8
undg <und3rdg@gmail.com>
2023-04-05 18:05:56 +0100

    vim: make data shit consistent. overriding fugitive user commands

--- vim/.config/nvim/lua/keymaps/fugitive.lua
+++ vim/.config/nvim/lua/keymaps/fugitive.lua
@@ -4,7 +4,12 @@ map.normal('<leader>gcd', ':Gcd<CR>')
 map.normal('<leader>glcd', ':Glcd<CR>')
 
 map.normal('<leader>gll', ':0Gclog<CR>')
+vim.api.nvim_create_user_command('GfileLog', '0Gclog', { force = true, })
 map.normal('<leader>gld', ':Gclog -- %<CR>')
+vim.api.nvim_create_user_command('GfileLogDiff', 'Gclog -- %', { force = true })
 
--- make shit consistent.
-vim.api.nvim_create_user_command('Gblame', 'G blame', {})
+-- MAKE DAT SHIT CONSISTENT. tpope sucks
+-- vim.api.nvim_del_user_command('Gblame')
+-- vim.api.nvim_del_user_command('Gbrowse')
+vim.api.nvim_create_user_command('Gblame', 'G blame', { force = true })
+vim.api.nvim_create_user_command('Gbrowse', 'GBrowse', { force = true })
