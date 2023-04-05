1eaaa722e9e1e556c85baee21eb32a00d6f63ffe
undg <und3rdg@gmail.com>
2023-04-05 02:09:28 +0100

    vim: improve fugitive

--- vim/.config/nvim/lua/keymaps/fugitive.lua
+++ vim/.config/nvim/lua/keymaps/fugitive.lua
@@ -2,3 +2,9 @@ local map = require('utils.map')
 
 map.normal('<leader>gcd', ':Gcd<CR>')
 map.normal('<leader>glcd', ':Glcd<CR>')
+
+map.normal('<leader>gll', ':0Gclog<CR>')
+map.normal('<leader>gld', ':Gclog -- %<CR>')
+
+-- make shit consistent.
+vim.api.nvim_create_user_command('Gblame', 'G blame', {})
