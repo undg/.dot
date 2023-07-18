1502a3d514b1ff47eddcbd86889d61076c52ecdb
undg <und3rdg@gmail.com>
2023-07-19 00:50:55 +0100

    vim: ^? keymap

--- vim/.config/nvim/lua/plugins/twoslash-queries.lua
+++ vim/.config/nvim/lua/plugins/twoslash-queries.lua
@@ -1,3 +1,4 @@
+local map = require('utils.map')
 local twoslash_ok, twoslash = pcall(require, 'twoslash-queries')
 if not twoslash_ok then
     print('plugins/twoslash-queries.lua: missing requirements')
@@ -5,7 +6,11 @@ if not twoslash_ok then
 end
 
 twoslash.setup({
-    multi_line = true, -- to print types in multi line mode
-    is_enabled = true, -- to keep disabled at startup and enable it on request with the EnableTwoslashQueries
+    multi_line = true,        -- to print types in multi line mode
+    is_enabled = true,        -- to keep disabled at startup and enable it on request with the EnableTwoslashQueries
     highlight = 'DevIconBat', -- to set up a highlight group for the virtual text
 })
+
+
+map.normal('<leader>si', ':TwoslashQueriesInspect<CR>')
+map.normal('<leader>sr', ':TwoslashQueriesRemove<CR>')
