085e80a9c36c2531979d820cb29973131193b10d
undg <und3rdg@gmail.com>
2023-07-22 13:49:38 +0100

    vim: remover plugins-old backup

--- vim/.config/nvim/lua/plugins-old/QFEnter.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd[[
-    augroup myvimrc
-        autocmd!
-        autocmd QuickFixCmdPost [^l]* cwindow
-        autocmd QuickFixCmdPost l*    lwindow
-    augroup END
-]]
--- vim/.config/nvim/lua/plugins-old/colorizer.lua
+++
@@ -1,3 +0,0 @@
-vim.g.colorizer_startup = 0
-vim.g.colorizer_maxlines = 1000
-
