95838cd3b883f257900eec5cd0143eb973e524c7
undg <und3rdg@gmail.com>
2023-08-26 20:26:07 +0100

    nvim: slower version of prettier

--- vim/.config/nvim/lua/lsp/init.lua
+++ vim/.config/nvim/lua/lsp/init.lua
@@ -50,9 +50,9 @@ local lsp2mason = {
 if string.match(vim.fn.getcwd(), '/Arahi/') then
     null_ls.setup({
         sources = {
-            -- null_ls.builtins.formatting.prettier.with({
-            --     prefer_local = 'node_modules/.bin',
-            -- }),
+            null_ls.builtins.formatting.prettier.with({
+                prefer_local = 'node_modules/.bin',
+            }),
             null_ls.builtins.formatting.shfmt,
             null_ls.builtins.formatting.fixjson,
 
@@ -70,7 +70,6 @@ if string.match(vim.fn.getcwd(), '/Arahi/') then
 else
     null_ls.setup({
         sources = {
-            null_ls.builtins.formatting.prettierd,
             null_ls.builtins.formatting.stylua,
             null_ls.builtins.formatting.shfmt,
             null_ls.builtins.formatting.fixjson,
