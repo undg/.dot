06f5c48e92e870491f542546c66d5592e29109be
undg <und3rdg@gmail.com>
2023-10-24 00:23:00 +0100

    [vim] add Sourcegraph commands and keybindings
    This commit refactors the Sourcegraph plugin configuration to add new commands and keybindings for interacting with Cody. It adds a `shorten_type_error` function to ask Cody to explain type errors concisely, maps it to `<leader>ce`, and creates a `CodyShortenTypeError` user command.

--- vim/.config/nvim/lua/plugin/sg.lua
+++ vim/.config/nvim/lua/plugin/sg.lua
@@ -1,14 +1,32 @@
+local map = require('utils.map')
+
 return {
     'sourcegraph/sg.nvim',
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         -- Sourcegraph configuration. All keys are optional
-        require('sg').setup({
+        local sg = require('sg')
+        local commands = require('sg.cody.commands')
+        sg.setup({
             -- Pass your own custom attach function
             --        - gd -> goto definition
             --        - gr -> goto references
             -- on_attach = your_custom_lsp_attach_function,
             -- on_attach = require('lspconfig').on_attach,
         })
+
+        local function shorten_type_error()
+            vim.diagnostic.goto_prev()
+            local error = vim.diagnostic.get_next().message
+            vim.diagnostic.goto_next()
+
+            commands.ask({
+                'Explain this diagnostic in shortest possible way. Remove all unnecesary noise, make it short and sweet.'
+                .. error,
+            })
+        end
+
+        vim.api.nvim_create_user_command('CodyShortenTypeError', shorten_type_error, {})
+        map.normal('<leader>ce', shorten_type_error)
     end,
 }
