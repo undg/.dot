4ae97d764807850f1322053afd3e24fa009a0b69
undg <und3rdg@gmail.com>
2023-10-25 02:21:02 +0100

    [vim] fix readme generator

--- vim/.config/nvim/lua/plugin/sg.lua
+++ vim/.config/nvim/lua/plugin/sg.lua
@@ -37,19 +37,20 @@ return {
 
         local function generate_readme()
             vim.api.nvim_command(
-                'CodyTask ',
+                'CodyTask '
+                ..
                 'Write a detailed README.md file to document the code located in the same directory as my current selection. Summarize what the code in this directory is meant to accomplish. Explain the key files, functions, classes, and features. Use Markdown formatting for headings, code blocks, lists, etc. to make the it organized and readable. Aim for a beginner-friendly explanation that gives a developer unfamiliar with the code a good starting point to understand it. Make sure to include: - Overview of directory purpose - Functionality explanations - Relevant diagrams or visuals if helpful. Write the README content clearly and concisely using complete sentences and paragraphs based on the shared context. Use proper spelling, grammar, and punctuation throughout. Surround your full README text with triple backticks so it renders properly as a code block. Do not make assumptions or fabricating additional details.'
                 .. vim.fn.system('ls %:p:h')
             )
         end
 
-        vim.api.nvim_create_user_command('CodyCommitMessage', generate_commit_message, {})
-        vim.api.nvim_create_user_command('CodyGitCommitMessage', function()
+        vim.api.nvim_create_user_command('CodyGenCommitMessage', generate_commit_message, {})
+        vim.api.nvim_create_user_command('CodyGenGitCommitMessage', function()
             vim.api.nvim_command('G commit')
             generate_commit_message()
-        end, { range = 2 })
+        end, {})
 
-        vim.api.nvim_create_user_command('CodyReadme', generate_readme, { range = 2})
+        vim.api.nvim_create_user_command('CodyGenReadme', generate_readme, {})
         vim.api.nvim_create_user_command('CodyShortenTypeError', shorten_type_error, {})
         map.normal('<leader>ce', shorten_type_error)
     end,
