9a2bac84838bd47ef1f0d24f1a6019b4833935ac
undg <und3rdg@gmail.com>
2023-10-25 02:09:03 +0100

    [vim] Update sg.lua plugin
    Modified the sg.lua Sourcegraph plugin to:
    
    - Rename commands variable to cody_commands
    - Replace do_task calls with CodyTask user commands
    - Add CodyGitCommitMessage command to commit changes after generating message
    - Set range to 2 for CodyReadme and CodyGitCommitMessage commands

--- vim/.config/nvim/lua/plugin/sg.lua
+++ vim/.config/nvim/lua/plugin/sg.lua
@@ -6,7 +6,7 @@ return {
     config = function()
         -- Sourcegraph configuration. All keys are optional
         local sg = require('sg')
-        local commands = require('sg.cody.commands')
+        local cody_commands = require('sg.cody.commands')
         sg.setup({
             on_attach = function(client, bufnr)
                 vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
@@ -20,35 +20,36 @@ return {
             local error = vim.diagnostic.get_next().message
             vim.diagnostic.goto_next()
 
-            commands.ask({
+            cody_commands.ask({
                 'Explain this diagnostic in shortest possible way. Remove all unnecesary noise, make it short and sweet.'
                 .. error,
             })
         end
 
         local function generate_commit_message()
-            local git_diff = vim.fn.system('git diff --cached')
-
-            commands.do_task(
-                0,
-                2,
-                2,
-                'Suggest an informative commit message by summarizing code changes from the shared command output. The commit message should follow the conventional commit format and provide meaningful context for future readers. Make first line very short but meaningful.'
-                .. git_diff
+            vim.api.nvim_command(
+                'CodyTask '
+                ..
+                'Suggest an informative commit message by summarizing code changes from the shared command output. The commit message should provide meaningful context for future readers.'
+                .. vim.fn.system('git diff --cached')
             )
         end
 
         local function generate_readme()
-            commands.do_task(
-                0,
-                1,
-                1,
+            vim.api.nvim_command(
+                'CodyTask ',
                 'Write a detailed README.md file to document the code located in the same directory as my current selection. Summarize what the code in this directory is meant to accomplish. Explain the key files, functions, classes, and features. Use Markdown formatting for headings, code blocks, lists, etc. to make the it organized and readable. Aim for a beginner-friendly explanation that gives a developer unfamiliar with the code a good starting point to understand it. Make sure to include: - Overview of directory purpose - Functionality explanations - Relevant diagrams or visuals if helpful. Write the README content clearly and concisely using complete sentences and paragraphs based on the shared context. Use proper spelling, grammar, and punctuation throughout. Surround your full README text with triple backticks so it renders properly as a code block. Do not make assumptions or fabricating additional details.'
+                .. vim.fn.system('ls %:p:h')
             )
         end
 
         vim.api.nvim_create_user_command('CodyCommitMessage', generate_commit_message, {})
-        vim.api.nvim_create_user_command('CodyReadme', generate_readme, {})
+        vim.api.nvim_create_user_command('CodyGitCommitMessage', function()
+            vim.api.nvim_command('G commit')
+            generate_commit_message()
+        end, { range = 2 })
+
+        vim.api.nvim_create_user_command('CodyReadme', generate_readme, { range = 2})
         vim.api.nvim_create_user_command('CodyShortenTypeError', shorten_type_error, {})
         map.normal('<leader>ce', shorten_type_error)
     end,
