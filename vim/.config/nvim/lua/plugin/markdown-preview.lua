083894561c57f9e7b2d4f53b6ed9cabe023eafd4
undg <und3rdg@gmail.com>
2023-10-28 15:08:18 +0100

    [vim] Simplified markdown preview plugin configuration
    The markdown preview plugin configuration was simplified by:
    
    - Changing the build command to just install dependencies
    - Reducing the filetypes to only "markdown"
    - Removing unnecessary config options

--- vim/.config/nvim/lua/plugin/markdown-preview.lua
+++ vim/.config/nvim/lua/plugin/markdown-preview.lua
@@ -1,9 +1,8 @@
-return {
-    'iamcco/markdown-preview.nvim',
-    build = 'cd app && npm install',
-    config = function()
-        -- you need yarn or npm already installed in os
-        vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown' },
+return 
+{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
 }
+
