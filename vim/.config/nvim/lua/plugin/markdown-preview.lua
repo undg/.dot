0f585d44f1296b39ef1f40399c2bde1ce2d0ed4f
undg <und3rdg@gmail.com>
2023-10-28 15:14:57 +0100

    [vim] Fix markdown preview plugin
    https://github.com/iamcco/markdown-preview.nvim/issues/616
    
    - Updated markdown-preview.nvim plugin to latest commit
    - Customized markdown preview plugin install script to handle rebuild
      error

--- vim/.config/nvim/lua/plugin/markdown-preview.lua
+++ vim/.config/nvim/lua/plugin/markdown-preview.lua
@@ -1,8 +1,29 @@
-return 
-{
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
-}
+return {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+        local job = require('plenary.job')
+        local install_path = vim.fn.stdpath('data') .. '/lazy/markdown-preview.nvim/app'
+        local cmd = 'bash'
 
+        if vim.fn.has('win64') == 1 then
+            cmd = 'pwsh'
+        end
+
+        job:new({
+            command = cmd,
+            args = { '-c', 'npm install && git restore .' },
+            cwd = install_path,
+            on_exit = function()
+                print('Finished installing markdown-preview.nvim')
+            end,
+            on_stderr = function(_, data)
+                print(data)
+            end,
+        }):start()
+
+        -- Options
+        vim.g.mkdp_auto_close = 0
+    end,
+}
