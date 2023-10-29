700cfce2d734d1dc514db02a0242f3f717bcf5e5
undg <und3rdg@gmail.com>
2023-10-29 17:01:44 +0000

    [vim] Add table utility functions
    Add two new table utility functions to lua/utils/table.lua:
    
    - concat: Concatenates two tables by appending the second table to the end of the first
    - map: Maps a function over the values of a table, returning a new table
    
    Also includes:
    
    - Docstrings with LuaDoc annotations
    - Input validation on map()

---
+++ vim/.config/nvim/lua/utils/table.lua
@@ -0,0 +1,44 @@
+local M = {}
+
+---Appends a table t2 to the end of a table t1
+---
+---@param t1 table first table
+---@param t2 table second table to append
+---@return table A new table with the mapped values
+function M.concat(t1, t2)
+    local tOut = {}
+
+    for i = 1, #t1 do
+        tOut[i] = t1[i]
+    end
+
+    for i = #t1, #t1 + #t2 do
+        tOut[i] = t2[i]
+    end
+
+    return tOut
+end
+
+---Maps a function fn over the values of a table, returning a new table
+---with the results.
+---
+---@generic T: table, K, V
+---@param input_table T The input table
+---@param mapper fun(value: V, key?: K): T The function to map over the values
+---@return T A new table with the mapped values
+function M.map(input_table, mapper)
+    local new_table = {}
+
+    if type(input_table) ~= 'table' then
+        print('Error: input_table must be a table')
+        P(input_table)
+        return new_table
+    end
+
+    for key, value in pairs(input_table) do
+        new_table[key] = mapper(value, key)
+    end
+    return new_table
+end
+
+return M
