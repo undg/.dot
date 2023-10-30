e5104d9922fe14496d3fbf3511f0e644897b90db
undg <und3rdg@gmail.com>
2023-10-30 00:01:33 +0000

    [vim] Add sg.nvim custom command: text.proofreading
    Added a new text module to provide text proofreading capabilities.
    
    Added mapping to invoke the new proofreading.
    
    This provides a simple way to get AI assistance with improving text quality.

--- vim/.config/nvim/lua/custom/sg/init.lua
+++ vim/.config/nvim/lua/custom/sg/init.lua
@@ -1,7 +1,8 @@
-local type_error = require("custom.sg.type-error")
-local commit = require("custom.sg.commit")
-local pull_request = require("custom.sg.pull-request")
-local readme = require("custom.sg.readme")
+local type_error = require('custom.sg.type-error')
+local commit = require('custom.sg.commit')
+local pull_request = require('custom.sg.pull-request')
+local readme = require('custom.sg.readme')
+local text = require('custom.sg.text')
 
 local M = {}
 
@@ -9,5 +10,6 @@ M.type_error = type_error
 M.commit = commit
 M.pull_request = pull_request
 M.readme = readme
+M.text = text
 
 return M
