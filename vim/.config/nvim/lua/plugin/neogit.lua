457bd944305340ef9f6751a6eda6784ee07507bb
undg <und3rdg@gmail.com>
2023-11-08 21:18:53 +0000

    [vim] Add neogit plugin (magit clone)
    Add neogit plugin for git integration.
    
    Changes:
    
    + Import neogit plugin in _all.lua
    + Add neogit.lua config file
    + Setup keybindings for neogit
    + Add dependencies for neogit
    
    This integrates neogit, a magit clone, for git operations
    in neovim.

---
+++ vim/.config/nvim/lua/plugin/neogit.lua
@@ -0,0 +1,18 @@
+return {
+    'NeogitOrg/neogit',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+        'sindrets/diffview.nvim',
+        'ibhagwan/fzf-lua',
+    },
+    config = function()
+        require('neogit').setup()
+        local keymap = require('utils.keymap')
+
+        keymap.normal('<leader>gg', ':Neogit<cr>', { desc = 'Neogit: Open' })
+        keymap.normal('<leader>gf', ':DiffviewFileHistory<cr>', { desc = 'Neogit: Open diff for current file' })
+        keymap.visual('<leader>gf', ':DiffviewFileHistory<cr>', { desc = 'Neogit: Open Open diff for current selection' })
+        keymap.normal('<leader>gq', ':DiffviewClose<cr>', { desc = 'Neogit: Close' })
+    end,
+}
