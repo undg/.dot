081000d9bc5cb900eb4bfece9b4ca8cb173744b5
undg <und3rdg@gmail.com>
2023-11-28 13:05:23 +0000

    [vim] General todo snippet with self comment function

--- vim/.config/nvim/lua/custom/init.lua
+++ vim/.config/nvim/lua/custom/init.lua
@@ -7,3 +7,17 @@ require('custom.css2tw')
 require('custom.open-plugin-page')
 
 
+vim.cmd[[
+
+function GetCommentMarker()
+if len(split(&l:commentstring, '%s')) == 1
+" if 'commentstring' xx%sxx contains no end part
+    return split(&l:commentstring, '%s')[0]
+  elseif match(&l:comments, '\v(,|^):[^,:]*(,|$)')
+    " if 'comments' contains ',:xxx,'
+    return matchstr(&l:comments, '\v(,|^):\zs[^,:]*\ze(,|$)')
+  else
+echoerr "unable to find line comment marker."
+endif
+endfunction
+]]
