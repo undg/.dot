e23300dd9bc82d86a0726b199996b49b67333cc9
undg <und3rdg@gmail.com>
2023-12-20 04:11:54 +0000

    [vim] rm few actions

--- vim/.config/nvim/lua/plugin/typescript.lua
+++ vim/.config/nvim/lua/plugin/typescript.lua
@@ -24,21 +24,17 @@ return {
                 require('twoslash-queries').attach(client, bufnr)
             end,
             settings = {
-                -- spawn additional tsserver instance to calculate diagnostics on it
                 separate_diagnostic_server = true,
-                -- "change"|"insert_leave" determine when the client asks the server about diagnostic
                 publish_diagnostic_on = 'insert_leave',
                 -- array of strings("fix_all"|"add_missing_imports"|"remove_unused"|
                 -- "remove_unused_imports"|"organize_imports") -- or string "all"
                 -- to include all supported code actions
                 -- specify commands exposed as code_actions
-                expose_as_code_action = 'all',
-                -- string|nil - specify a custom path to `tsserver.js` file, if this is nil or file under path
-                -- not exists then standard path resolution strategy is applied
+                expose_as_code_action = { 'fix_all', 'add_missing_imports', 'remove_unused', 'organize_imports' },
                 tsserver_path = nil,
                 -- specify a list of plugins to load by tsserver, e.g., for support `styled-components`
                 -- (see 💅 `styled-components` support section)
-                tsserver_plugins = {},
+                tsserver_plugins = { 'styled-components' },
                 -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
                 -- memory limit in megabytes or "auto"(basically no limit)
                 tsserver_max_memory = 'auto',
