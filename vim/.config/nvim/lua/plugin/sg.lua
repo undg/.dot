f88f7e505881c44dc6db617525c70606676281cd
undg <und3rdg@gmail.com>
2023-12-20 14:16:56 +0000

    [vim] ai keybindings

--- vim/.config/nvim/lua/plugin/sg.lua
+++ vim/.config/nvim/lua/plugin/sg.lua
@@ -14,36 +14,36 @@ return {
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = bufnr })
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
             end,
-            auth_strategy = "environment-variables",
+            auth_strategy = 'environment-variables',
         })
 
         -- stylua: ignore start
         vim.api.nvim_create_user_command('AiCommitTitle', ai.commit.title.get, { desc = ai.commit.title.desc })
-        vim.keymap.set('n', '<leader>at', ai.commit.title.get, { desc = ai.commit.title.desc })
+        vim.keymap.set('n', '<leader>agt', ai.commit.title.get, { desc = ai.commit.title.desc })
 
         vim.api.nvim_create_user_command('AiCommitMessage', ai.commit.message.get, { desc = ai.commit.message.desc })
-        vim.keymap.set('n', '<leader>ac', ai.commit.message.get, { desc = ai.commit.message.desc })
+        vim.keymap.set('n', '<leader>agc', ai.commit.message.get, { desc = ai.commit.message.desc })
 
         vim.api.nvim_create_user_command('AiCommitMessageOpen', ai.commit.open_with_message.get,
             { desc = ai.commit.open_with_message.desc })
-        vim.keymap.set('n', '<leader>ag', ai.commit.open_with_message.get, { desc = ai.commit.open_with_message.desc })
+        vim.keymap.set('n', '<leader>agg', ai.commit.open_with_message.get, { desc = ai.commit.open_with_message.desc })
 
         vim.api.nvim_create_user_command('AiPullRequestDescription', ai.pull_request.description.get,
             { desc = ai.pull_request.description.desc })
-        vim.keymap.set('n', '<leader>ap', ai.pull_request.description.get, { desc = ai.pull_request.description.desc })
+        vim.keymap.set('n', '<leader>agp', ai.pull_request.description.get, { desc = ai.pull_request.description.desc })
 
         vim.api.nvim_create_user_command('AiGenerateReadme', ai.readme.body.get,
             { desc = ai.readme.body.desc })
 
         vim.api.nvim_create_user_command('AiTypeErrorShort', ai.type_error.shorten.get,
             { desc = ai.type_error.shorten.desc })
-        vim.keymap.set('n', '<leader>ae', ai.type_error.shorten.get,
-            { desc = ai.type_error.shorten.desc })
+        vim.keymap.set('n', '<leader>ae', ai.type_error.shorten.get, { desc = ai.type_error.shorten.desc })
 
         vim.api.nvim_create_user_command('AiTextProofread', ai.type_error.shorten.get,
             { desc = ai.type_error.shorten.desc })
-        vim.keymap.set('n', '<leader>at', ai.text.proofread.get,
-            { desc = ai.text.proofread.desc })
+        vim.keymap.set('n', '<leader>at', ai.text.proofread.get, { desc = ai.text.proofread.desc })
         -- stylua: ignore end
+
+        vim.keymap.set('v', '<leader>aca', ':CodyAsk ')
     end,
 }
