069a43c7d72b1d1a87c39cb83297544ace6aed79
undg <und3rdg@gmail.com>
2024-01-04 20:58:12 +0000

    [vim] rearange code

--- vim/.config/nvim/lua/plugin/gp-nvim.lua
+++ vim/.config/nvim/lua/plugin/gp-nvim.lua
@@ -17,59 +17,53 @@ return {
 
             -- directory for storing chat files
             chat_dir = vim.fn.stdpath('data'):gsub('/$', '') .. '/gp/chats',
+
+            -- default command agents (model + persona)
+            -- name, model and system_prompt are mandatory fields
+            -- to use agent for chat set chat = true, for command set command = true
+            -- to remove some default agent completely set it just with the name like:
+            -- agents = {  { name = "ChatGPT4" }, ... },
             agents = {
-                {
-                    name = 'ChatGPT4',
-                    chat = true,
-                    command = false,
-                    -- string with model name or table with model name and parameters
-                    model = { model = 'gpt-4-1106-preview', temperature = 1.1, top_p = 1 },
-                    -- system prompt (use this to specify the persona/role of the AI)
-                    system_prompt = 'You are a general AI assistant.\n\n'
-                        .. 'The user provided the additional info about how they would like you to respond:\n\n'
-                        .. "- If you're unsure don't guess and say you don't know instead.\n"
-                        .. '- Ask question if you need clarification to provide better answer.\n'
-                        .. '- Think deeply and carefully from first principles step by step.\n'
-                        .. '- Zoom out first to see the big picture and then zoom in to details.\n'
-                        .. '- Use Socratic method to improve your thinking and coding skills.\n'
-                        .. "- Don't elide any code from your output if the answer requires coding.\n"
-                        .. "- Be conceal. Make your answers short and to the point. Do not mansplain.\n"
-                        .. "- You can be funny and sacastic.\n"
-                        .. "- Take a deep breath; You've got this!\n",
-                },
                 {
                     name = 'ChatGPT3-5',
                     chat = true,
                     command = false,
                     -- string with model name or table with model name and parameters
-                    model = { model = 'gpt-3.5-turbo-1106', temperature = 1.1, top_p = 1 },
+                    model = { model = 'gpt-3.5-turbo-1106', temperature = 0.5, top_p = 1 },
                     -- system prompt (use this to specify the persona/role of the AI)
-                    system_prompt = 'You are a general AI assistant.\n\n'
-                        .. 'The user provided the additional info about how they would like you to respond:\n\n'
-                        .. "- If you're unsure don't guess and say you don't know instead.\n"
-                        .. '- Ask question if you need clarification to provide better answer.\n'
-                        .. '- Think deeply and carefully from first principles step by step.\n'
-                        .. '- Zoom out first to see the big picture and then zoom in to details.\n'
-                        .. '- Use Socratic method to improve your thinking and coding skills.\n'
-                        .. "- Don't elide any code from your output if the answer requires coding.\n"
-                        .. "- Be conceal. Make your answers short and to the point. Do not mansplain.\n"
-                        .. "- You can be funny and sacastic.\n"
-                        .. "- Take a deep breath; You've got this!\n",
+                    system_prompt =
+                        'You are a ChatGPT3-5, focused on delivering efficient, succinct responses without unnecessary elaboration.\n\n'
+                        .. "- If unsure, respond with 'I don't know'.\n"
+                        .. '- Request clarification only when crucial.\n'
+                        .. '- Emphasize first principles for reasoning.\n'
+                        .. '- Pay attention to the broader context before specifics.\n'
+                        .. '- Use the Socratic method to refine thinking and problem-solving.\n'
+                        .. '- Provide complete code when necessary without excessive details.\n'
+                        .. '- Keep explanations concise and precise, avoiding excessive verbosity.\n'
+                        .. '- Utilize humor or sarcasm when contextually appropriate.\n'
+                        .. "- Stay focused and effective in your responses. You've got this!\n",
                 },
                 {
-                    name = 'CodeGPT4',
+                    name = 'ChatGPT4',
                     chat = true,
-                    command = true,
+                    command = false,
                     -- string with model name or table with model name and parameters
-                    model = { model = 'gpt-4-1106-preview', temperature = 0.8, top_p = 1 },
-                    -- system prompt (use this to specify the persona/role of the AI)
-                    system_prompt = 'You are an AI working as a code editor.\n\n'
-                        .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
-                        .. 'START AND END YOUR ANSWER WITH:\n\n```',
+                    model = { model = 'gpt-4-1106-preview', temperature = 0.5, top_p = 1 },
+                    system_prompt =
+                        'You are a concise AI assistant. Provide short, to-the-point answers quickly. Use plain language.\n\n'
+                        .. "- If unsure, say you don't know.\n"
+                        .. '- Ask for clarity when needed.\n'
+                        .. '- Use first principles for deeper understanding.\n'
+                        .. '- Look at the big picture before details.\n'
+                        .. '- Employ Socratic questioning to refine thinking and code.\n'
+                        .. '- Provide full code when relevant.\n'
+                        .. '- Keep explanations brief, clear, and avoid overexplaining.\n'
+                        .. '- Add humor or sarcasm when fitting.\n'
+                        .. "- Be ready. Let's do this!\n",
                 },
                 {
                     name = 'CodeGPT3-5',
-                    chat = true,
+                    chat = false,
                     command = true,
                     -- string with model name or table with model name and parameters
                     model = { model = 'gpt-3.5-turbo-1106', temperature = 0.8, top_p = 1 },
@@ -78,6 +72,17 @@ return {
                         .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
                         .. 'START AND END YOUR ANSWER WITH:\n\n```',
                 },
+                {
+                    name = 'CodeGPT4',
+                    chat = false,
+                    command = true,
+                    -- string with model name or table with model name and parameters
+                    model = { model = 'gpt-4-1106-preview', temperature = 0.8, top_p = 1 },
+                    -- system prompt (use this to specify the persona/role of the AI)
+                    system_prompt = 'You are an AI working as a code editor.\n\n'
+                        .. 'Please AVOID COMMENTARY OUTSIDE OF THE SNIPPET RESPONSE.\n'
+                        .. 'START AND END YOUR ANSWER WITH:\n\n```',
+                }
             },
             -- chat user prompt prefix
             chat_user_prefix = '🗨:',
