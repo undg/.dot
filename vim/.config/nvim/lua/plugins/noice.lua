c61172196839f8cd99f861e7f325219464025046
undg <und3rdg@gmail.com>
2024-01-08 04:37:39 +0000

    [vim] shush lua lsp barks

--- vim/.config/nvim/lua/plugins/noice.lua
+++ vim/.config/nvim/lua/plugins/noice.lua
@@ -29,13 +29,51 @@ return {
                 -- opts: any options passed to the view
                 -- icon_hl_group: optional hl_group for the icon
                 -- title: set to anything or empty string to hide
-                cmdline = { pattern = '^:', icon = '', lang = 'vim' },
-                search_down = { kind = 'search', pattern = '^/', icon = ' ', lang = 'regex' },
-                search_up = { kind = 'search', pattern = '^%?', icon = ' ', lang = 'regex' },
-                filter = { pattern = '^:%s*!', icon = '$', lang = 'bash' },
-                lua = { pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' }, icon = '', lang = 'lua' },
-                help = { pattern = '^:%s*he?l?p?%s+', icon = '' },
-                input = {}, -- Used by input()
+                cmdline = {
+                    kind = '',
+                    view = 'cmdline_popup',
+                    pattern = '^:',
+                    icon = '',
+                    lang = 'vim',
+                },
+                search_down = {
+                    kind = 'search',
+                    view = 'cmdline',
+                    pattern = '^/',
+                    icon = ' ',
+                    lang = 'regex',
+                },
+                search_up = {
+                    kind = 'search',
+                    view = 'cmdline',
+                    pattern = '^%?',
+                    icon = ' ',
+                    lang = 'regex',
+                },
+                filter = {
+                    kind = '',
+                    view = 'cmdline_popup',
+                    pattern = '^:%s*!',
+                    icon = '$',
+                    lang = 'bash',
+                },
+                lua = {
+                    kind = '',
+                    view = 'cmdline_popup',
+                    pattern = { '^:%s*lua%s+', '^:%s*lua%s*=%s*', '^:%s*=%s*' },
+                    icon = '',
+                    lang = 'lua',
+                },
+                help = {
+                    kind = '',
+                    view = 'cmdline_popup',
+                    pattern = '^:%s*he?l?p?%s+',
+                    icon = '',
+                },
+                input = {
+                    kind = '',
+                    view = 'cmdline_popup',
+                }, -- Used by input()
                 -- lua = false, -- to disable a format, set to `false`
             },
         },
