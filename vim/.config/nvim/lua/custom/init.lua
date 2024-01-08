a57999f3fac05f1b06c65b2c053e20653b0f4077
undg <und3rdg@gmail.com>
2024-01-08 04:18:37 +0000

    [vim] arrow up/down for command line with pum visible only

--- vim/.config/nvim/lua/custom/init.lua
+++ vim/.config/nvim/lua/custom/init.lua
@@ -21,17 +21,11 @@ vim.cmd([[
   endfunction
 ]])
 
-vim.cmd[[
-
-function GetCommentMarker()
-if len(split(&l:commentstring, '%s')) == 1
-" if 'commentstring' xx%sxx contains no end part
-    return split(&l:commentstring, '%s')[0]
-  elseif match(&l:comments, '\v(,|^):[^,:]*(,|$)')
-    " if 'comments' contains ',:xxx,'
-    return matchstr(&l:comments, '\v(,|^):\zs[^,:]*\ze(,|$)')
-  else
-echoerr "unable to find line comment marker."
-endif
-endfunction
-]]
+-- in command line, after TAB, use arrow up/down for completion, instead of left/right.
+-- preserver arrow up/down for last command when pum is not visible
+vim.cmd([[
+  if &wildoptions =~# "pum"
+    cnoremap <expr> <up> pumvisible() ? '<left>' : '<up>'
+    cnoremap <expr> <down> pumvisible() ? '<right>' : '<down>'
+  endif
+]])
