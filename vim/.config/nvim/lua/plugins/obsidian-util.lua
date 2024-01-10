d2e1a00d7d312d4cd31643fe5ee9e09a842bf949
undg <und3rdg@gmail.com>
2024-01-10 01:40:28 +0000

    [vim] obsidian + keymap + alpha

---
+++ vim/.config/nvim/lua/plugins/obsidian-util.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.workspaces = {
+    work = {
+        name = 'Work',
+        path = '~/Dropbox/DropsyncFiles/Work',
+    },
+    personal = {
+        name = 'Personal',
+        path = '~/Dropbox/DropsyncFiles/Personal',
+    },
+    wiki = {
+        name = 'Wiki',
+        path = '~/Dropbox/DropsyncFiles/vimwiki',
+    },
+}
+
+return M
