18fec305a3c0ef9ef7515c10760eb09efb347ef7
undg <und3rdg@gmail.com>
2024-01-13 01:24:05 +0000

    [vim] cleaning

--- vim/.config/nvim/lua/plugins/colorizer.lua
+++ vim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,8 +1,11 @@
-return {
+local M = {
     'lilydjwg/colorizer', -- https://github.com/lilydjwg/colorizer
     cmd = { 'ColorHighlight', 'ColorToggle' },
-    init = function()
-        vim.g.colorizer_startup = 0
-        vim.g.colorizer_maxlines = 1000
-    end,
 }
+return
+function M.init()
+    vim.g.colorizer_startup = 0
+    vim.g.colorizer_maxlines = 1000
+end
+
+return M
--- vim/.config/nvim/lua/plugins/nvim-surround.lua
+++ vim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -1,5 +1,13 @@
-return {
+local M = {
     'kylechui/nvim-surround', -- https://github.com/kylechui/nvim-surround
     version = '*',            -- Use for stability; omit to use `main` branch for the latest features
-    opts = {},
+    event = 'VeryLazy',
 }
+
+M.opts = {
+    highlight = {
+        duration = 200,
+    },
+}
+
+return M
