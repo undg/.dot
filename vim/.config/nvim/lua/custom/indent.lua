021b84b77498d8a44e77a060702a15ba75f89cf1
undg <und3rdg@gmail.com>
2024-01-21 01:27:24 +0000

    [vim] fix indentTogle

--- vim/.config/nvim/lua/custom/indent.lua
+++ vim/.config/nvim/lua/custom/indent.lua
@@ -3,13 +3,15 @@ local function toggle()
         vim.g.softtabstop = 4
         vim.opt.softtabstop = 4
         vim.opt.shiftwidth = 4
+        vim.opt.tabstop = 4
         print('Switched to: Indent with 4 spaces.')
     else
         vim.g.softtabstop = 2
         vim.opt.softtabstop = 2
         vim.opt.shiftwidth = 2
+        vim.opt.tabstop = 2
         print('Switched to: Indent with 2 spaces.')
     end
 end
 
-vim.api.nvim_create_user_command('ToggleIndent', toggle, {})
+vim.api.nvim_create_user_command('IndentToggle', toggle, {})
