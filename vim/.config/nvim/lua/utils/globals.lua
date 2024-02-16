1f6255b64c333cee0c752e4a269ee5282e0ec2d0
undg <und3rdg@gmail.com>
2024-02-16 21:57:44 +0000

    [vim] improve LuaDoc

--- vim/.config/nvim/lua/utils/globals.lua
+++ vim/.config/nvim/lua/utils/globals.lua
@@ -1,25 +1,25 @@
--- R reloads the given Lua package
---
--- @param package string: The name of the package to reload
--- @return The reloaded package
---
--- @usage
--- local utils = R('utils')
+--- R reloads the given Lua package
+---
+--- @param package string: The name of the package to reload
+--- @return unknown|nil The reloaded package
+---
+--- @usage
+--- local utils = R('utils')
 R = function(package)
     require('package').loaded = nil
     return require(package)
 end
 
--- P prints the given value and returns it
---
--- @param any: The value to print and return
--- @return The input value
---
--- @usage
--- local x = P({a=1, b=2})
---
--- @usage
--- P({a=1, b=2})
+--- P prints the given value and returns it
+---
+--- @param any unknown: The value to print and return
+--- @return unknown: The input value
+---
+--- @usage
+--- local x = P({a=1, b=2})
+---
+--- @usage
+--- P({a=1, b=2})
 P = function(any)
     print(vim.inspect(any))
     return any
@@ -27,6 +27,7 @@ end
 
 LAZY_PLUGIN_SPEC = {}
 
+---Load plugin spec from file
 ---@param path string @path to spec file
 function Spec(path)
     table.insert(LAZY_PLUGIN_SPEC, { import = path })
