49b1ec02ad9193750e8cbaa82a879cee252a55ff
undg <und3rdg@gmail.com>
2024-02-26 10:37:50 +0000

    [vim] re-enable eslint. Performance issues are still significant. ⌛

--- vim/.config/nvim/lua/lsp/init.lua
+++ vim/.config/nvim/lua/lsp/init.lua
@@ -47,7 +47,7 @@ local lsp2mason = {
 
 local M_null_ls_sources = {
     null_ls.builtins.diagnostics.eslint_d,
-    -- null_ls.builtins.formatting.eslint_d,
+    null_ls.builtins.formatting.eslint_d,
     -- null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.fixjson,
