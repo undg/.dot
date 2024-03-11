828e25e9b49e220ef14c19dc2e7e776e43802221
undg <und3rdg@gmail.com>
2024-03-11 11:26:08 +0000

    [vim] sensible `s`

--- vim/.config/nvim/lua/plugins/mini-surround.lua
+++ vim/.config/nvim/lua/plugins/mini-surround.lua
@@ -12,7 +12,7 @@ return {
 
             -- Module mappings. Use `''` (empty string) to disable one.
             mappings = {
-                add = 'sa',            -- Add surrounding in Normal and Visual modes
+                add = 's',            -- Add surrounding in Normal and Visual modes
                 delete = 'sd',         -- Delete surrounding
                 find = 'sf',           -- Find surrounding (to the right)
                 find_left = 'sF',      -- Find surrounding (to the left)
