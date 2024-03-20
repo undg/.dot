e99a627674722ce0e6ed9569511caf6d8f66bf96
undg <und3rdg@gmail.com>
2024-03-20 23:40:58 +0000

    [vim] keymap from tpope plugin

--- vim/.config/nvim/lua/plugins/mini-surround.lua
+++ vim/.config/nvim/lua/plugins/mini-surround.lua
@@ -17,7 +17,7 @@ return {
                 find = 'sf',           -- Find surrounding (to the right)
                 find_left = 'sF',      -- Find surrounding (to the left)
                 highlight = 'sh',      -- Highlight surrounding
-                replace = 'sr',        -- Replace surrounding
+                replace = 'cs',        -- Replace surrounding
                 update_n_lines = 'sn', -- Update `n_lines`
 
                 suffix_last = 'l',     -- Suffix to search with "prev" method
