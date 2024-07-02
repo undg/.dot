16f29a0593c5b6838c797de6c2a7fc1682ed44bf
undg <und3rdg@gmail.com>
2024-07-02 17:11:25 +0100

    [bin] [bin] wip handbrake

--- bin/bin/handbrake-youtube.sh
+++ bin/bin/handbrake-youtube.sh
@@ -5,7 +5,7 @@ Y='\033[1;32m' #'1;32' is Yellow's ANSI color code
 B='\033[0;34m' #'0;34' is Blue's ANSI color code
 NC='\033[0m'   #'0'    is back no color
 
-PRESET_JSON="~/bin/handbrake-youtube-preset.sh"
+PRESET_JSON="~/bin/handbrake-youtube/preset.jsom"
 
 
 if [[ $# -eq 0 ]] || [[ "$1" =~ ^(-h|--help|help)$ ]]; then
@@ -20,21 +20,18 @@ if [[ $# -eq 0 ]] || [[ "$1" =~ ^(-h|--help|help)$ ]]; then
 fi
 
 CURRENT_DIR=$(pwd)
-# @TODO (undg) 2024-06-28: fix files with spaces in names
-files=$@ # files passed as an arguments
+f=$@ # file passed as an arguments
 
-for f in $files; do
-	f_date=$(date -r "$f" +"%Y-%m-%d_%H-%M-%S")
-	f_fullname="${f##*/}"
-	f_name="${f_fullname%.*}"
-	f_ext=${f##*.}
-	f_out="$f_name-handbrake.mp4"
+f_date=$(date -r "$f" +"%Y-%m-%d_%H-%M-%S")
+f_fullname="${f##*/}"
+f_name="${f_fullname%.*}"
+f_ext=${f##*.}
+f_out="$f_name-handbrake.mp4"
 
-	printf "\n${B}~~~~~~>${G} $CURRENT_DIR/$f ${B}===>${G} $f_out ${NC}\n"
+printf "\n${B}~~~~~~>${G} $CURRENT_DIR/$f ${B}===>${G} $f_out ${NC}\n"
 
-	HandBrakeCLI --preset-import-file "$PRESET_JSON" -i "$f" -o "$f_out"
+HandBrakeCLI --preset-import-file "$PRESET_JSON"  -i "\"$f\"" -o \""$f_out"\"
 
-	notify-send "handbraked: $f"
-done
+notify-send "handbraked: $f"
 
 notify-send "handbraked-all: $files"
