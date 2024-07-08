fefed70bb2ada6cc36844dfdac3880599697a5a6
undg <und3rdg@gmail.com>
2024-07-08 23:45:46 +0100

    [vim] remove one checkbox option

--- vim/.config/nvim/lua/plugins/obsidian.lua
+++ vim/.config/nvim/lua/plugins/obsidian.lua
@@ -34,7 +34,7 @@ return {
           -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
           -- [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
           -- ['x'] = { char = '', hl_group = 'ObsidianDone' },
-          ['>'] = { char = '', hl_group = 'ObsidianTodo' },
+          -- ['>'] = { char = '', hl_group = 'ObsidianTodo' },
           -- ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
           ['!'] = { char = '', hl_group = 'ObsidianImportant' },
           -- Replace the above with this if you don't have a patched font:
