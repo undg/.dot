0623787c60a717998ffe96751a0e6f2e496e4d5b
undg <und3rdg@gmail.com>
2024-07-10 10:32:59 +0100

    [vim] [vim] remove more nonsense from AK's eslint bloated rules

--- vim/.config/nvim/lua/lsp/eslint.lua
+++ vim/.config/nvim/lua/lsp/eslint.lua
@@ -32,6 +32,7 @@ return {
             { rule = '@typescript-eslint/no-unused-vars', severity = 'off' },
             { rule = 'spaced-comment',                    severity = 'off' },
             { rule = 'simple-import-sort/imports',        severity = 'off' },
+            { rule = 'react/self-closing-comp',           severity = 'off' },
         },
         run = 'onSave',
         severity_sort = true,
