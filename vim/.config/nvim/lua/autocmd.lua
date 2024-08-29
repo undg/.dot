2a2ab74a54266ea2255d74a4e7f6c83b89ca0e14
undg <und3rdg@gmail.com>
2024-08-29 19:11:54 +0100

    [vim] git conflicts autocommand

--- vim/.config/nvim/lua/autocmd.lua
+++ vim/.config/nvim/lua/autocmd.lua
@@ -83,3 +83,15 @@ vim.api.nvim_create_autocmd({ 'FileType' }, {
         Keymap.normal('gfr', ':TSToolsFileReferences<cr>', { desc = 'lsp: show file references' })
     end,
 })
+
+-- Detect Git conflicts and set up a keymap to resolve them
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'GitConflictDetected',
+  callback = function()
+    vim.notify('Conflict detected in '..vim.fn.expand('<afile>'))
+    vim.keymap.set('n', 'cww', function()
+      engage.conflict_buster()
+      create_buffer_local_mappings()
+    end)
+  end
+})
---
+++ vim/.config/nvim/lua/plugins/git-conflict.lua
@@ -0,0 +1,24 @@
+return {
+    'akinsho/git-conflict.nvim', -- https://github.com/akinsho/git-conflict.nvim
+    version = '*',
+    config = true,
+}
+
+-- {
+--   default_mappings = true, -- disable buffer local mapping created by this plugin
+--   default_commands = true, -- disable commands created by this plugin
+--   disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
+--   list_opener = 'copen', -- command or function to open the conflicts list
+--   highlights = { -- They must have background color, otherwise the default color will be used
+--     incoming = 'DiffAdd',
+--     current = 'DiffText',
+--   }
+-- }
+
+-- GitConflictChooseOurs — Select the current changes.
+-- GitConflictChooseTheirs — Select the incoming changes.
+-- GitConflictChooseBoth — Select both changes.
+-- GitConflictChooseNone — Select none of the changes.
+-- GitConflictNextConflict — Move to the next conflict.
+-- GitConflictPrevConflict — Move to the previous conflict.
+-- GitConflictListQf — Get all conflict to quickfix
