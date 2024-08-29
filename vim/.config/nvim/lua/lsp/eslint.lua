22dcc1e2977a005708f104cb9804815dd0e9e004
undg <und3rdg@gmail.com>
2024-08-29 18:08:06 +0100

    [vim] playing with ai

--- vim/.config/nvim/lua/lsp/eslint.lua
+++ vim/.config/nvim/lua/lsp/eslint.lua
@@ -2,9 +2,11 @@ local configs = require('lspconfig/configs')
 
 return {
     on_attach = function(client, bufnr)
+
+        -- Explanation: Setting `documentFormattingProvider` to false prevents the LSP client from handling document formatting, which is typically handled by ESLint itself.
         client.server_capabilities.documentFormattingProvider = true
-        local function buf_set_option(...)
-            vim.api.nvim_buf_set_option(bufnr, ...)
+        local function buf_set_option(name, value)
+            vim.api.nvim_buf_set_option(bufnr, name, value)
         end
 
         buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
@@ -50,4 +52,29 @@ return {
     --         description = 'Fix all eslint problems for this buffer',
     --     },
     -- },
+    -- commands = {
+    --     EslintFixAll = {
+    --         function()
+    --             vim.lsp.buf.format({ async = false })
+    --         end,
+    --         description = 'Fix all eslint problems for this buffer', -- conflict
+    --     },
+    -- },
+    commands = {
+        EslintFixAll = {
+            function()
+                vim.lsp.buf.format({ async = false })
+            end,
+            description = 'Fix all eslint problems for this buffer',
+        },
+        EslintRunOnBuffer = {
+            function()
+                vim.lsp.buf.execute_command({
+                    command = "_typescript.eslint.executeAutofix",
+                    arguments = { vim.api.nvim_buf_get_name(0) }
+                })
+            end,
+            description = 'Run ESLint on the current buffer',
+        },
+    },
 }
