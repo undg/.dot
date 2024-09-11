894c17a8d55fc200aa128d08180bf3dfea1f5064
undg <und3rdg@gmail.com>
2024-09-11 16:10:38 +0100

    [vim] rearange plugins

--- vim/.config/nvim/lua/plugins/avante.lua
+++ vim/.config/nvim/lua/plugins/avante.lua
@@ -1,5 +1,11 @@
 return {
     'yetone/avante.nvim', -- https://github.com/yetone/avante.nvim
+    dependencies = {
+        'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+        'stevearc/dressing.nvim',
+        'nvim-lua/plenary.nvim',
+        'MunifTanjim/nui.nvim',
+    },
     event = 'VeryLazy',
     build = 'make',       -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
     opts = {
@@ -18,20 +24,6 @@ return {
                 align = 'center', -- left, center, right for title
                 rounded = true,
             },
-        },
-    },
-    dependencies = {
-        'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-        'stevearc/dressing.nvim',
-        'nvim-lua/plenary.nvim',
-        'MunifTanjim/nui.nvim',
-        --- The below is optional, make sure to setup it properly if you have lazy=true
-        {
-            'MeanderingProgrammer/render-markdown.nvim',
-            opts = {
-                file_types = { 'markdown', 'Avante' },
-            },
-            ft = { 'markdown', 'Avante' },
         },
     },
 }
