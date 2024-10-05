df49a52aef27e92795c6bbb526b72b174007034d
undg <und3rdg@gmail.com>
2024-10-05 23:35:56 +0100

    [vim] unrender md in insert mode

--- vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
+++ vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -3,6 +3,22 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
     opts = {
         file_types = { 'markdown', 'Avante' },
+        win_options = {
+            -- See :h 'conceallevel'
+            conceallevel = {
+                -- Used when not being rendered, get user setting
+                default = vim.api.nvim_get_option_value('conceallevel', {}),
+                -- Used when being rendered, concealed text is completely hidden
+                rendered = 3,
+            },
+            -- See :h 'concealcursor'
+            concealcursor = {
+                -- Used when not being rendered, get user setting
+                default = vim.api.nvim_get_option_value('concealcursor', {}),
+                -- Used when being rendered, disable concealing text in all modes
+                rendered = '',
+            },
+        },
         bullet = {
             right_pad = 1,
         },
