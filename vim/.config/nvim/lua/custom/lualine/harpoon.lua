9493a5f3f7dfc24b3ed3532f744a3d54dd3e6dda
undg <und3rdg@gmail.com>
2024-10-23 20:33:54 +0100

    [vim] star current harpoon, rearange lualine

--- vim/.config/nvim/lua/custom/lualine/harpoon.lua
+++ vim/.config/nvim/lua/custom/lualine/harpoon.lua
@@ -1,5 +1,7 @@
 local harpoon_ok, harpoon = pcall(require, 'harpoon')
 local path = require('utils.path')
+local highlight = require('lualine.highlight')
+local color = { norm = {bg = '#228B22'}, curr = {bg = '#C70039'} }
 
 local not_ok = not harpoon_ok and 'harpoon' --
     or false
@@ -9,11 +11,25 @@ if not_ok then
     return
 end
 
+
 return function()
+    local separator = ''
+    local buf_curr = vim.api.nvim_get_current_buf()
+    local buf_name = vim.api.nvim_buf_get_name(buf_curr)
+
     local menu_items = harpoon.get_mark_config().marks ---@diagnostic disable-line: undefined-field
-    local menu_string = ''
+    
+
+
+    local menu_string = separator
+    local data = separator
+
+    local pieces = {}
+
     for _, item in ipairs(menu_items) do
-        menu_string = menu_string .. path.shorten(item.filename) .. ''
+        local is_curr = string.find(buf_name, item.filename)
+        local star = is_curr and '✴' or ''
+        menu_string = menu_string .. star .. path.shorten(item.filename) .. star .. separator
     end
     return menu_string
 end
