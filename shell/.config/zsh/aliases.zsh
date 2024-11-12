daf669813137f61b081371d82469dd9619b77657
undg <und3rdg@gmail.com>
2024-11-12 18:08:57 +0000

    [shell] [shell] install on prompt

--- shell/.config/zsh/aliases.zsh
+++ shell/.config/zsh/aliases.zsh
@@ -1,18 +1,61 @@
 #!/usr/bin/env zsh
 
 
-# Main install function
-
+# main install function
+SKIP_INSTALLS=0
+FIRST_INSTALLS=0
 install() {
-  local package=$1
+	local package=$1
 
-  if is_installed pacman; then
-    install_with_yay $package
-  elif is_installed pkg; then
-    install_with_pkg $package
-  fi
+	if [[ $SKIP_INSTALLS -eq 1 ]]; then
+		echo "Skipping installation of $package"
+		return
+	fi
 
-  exec zsh
+	if [[ $FIRST_INSTALLS -eq 1 ]]; then
+		echo "Package to install: $package"
+		read -q "REPLY?Proceed with installation? (y/n) "
+		echo
+		if [[ $REPLY =~ ^[Yy]$ || $REPLY == "" ]]; then
+			echo "Installation cancelled. Skipping all future installs."
+			SKIP_INSTALLS=1
+			return
+		fi
+	fi
+
+	if is_installed pacman; then
+		install_with_yay $package
+	elif is_installed pkg; then
+		install_with_pkg $package
+	elif is_installed apt; then
+		install_with_apt $package
+	fi
+
+	exec zsh
+}
+
+# helper functions
+is_installed() {
+	hash $1 2>/dev/null
+}
+
+install_with_yay() {
+	echo "Installing $1 with pacman"
+	yay -S $1
+}
+
+install_with_pkg() {
+	if [[ $1 == "xclip" || $1 == "fping" ]]; then
+		return
+	fi
+
+	echo "Installing $1 with pkg"
+	pkg install $1
+}
+
+install_with_apt() {
+	echo "Installing $1 with apt"
+	sudo apt install $1
 }
 
 # Alias definitions
@@ -20,15 +63,15 @@ install() {
 alias :q='exit'
 
 if command -v eza &>/dev/null; then
-    alias l='eza'
-    alias ls='eza --group-directories-first --icons --git'
-    alias ll='ls -lah --git'
-    alias la='eza -a'
-    alias tree='ll --tree'
-    alias treee='ll --tree --level=3'
+	alias l='eza'
+	alias ls='eza --group-directories-first --icons --git'
+	alias ll='ls -lah --git'
+	alias la='eza -a'
+	alias tree='ll --tree'
+	alias treee='ll --tree --level=3'
 else
-    echo "eza is not installed."
-    install eza
+	echo "eza is not installed."
+	install eza
 fi
 
 alias lt="ls --sort newest"
@@ -37,10 +80,10 @@ alias ltr="ls --sort oldest"
 alias lltr="ll --sort oldest"
 
 if command -v fasd &>/dev/null; then
-    alias j="fasd_cd -d -r"
+	alias j="fasd_cd -d -r"
 else
-    echo "fasd is not installed."
-    install fasd
+	echo "fasd is not installed."
+	install fasd
 fi
 
 # Search
@@ -54,100 +97,100 @@ alias duh="du -h | sort -h"
 alias watch="watch --color --interval 0.5 "
 
 if hash nvim 2>/dev/null; then
-    alias vim="nvim"
-    alias v="nvim --nopugin"
-    alias gp="nvim -c ':GpProofread'"
+	alias vim="nvim"
+	alias v="nvim --nopugin"
+	alias gp="nvim -c ':GpProofread'"
 else
-    echo "neovim is missing"
-    install neovim
+	echo "neovim is missing"
+	install neovim
 fi
 
 if hash mise 2>/dev/null; then
-    alias m="mise"
+	alias m="mise"
 else
-    echo "mise is missing"
-    install mise
+	echo "mise is missing"
+	install mise
 fi
 
 if hash fping 2>/dev/null; then
-    alias wping="fping wp.pl -l | cut -d , -f 3-4"
+	alias wping="fping wp.pl -l | cut -d , -f 3-4"
 else
-    echo "fping is missing"
-    install fping
+	echo "fping is missing"
+	install fping
 fi
 
 if hash ranger 2>/dev/null; then
-    alias ranger='ranger --choosedir=$HOME/.rangerdir; cd `cat $HOME/.rangerdir`'
-    alias ra="ranger"
+	alias ranger='ranger --choosedir=$HOME/.rangerdir; cd `cat $HOME/.rangerdir`'
+	alias ra="ranger"
 else
-    echo "ranger is missing"
-    install ranger
+	echo "ranger is missing"
+	install ranger
 fi
 
 if hash trash 2>/dev/null; then
-    alias rmm="trash"
+	alias rmm="trash"
 else
-    echo "trash-cli is not installed."
-    install trash-cli
+	echo "trash-cli is not installed."
+	install trash-cli
 fi
 
 if hash git 2>/dev/null; then
-    # cd to git root
-    alias gcd='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
+	# cd to git root
+	alias gcd='[ ! -z `git rev-parse --show-cdup` ] && cd `git rev-parse --show-cdup || pwd`'
 else
-    echo "git is missing"
-    install git
+	echo "git is missing"
+	install git
 fi
 
 if hash gh 2>/dev/null; then
-    alias gh-dash='gh dash'
-    alias gh-create='gh pr create -t $(git branch --show-current)'
-    alias gh-create-web='gh pr create -w -t $(git branch --show-current)'
-    # alias gh-complete='gh pr merge --auto --delete-branch --squash'
-    # alias gh-complete='gh pr merge --auto --delete-branch --squash -t $(git branch --show-current)'
+	alias gh-dash='gh dash'
+	alias gh-create='gh pr create -t $(git branch --show-current)'
+	alias gh-create-web='gh pr create -w -t $(git branch --show-current)'
+	# alias gh-complete='gh pr merge --auto --delete-branch --squash'
+	# alias gh-complete='gh pr merge --auto --delete-branch --squash -t $(git branch --show-current)'
 
 else
-    echo "gh is missing"
-    install github-cli
+	echo "gh is missing"
+	install github-cli
 fi
 
 if hash lazygit 2>/dev/null; then
-    alias lg='lazygit'
-    alias gg='lazygit'
+	alias lg='lazygit'
+	alias gg='lazygit'
 else
-    echo "lazygit is missing"
-    install lazygit
+	echo "lazygit is missing"
+	install lazygit
 fi
 
 if hash cmatrix 2>/dev/null; then
-    alias s='cmatrix -absCcyan'
+	alias s='cmatrix -absCcyan'
 else
-    echo "cmatrix is missing"
-    install cmatrix
+	echo "cmatrix is missing"
+	install cmatrix
 fi
 
 # translation with http://git.io/trans
 if hash trans 2>/dev/null; then
-    alias t='trans en:pl'
-    alias tj='trans en:pl -j'
-    alias ts='trans en:pl -speak'
-    alias tsj='trans en:pl -speak -j'
-    alias tp='trans pl:en'
-    alias tpj='trans pl:en -j'
-    alias tps='trans pl:en -speak'
-    alias tpsj='trans pl:en -speak -j'
+	alias t='trans en:pl'
+	alias tj='trans en:pl -j'
+	alias ts='trans en:pl -speak'
+	alias tsj='trans en:pl -speak -j'
+	alias tp='trans pl:en'
+	alias tpj='trans pl:en -j'
+	alias tps='trans pl:en -speak'
+	alias tpsj='trans pl:en -speak -j'
 else
-    echo "translate-shell is missing,\r\nget it from http://git.io/trans"
-    install translate-shell
+	echo "translate-shell is missing,\r\nget it from http://git.io/trans"
+	install translate-shell
 fi
 
 if hash ag 2>/dev/null; then
-    alias ag='\ag  --smart-case --pager="less -XFR"'
+	alias ag='\ag  --smart-case --pager="less -XFR"'
 else
-    echo "the_silver_searcher or silversearcher-ag or silver-searcher is not installed."
-    install the_silver_searcher
-    install silversearcher-ag
-    install silver-searcher
+	echo "the_silver_searcher or silversearcher-ag or silver-searcher is not installed."
+	install the_silver_searcher
+	install silversearcher-ag
+	install silver-searcher
 fi
 
 alias y='yarn'
@@ -156,33 +199,33 @@ alias p='pnpm'
 alias vimwiki='nvim -c VimwikiIndex'
 
 if hash xclip 2>/dev/null; then
-    alias xclip='xclip -sel c'
+	alias xclip='xclip -sel c'
 else
-    echo "xclip is not installed."
-    install xclip
+	echo "xclip is not installed."
+	install xclip
 fi
 
 if hash tig 2>/dev/null; then
-    alias tiga='tig --all'
+	alias tiga='tig --all'
 else
-    echo "tig is not installed."
-    install tig
+	echo "tig is not installed."
+	install tig
 fi
 
 if hash tmux 2>/dev/null; then
-    alias tn='tmux new -t'
-    alias ta='tmux new -A -s'
-    alias tl='tmux ls'
+	alias tn='tmux new -t'
+	alias ta='tmux new -A -s'
+	alias tl='tmux ls'
 else
-    echo "tmux is not installed."
-    install tmux
+	echo "tmux is not installed."
+	install tmux
 fi
 
 if hash  googler &>/dev/null; then
-    alias g='googler --np '
+	alias g='googler --np '
 else
-    echo "googler is not installed."
-    install googler
+	echo "googler is not installed."
+	install googler
 fi
 
 alias yay-update='yay -Quq --aur | xargs -n 1 yay -S --noconfirm'
@@ -194,27 +237,8 @@ alias date-today="date +%Y-%m-%d"
 alias date-now="date +%Y-%m-%d_%H-%m_%S"
 
 colors() {
-  for i in {0..255};
-  do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'};
-  done
+	for i in {0..255};
+	do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\n'};
+	done
 }
 
-# Helper functions
-
-is_installed() {
-  hash $1 2>/dev/null
-}
-
-install_with_yay() {
-  echo "Installing $1 with pacman"
-  yay -S $1
-}
-
-install_with_pkg() {
-  if [[ $1 == "xclip" || $1 == "fping" ]]; then
-    return
-  fi
-
-  echo "Installing $1 with pkg"
-  pkg install $1
-}
