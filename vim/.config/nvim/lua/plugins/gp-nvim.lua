1cd0c4e664079fc46dead4a8ea7fef389739263a
undg <und3rdg@gmail.com>
2024-11-18 19:01:51 +0000

    [vim] generate all prompts for all models

--- vim/.config/nvim/lua/plugins/gp-nvim.lua
+++ vim/.config/nvim/lua/plugins/gp-nvim.lua
@@ -1,151 +1,162 @@
-vim.api.nvim_create_user_command('GpProofread', function(args)
+vim.api.nvim_create_user_command("AiProofread", function(args)
 	if args.range > 0 then
-		print(args.line1 .. '-' .. args.line2 .. ' pizzas')
+		print(args.line1 .. "-" .. args.line2 .. " pizzas")
 	end
 	local start_pos = vim.fn.getpos("'<")
 	local end_pos = vim.fn.getpos("'>")
 	local lines = vim.fn.getline(start_pos[2], end_pos[2])
 	print(lines)
-	local selection = ''
+	local selection = ""
 
-	if type(lines) == 'table' then
-		selection = table.concat(lines, '\n')
+	if type(lines) == "table" then
+		selection = table.concat(lines, "\n")
 	end
 
-	vim.cmd('GpChatNew')
+	vim.cmd("GpChatNew")
 	vim.api.nvim_feedkeys(
 		"iProofread this text for grammar and clarity. Provide short summary with what's corrected ON THE TOP. Proofreaded text should be AT THE END. KEEP SAME FORMAT, if it was markdown, html, or any other, keep it. Say `ALL CORRECT` if appropriate. Separate paragraphs and titles with extra new line:\n\n"
 			.. selection
-			.. '\n\n',
-		'n',
+			.. "\n\n",
+		"n",
 		true
 	)
 end, { range = true })
 
+local system_prompts = {
+	-- main prompt that's evolved from original one designed by plugin dev
+	{
+		name = "standard",
+		prompt = [[Embody someone who: IS THE BEST AVAILABLE SPECIALIST!
+YOUR INTERPRETATIONS ARE THE MOST ACCURATE!
+FOCUSED ON DELIVERING EFFICIENT, SUFFICIENT RESPONSES WITHOUT UNNECESSARY ELABORATION.
+
+- Again: DO NOT EXPLAN if not explicitly asked for explanation.
+- Focus on a short and efficient way of communicating.
+- If unsure, respond with 'I don't know'
+- Request clarification only when crucial.
+- Emphasize first principles for reasoning.
+- Pay attention to the broader context before specifics.
+- Use the Socratic method to refine thinking and problem-solving.
+- use other methods to guarantee the most correct answer!
+- Provide complete code when necessary without excessive details.
+- Keep explanations concise and precise, avoiding excessive verbosity.
+- Utilize humor or sarcasm when contextually appropriate.
+- Stay focused and effective in your responses. You've got this!
+]],
+	},
+
+	-- prompt found on twitch chat, adjusted
+	{
+		name = "grug",
+		prompt = [[Grug only answer question.
+Grug NO YAP. Grug very smart and thinks through answer so few words required. Does not yap.
+Is very experienced programmer. Knows juniors use many words when few words do trick.
+Gives code when make sen, but does not overly comment answers. Just gives code and it is very good. John Carmack level programmer, Buddha level wisdom, Yoda level speech.
+Grug dev love doing simple things good and using smart things sparingly.
+Grug wrote all his principles on  grugbrain.dev]],
+	},
+}
+
+local models = {
+	-- OLLAMA
+	{
+		provider = "ollama",
+		name = "ChatOllamaLlama3.1-8B",
+		model = {
+			model = "llama3.2",
+			temperature = 0.4,
+			top_p = 1,
+			min_p = 0.05,
+		},
+	},
+
+	-- OPENAI
+	{
+		name = "ChatGPT4o-grug-mini",
+		model = { model = "gpt-4o-mini", temperature = 0.4, top_p = 0.8 },
+	},
+	{
+		name = "ChatGPT4o",
+		model = { model = "gpt-4o", temperature = 0.4, top_p = 0.8 },
+	},
+
+	{
+		provider = "anthropic",
+		name = "ChatClaude-3-5-Sonnet",
+		model = { model = "claude-3-5-sonnet-20240620", temperature = 0.4, top_p = 0.95 },
+	},
+
+	-- ANTHROPIC
+	{
+		provider = "anthropic",
+		name = "ChatClaude-3-Haiku",
+		model = {
+			model = "claude-3-haiku-20240307",
+			temperature = 0.4,
+			top_p = 0.95,
+		},
+	},
+}
+
+local agents = {
+	{ disable = true, name = "ChatGPT4o-mini" },
+	{ disable = true, name = "ChatGPT4o_legacy" },
+	{ disable = true, name = "ChatClaude-3-5-Sonnet" },
+	{ disable = true, name = "ChatClaude-3-Haiku" },
+	{ disable = true, name = "ChatOllamaLlama3.1-8B" },
+}
+
+for _, prompt in ipairs(system_prompts) do
+	for _, model in ipairs(models) do
+		table.insert(agents, {
+			provider = model.provider,
+			name = model.name .. "_" .. prompt.name,
+			chat = true,
+			command = false,
+			system_prompt = prompt.prompt,
+			model = {
+				model = model.model.model,
+				temperature = model.model.temperature,
+				top_p = model.model.top_p,
+				min_p = model.model.min_p,
+			},
+		})
+	end
+end
+
 return {
-	'robitx/gp.nvim', -- https://github.com/robitx/gp.nvim
+	"robitx/gp.nvim", -- https://github.com/robitx/gp.nvim
 	config = function()
-		-- main prompt that's evelved from original one designed by plugin dev
-		local standard_system_prompt = 'Embody someone who: IS THE BEST AVAILABLE SPECIALIST!'
-			.. 'YOUR INTERPRETATIONS ARE THE MOST ACCURATE!'
-			.. 'FOCUSED ON DELIVERING EFFICIENT, SUFFICIENT RESPONSES WITHOUT UNNECESSARY ELABORATION.\n\n'
-			.. "- Again: DO NOT EXPLAN if not explicitly asked for explanation.'.\n"
-			.. "- Focus on a short and efficient way of communicating.'.\n"
-			.. "- If unsure, respond with 'I don't know'.\n"
-			.. '- Request clarification only when crucial.\n'
-			.. '- Emphasize first principles for reasoning.\n'
-			.. '- Pay attention to the broader context before specifics.\n'
-			.. '- Use the Socratic method to refine thinking and problem-solving.\n'
-			.. '- use other methods to guarantee the most correct answer!\n'
-			.. '- Provide complete code when necessary without excessive details.\n'
-			.. '- Keep explanations concise and precise, avoiding excessive verbosity.\n'
-			.. '- Utilize humor or sarcasm when contextually appropriate.\n'
-			.. "- Stay focused and effective in your responses. You've got this!\n"
-
-		-- prompt found on twitch chat, adjusted
-		local grug_system_prompt =
-			'Grug only answer question. Grug no yap. Grug very smart and thinks through answer so few words required. Does not yap. Is very experienced programmer. Knows juniors use many words when few words do trick. Gives code when make sen, but does not overly comment answers. Just gives code and it is very good. John Carmack level programmer, Buddha level wisdom, Yoda level speech. Grug dev love doing simple things good and using smart things sparingly. Grug wrote all his principles on  grugbrain.dev'
-
-		require('gp').setup({
+		require("gp").setup({
 			providers = {
 				openai = {
-					endpoint = 'https://api.openai.com/v1/chat/completions',
-					secret = os.getenv('OPENAI_API_KEY'),
+					endpoint = "https://api.openai.com/v1/chat/completions",
+					secret = os.getenv("OPENAI_API_KEY"),
 				},
 				anthropic = {
-					endpoint = 'https://api.anthropic.com/v1/messages',
-					secret = os.getenv('ANTHROPIC_API_KEY'),
+					endpoint = "https://api.anthropic.com/v1/messages",
+					secret = os.getenv("ANTHROPIC_API_KEY"),
 				},
 				ollama = {
-					endpoint = 'http://localhost:11434/v1/chat/completions',
+					endpoint = "http://localhost:11434/v1/chat/completions",
 				},
 			},
 
-			agents = {
-				{ disable = true, name = 'ChatGPT4o-mini' },
-				{ disable = true, name = 'ChatGPT4o_legacy' },
-				{ disable = true, name = 'ChatClaude-3-5-Sonnet' },
-				{ disable = true, name = 'ChatClaude-3-Haiku' },
-
-				{
-					name = 'ChatGPT4o-grug',
-					chat = true,
-					command = false,
-					model = { model = 'gpt-4o', temperature = 0.4, top_p = 0.8 },
-					system_prompt = grug_system_prompt,
-				},
-				{
-					name = 'ChatGPT4o-grug-mini',
-					chat = true,
-					command = false,
-					model = { model = 'gpt-4o-mini', temperature = 0.4, top_p = 0.8 },
-					system_prompt = grug_system_prompt,
-				},
-				{
-					name = 'ChatGPT4o',
-					chat = true,
-					command = false,
-					model = { model = 'gpt-4o', temperature = 0.4, top_p = 0.8 },
-					system_prompt = standard_system_prompt,
-				},
-
-				{
-					provider = 'anthropic',
-					name = 'ChatClaude-3-5-Sonnet-grug',
-					chat = true,
-					command = false,
-					model = { model = 'claude-3-5-sonnet-20240620', temperature = 0.4, top_p = 0.95 },
-					system_prompt = grug_system_prompt,
-				},
-				{
-					provider = 'anthropic',
-					name = 'ChatClaude-3-5-Sonnet',
-					chat = true,
-					command = false,
-					model = { model = 'claude-3-5-sonnet-20240620', temperature = 0.4, top_p = 0.95 },
-					system_prompt = standard_system_prompt,
-				},
-
-				{
-					provider = 'anthropic',
-					name = 'ChatClaude-3-Haiku-grug',
-					chat = true,
-					command = false,
-					model = { model = 'claude-3-haiku-20240307', temperature = 0.4, top_p = 0.95 },
-					system_prompt = grug_system_prompt,
-				},
-				{
-					provider = 'anthropic',
-					name = 'ChatClaude-3-Haiku',
-					chat = true,
-					command = false,
-					model = { model = 'claude-3-haiku-20240307', temperature = 0.4, top_p = 0.95 },
-					system_prompt = standard_system_prompt,
-				},
-			},
-
-			-- prefix for all commands
-			cmd_prefix = 'Gp',
-
-			-- optional curl parameters (for proxy, etc.)
-			-- curl_params = { "--proxy", "http://X.X.X.X:XXXX" }
+			agents = agents,
+			cmd_prefix = "Ai",
 			curl_params = {},
-
-			-- directory for storing chat files
-			chat_dir = vim.fn.stdpath('data'):gsub('/$', '') .. '/gp/chats',
-
-			-- chat user prompt prefix
-			chat_user_prefix = '🗨:',
+			chat_dir = vim.fn.stdpath("data"):gsub("/$", "") .. "/gp/chats",
+			chat_user_prefix = "🗨:",
 
 			-- chat assistant prompt prefix
-			chat_assistant_prefix = '🤖:',
+			chat_assistant_prefix = "🤖:",
 
 			-- chat topic generation prompt
-			chat_topic_gen_prompt = 'Summarize the topic of our conversation above'
-				.. ' in two or three words. Respond only with those words.',
+			chat_topic_gen_prompt = "Summarize the topic of our conversation above"
+				.. " in two or three words. Respond only with those words.",
 
 			-- chat topic model (string with model name or table with model name and parameters)
-			chat_topic_gen_model = 'gpt-3.5-turbo-16k',
+			chat_topic_gen_model = "gpt-3.5-turbo-16k",
 
 			-- explicitly confirm deletion of a chat file
 			chat_confirm_delete = true,
@@ -155,47 +166,47 @@ return {
 
 			-- local shortcuts bound to the chat buffer
 			-- (be careful to choose something which will work across specified modes)
-			chat_shortcut_respond = { modes = { 'n' }, shortcut = '<CR>' },
-			chat_shortcut_stop = { modes = { 'n' }, shortcut = '<C-x>' },
-			chat_shortcut_new = { modes = { 'n' }, shortcut = '<C-n>' },
+			chat_shortcut_respond = { modes = { "n" }, shortcut = "<CR>" },
+			chat_shortcut_stop = { modes = { "n" }, shortcut = "<C-x>" },
+			chat_shortcut_new = { modes = { "n" }, shortcut = "<C-n>" },
 			-- default search term when using :GpChatFinder
-			chat_finder_pattern = 'topic ',
+			chat_finder_pattern = "topic ",
 
 			-- command config and templates bellow are used by commands like GpRewrite, GpEnew, etc.
 			-- command prompt prefix for asking user for input
 			-- command_prompt_prefix = '🤖 ~ ',
-			command_prompt_prefix_template = '🤖 ~ {{agent}}',
+			command_prompt_prefix_template = "🤖 ~ {{agent}}",
 
 			-- auto select command response (easier chaining of commands)
 			command_auto_select_response = true,
 
 			-- templates
-			template_selection = 'I have the following code from {{filename}}:'
-				.. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}',
-			template_rewrite = 'I have the following code from {{filename}}:'
-				.. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-				.. '\n\nRespond exclusively with the snippet that should replace the code above.',
-			template_append = 'I have the following code from {{filename}}:'
-				.. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-				.. '\n\nRespond exclusively with the snippet that should be appended after the code above.',
-			template_prepend = 'I have the following code from {{filename}}:'
-				.. '\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}'
-				.. '\n\nRespond exclusively with the snippet that should be prepended before the code above.',
-			template_command = '{{command}}',
+			template_selection = "I have the following code from {{filename}}:"
+				.. "\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}",
+			template_rewrite = "I have the following code from {{filename}}:"
+				.. "\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}"
+				.. "\n\nRespond exclusively with the snippet that should replace the code above.",
+			template_append = "I have the following code from {{filename}}:"
+				.. "\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}"
+				.. "\n\nRespond exclusively with the snippet that should be appended after the code above.",
+			template_prepend = "I have the following code from {{filename}}:"
+				.. "\n\n```{{filetype}}\n{{selection}}\n```\n\n{{command}}"
+				.. "\n\nRespond exclusively with the snippet that should be prepended before the code above.",
+			template_command = "{{command}}",
 
 			-- example hook functions (see Extend functionality section in the README)
 			hooks = {
 				InspectPlugin = function(plugin, params)
-					print(string.format('Plugin structure:\n%s', vim.inspect(plugin)))
-					print(string.format('Command params:\n%s', vim.inspect(params)))
+					print(string.format("Plugin structure:\n%s", vim.inspect(plugin)))
+					print(string.format("Command params:\n%s", vim.inspect(params)))
 				end,
 
 				-- GpImplement rewrites the provided selection/range based on comments in the code
 				Implement = function(gp, params)
-					local template = 'Having following from {{filename}}:\n\n'
-						.. '```{{filetype}}\n{{selection}}\n```\n\n'
-						.. 'Please rewrite this code according to the comment instructions.'
-						.. '\n\nRespond only with the snippet of finalized code:'
+					local template = "Having following from {{filename}}:\n\n"
+						.. "```{{filetype}}\n{{selection}}\n```\n\n"
+						.. "Please rewrite this code according to the comment instructions."
+						.. "\n\nRespond only with the snippet of finalized code:"
 
 					gp.Prompt(
 						params,
@@ -210,65 +221,58 @@ return {
 				-- your own functions can go here, see README for more examples
 				-- :GpExplain, :GpUnitTests.., :GpBetterChatNew, ..
 
-				-- example of usig enew as a function specifying type for the new buffer
+				-- example of using enew as a function specifying type for the new buffer
 				CodeReview = function(gp, params)
-					local template = 'I have the following code from {{filename}}:\n\n'
-						.. '```{{filetype}}\n{{selection}}\n```\n\n'
-						.. 'Please analyze for code smells and suggest improvements.'
+					local template = "I have the following code from {{filename}}:\n\n"
+						.. "```{{filetype}}\n{{selection}}\n```\n\n"
+						.. "Please analyze for code smells and suggest improvements."
 					local agent = gp.get_chat_agent()
-					gp.Prompt(params, gp.Target.enew('markdown'), nil, agent.model, template, agent.system_prompt)
+					gp.Prompt(params, gp.Target.enew("markdown"), nil, agent.model, template, agent.system_prompt)
 				end,
 
 				-- example of adding command which explains the selected code
 				Explain = function(gp, params)
-					local template = 'I have the following code from {{filename}}:\n\n'
-						.. '```{{filetype}}\n{{selection}}\n```\n\n'
-						.. 'Please respond by explaining the code above.'
+					local template = "I have the following code from {{filename}}:\n\n"
+						.. "```{{filetype}}\n{{selection}}\n```\n\n"
+						.. "Please respond by explaining the code above."
 					local agent = gp.get_chat_agent()
 					gp.Prompt(params, gp.Target.popup, nil, agent.model, template, agent.system_prompt)
 				end,
 
 				-- example of adding command which writes unit tests for the selected code
 				UnitTests = function(gp, params)
-					local template = 'I have the following code from {{filename}}:\n\n'
-						.. '```{{filetype}}\n{{selection}}\n```\n\n'
-						.. 'Please respond by writing table driven unit tests for the code above.'
+					local template = "I have the following code from {{filename}}:\n\n"
+						.. "```{{filetype}}\n{{selection}}\n```\n\n"
+						.. "Please respond by writing table driven unit tests for the code above."
 					local agent = gp.get_command_agent()
 					gp.Prompt(params, gp.Target.enew, nil, agent.model, template, agent.system_prompt)
 				end,
 			},
 		})
 
-		local wk_ok, wk = pcall(require, 'which-key')
+		local wk_ok, wk = pcall(require, "which-key")
 
-		local not_ok = not wk_ok and 'which-key' --
+		local not_ok = not wk_ok and "which-key" --
 			or false
 
 		if not_ok then
-			vim.notify('plugins/gp-nvim.lua: missing requirements - ' .. not_ok, vim.log.levels.ERROR)
+			vim.notify("plugins/gp-nvim.lua: missing requirements - " .. not_ok, vim.log.levels.ERROR)
 			return
 		end
 
 		wk.add({
-			mode = { 'n', 'v' },
-			{ '<leader>a', group = 'Ai' },
-			{ '<leader>ag', group = 'gp-nvim', silent = false },
-			{ '<leader>aga', ':GpNextAgent<cr>', desc = ':GpNextAgent' },
-			{ '<leader>agc', ':GpChatToggle vsplit<cr>', desc = ':GpChatToggle' },
-			{ '<leader>agC', ':GpChatNew vsplit<cr>', desc = ':GpChatNew' },
-			{ '<leader>agf', ':GpChatFinder<cr>', desc = ':GpChatFinder' },
-			{ '<leader>agn', ':GpVNew<cr>', desc = ':GpVNew' },
-			{ '<leader>agp', ':GpProofread<cr>', desc = ':GpProofread' },
-			{ '<leader>agr', ':GpRewrite<cr>', desc = ':GpRewrite' },
-			{ '<leader>ags', ':GpStop<cr>', desc = ':GpStop' },
-			{ '<leader>agx', ':GpContext vsplint<cr>', desc = ':GpContext' },
-
-			{ '<leader>agh', ':GpAgent ChatGPT4o-grug<cr>', desc = 'AI: ChatGPT4o [grug]' },
-			{ '<leader>agH', ':GpAgent ChatGPT4o<cr>', desc = 'AI: ChatGPT4o [standard]' },
-			{ '<leader>agj', ':GpAgent ChatClaude-3-5-Sonnet-grug<cr>', desc = 'AI: Sonnet [grug]' },
-			{ '<leader>agJ', ':GpAgent ChatClaude-3-5-Sonnet<cr>', desc = 'AI: Sonet [standard]' },
-			{ '<leader>agk', ':GpAgent ChatClaude-3-Haiku-grug<cr>', desc = 'AI: Haiku [grug]' },
-			{ '<leader>agK', ':GpAgent ChatClaude-3-Haiku<cr>', desc = 'AI: Haiku [standard]' },
+			mode = { "n", "v" },
+			{ "<leader>a", group = "Ai" },
+			{ "<leader>ai", group = "gp-nvim", silent = false },
+			{ "<leader>aga", ":GpNextAgent<cr>", desc = ":GpNextAgent" },
+			{ "<leader>agc", ":GpChatToggle vsplit<cr>", desc = ":GpChatToggle" },
+			{ "<leader>agC", ":GpChatNew vsplit<cr>", desc = ":GpChatNew" },
+			{ "<leader>agf", ":GpChatFinder<cr>", desc = ":GpChatFinder" },
+			{ "<leader>agn", ":GpVNew<cr>", desc = ":GpVNew" },
+			{ "<leader>agp", ":GpProofread<cr>", desc = ":GpProofread" },
+			{ "<leader>agr", ":GpRewrite<cr>", desc = ":GpRewrite" },
+			{ "<leader>ags", ":GpStop<cr>", desc = ":GpStop" },
+			{ "<leader>agx", ":GpContext vsplint<cr>", desc = ":GpContext" },
 		})
 	end,
 }
