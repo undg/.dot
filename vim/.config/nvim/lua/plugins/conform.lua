741287e7031c3edd4b05f8d255c16d3b1079d013
undg <und3rdg@gmail.com>
2024-11-26 22:41:42 +0000

    [vim] prioritise conform over lsp formatter.

--- vim/.config/nvim/lua/plugins/conform.lua
+++ vim/.config/nvim/lua/plugins/conform.lua
@@ -1,20 +1,12 @@
---- Formats buffers using the attached language server clients
---- or using `conform` as a fallback.
-local function format()
-	local buf_clients = vim.lsp.get_clients()
+local function format_on_save_toggle()
+	vim.g.format_on_save = not vim.g.format_on_save
 
-	-- Check LSP clients that support formatting
-	for _, client in pairs(buf_clients) do
-		if client.supports_method("textDocument/formatting") then
-			vim.lsp.buf.format({ async = true })
-			vim.notify_once('format with lsp.')
-			return
-		end
+	local msg = "Format on save = OFF"
+	if vim.g.format_on_save then
+		msg = "Format on save = ON"
 	end
 
-	vim.notify_once('format with conform.')
-	-- Fallback on conform
-	require("conform").format()
+	vim.notify(msg, vim.log.levels.INFO)
 end
 
 return {
@@ -34,19 +26,32 @@ return {
 				reacttypescript = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			},
+			-- Set this to change the default values when calling conform.format()
+			-- This will also affect the default values for format_on_save/format_after_save
+			default_format_opts = {
+				lsp_format = "last",
+			},
+			-- Set the log level. Use `:ConformInfo` to see the location of the log file.
+			log_level = vim.log.levels.ERROR,
+			-- Conform will notify you when a formatter errors
+			notify_on_error = true,
+			-- Conform will notify you when no formatters are available for the buffer
+			notify_no_formatters = true,
 		})
 
-		Keymap.normal("<leader>p", format, { desc = "Format" })
+		local wk_ok, wk = pcall(require, "which-key")
+		local not_ok = not wk_ok and "which-key" --
+			or false
+		if not_ok then
+			vim.notify("plugins/obsidian.lua: missing requirements - " .. not_ok, vim.log.levels.ERROR)
+			return
+		end
 
-		Keymap.normal("<LEADER>P", function()
-			vim.g.format_on_save = not vim.g.format_on_save
+		wk.add({
+			{ "<leader>s", group = "Toggle", slient = false },
+			{ "<leader>sps", format_on_save_toggle, { desc = "Toggle format on save" } },
 
-			local msg = "Format on save = OFF"
-			if vim.g.format_on_save then
-				msg = "Format on save = ON"
-			end
-
-			vim.notify(msg, vim.log.levels.INFO)
-		end, { desc = "Toggle format on save" })
+			{ "<leader>p", require("conform").format , { desc = "Format" } },
+		})
 	end,
 }
