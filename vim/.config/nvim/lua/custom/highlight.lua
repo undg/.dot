1e3e488e87a4b58d2b0cfab780a3712585298ae4
undg <und3rdg@gmail.com>
2024-12-09 19:52:47 +0000

    [vim] highlight multiple lines, https://vimtricks.com/p/highlight-specific-lines/

---
+++ vim/.config/nvim/lua/custom/highlight.lua
@@ -0,0 +1,10 @@
+
+-- highlight setup
+vim.api.nvim_set_hl(0, 'HiglightLInePresent', {  bg = '#ffffff' })
+
+-- mappings
+vim.keymap.set('n', '<Leader>1', function()
+    vim.fn.matchadd('HiglightLInePresent', '\\%' .. vim.fn.line('.') .. 'l')
+end, { silent = true })
+
+vim.keymap.set('n', '<Leader>2', vim.fn.clearmatches, { silent = true })
