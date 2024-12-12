c259e92d1e641ddd374d1e2acbaf5c11b1ad2e3c
undg <und3rdg@gmail.com>
2024-12-12 11:11:26 +0000

    [vim] change provider in avante. Antropic start's to have performance issues

--- vim/.config/nvim/lua/plugins/avante.lua
+++ vim/.config/nvim/lua/plugins/avante.lua
@@ -1,19 +1,20 @@
 return {
-	'yetone/avante.nvim', -- https://github.com/yetone/avante.nvim
+	"yetone/avante.nvim",        -- https://github.com/yetone/avante.nvim
 	dependencies = {
-		'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-		'stevearc/dressing.nvim',
-		'nvim-lua/plenary.nvim',
-		'MunifTanjim/nui.nvim',
+		"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+		"stevearc/dressing.nvim",
+		"nvim-lua/plenary.nvim",
+		"MunifTanjim/nui.nvim",
 	},
-	event = 'VeryLazy',
-	build = 'make', -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
+	event = "VeryLazy",
+	build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
 	opts = {
+		---@diagnostic disable-next-line: duplicate-doc-alias
 		---@alias Provider "openai" | "claude" | "azure"  | "copilot" | "cohere" | [string]
-		provider = 'claude',
+		provider = "openai",
 		claude = {
-			endpoint = 'https://api.anthropic.com',
-			model = 'claude-3-5-sonnet-20240620',
+			endpoint = "https://api.anthropic.com",
+			model = "claude-3-5-sonnet-20240620",
 			temperature = 0,
 			max_tokens = 4096,
 		},
@@ -21,7 +22,7 @@ return {
 			wrap = true, -- similar to vim.o.wrap
 			width = 50, -- default % based on available width
 			sidebar_header = {
-				align = 'center', -- left, center, right for title
+				align = "center", -- left, center, right for title
 				rounded = true,
 			},
 		},
