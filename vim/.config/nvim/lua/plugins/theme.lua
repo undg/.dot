f56b3a5675fa2c2ef9e0a638b843a0f002aa0038
undg <und3rdg@gmail.com>
2024-12-23 20:22:34 +0000

    [vim] Restyle hover popup

--- vim/.config/nvim/lua/plugins/theme.lua
+++ vim/.config/nvim/lua/plugins/theme.lua
@@ -65,11 +65,10 @@ return {
 			border = 'rounded',
 			style = 'minimal',
 			noautocmd = true,
-			height = 3,
-			width = 20,
+			wrap_at = 40,
 		}
-		vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, styled)
 
+		vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, styled)
 		vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, styled)
 
 		vim.diagnostic.config({
