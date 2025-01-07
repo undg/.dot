a52198f8f6100d2f5858d45dece5ec626518b115
undg <und3rdg@gmail.com>
2025-01-07 23:12:32 +0000

    [vim] off by default

--- vim/.config/nvim/lua/plugins/streamer-mode.lua
+++ vim/.config/nvim/lua/plugins/streamer-mode.lua
@@ -1,6 +1,6 @@
 return {
 	"Kolkhis/streamer-mode.nvim", -- https://github.com/Kolkhis/streamer-mode.nvim,
-	config = function()
-		require("streamer-mode").setup({ default_state = "on" })
-	end,
+	-- config = function()
+	-- 	require("streamer-mode").setup({ default_state = "on" })
+	-- end,
 }
