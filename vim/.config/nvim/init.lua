50a55dc333ca6d6aa3dba48f8d18d19ac2e78f0d
undg <und3rdg@gmail.com>
2025-01-09 02:42:09 +0000

    [vim] tidy

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -1,26 +1,26 @@
-require('utils.globals')
+require("utils.globals")
 
 -- Make sure to set `mapleader` before lazy so your mappings are correct.
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-require('plugins')
-require('start-lazy')
+require("plugins")
+require("start-lazy")
 
-require('custom.formater')
-require('custom.indent-toggle')
-require('custom.conceal-toggle')
-require('custom.json2ts')
-require('custom.search-selected')
-require('custom.ultisnip-functions')
-require('custom.pum-completion-up-down')
-require('custom.hover-fix') -- @TODO (undg) 2024-03-20: Check it once a while and try to delete fix. Doc inside file.
-require('custom.highlight')
+require("custom.formater")
+require("custom.indent-toggle")
+require("custom.conceal-toggle")
+require("custom.json2ts")
+require("custom.search-selected")
+require("custom.ultisnip-functions")
+require("custom.pum-completion-up-down")
+require("custom.hover-fix") -- @TODO (undg) 2024-03-20: Check it once a while and try to delete fix. Doc inside file.
+require("custom.highlight")
 
-require('lsp')
+require("lsp")
 
-require('config')
+require("config")
 
-require('keymap')
+require("keymap")
 
-require('autocmd')
+require("autocmd")
