5379846dc432b2718d902dad9a1acb679a3a06fe
undg <und3rdg@gmail.com>
2025-01-09 22:21:41 +0000

    [vim] keymap for window

--- vim/.config/nvim/lua/plugins/windows-nvim.lua
+++ vim/.config/nvim/lua/plugins/windows-nvim.lua
@@ -10,6 +10,24 @@ function M.config()
 	vim.o.winminwidth = 10
 	vim.o.equalalways = false
 	require("windows").setup()
+
+	local ok_wk, wk = pcall(require, "which-key")
+
+	local not_ok = not ok_wk and "wk not ok\n" or false -- all good, not not_ok
+
+	if not_ok then
+		vim.notify("lsp/plugins/windows-nvim.lua: require failed - " .. not_ok, vim.log.levels.ERROR)
+		return
+	end
+
+	wk.add({
+		{ "<leader>w",  group = "Window",                  silent = false },
+		{ "<leader>wt", ":WindowsToggleAutowidth<cr>" },
+		{ "<leader>ww", ":WindowsEqualize<cr>" },
+		{ "<leader>wm", ":WindowsMaximize<cr>" },
+		{ "<leader>wv", ":WindowsMaximizeVertically<cr>" },
+		{ "<leader>ws", ":WindowsMaximizeHorizontally<cr>" },
+	})
 end
 
 return M
