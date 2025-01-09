92fed603af34037c88c94072e8f578fcf7a23113
undg <und3rdg@gmail.com>
2025-01-09 20:11:40 +0000

    [vim] tydy

--- vim/.config/nvim/lua/plugins/windows-nvim.lua
+++ vim/.config/nvim/lua/plugins/windows-nvim.lua
@@ -1,15 +1,15 @@
 local M = {
-	'anuvyklack/windows.nvim',
+	"anuvyklack/windows.nvim", -- https://github.com/anuvyklack/windows.nvim
 	dependencies = {
-		'anuvyklack/middleclass',
-		'anuvyklack/animation.nvim',
+		"anuvyklack/middleclass",
+		"anuvyklack/animation.nvim",
 	},
 }
 function M.config()
 	vim.o.winwidth = 10
 	vim.o.winminwidth = 10
 	vim.o.equalalways = false
-	require('windows').setup()
+	require("windows").setup()
 end
 
 return M
