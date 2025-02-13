899e19f104e8f7a4145576cceef1911e6934a2a7
undg <und3rdg@gmail.com>
2025-02-13 23:56:50 +0000

    [vim] install nvim-lsp-file-operations to auto upadate imports in neo tree

---
+++ vim/.config/nvim/lua/plugins/nvim-lsp-file-operations.lua
@@ -0,0 +1,14 @@
+return {
+
+	"antosha417/nvim-lsp-file-operations", -- https://github.com/antosha417/nvim-lsp-file-operations
+	requires = {
+		"nvim-lua/plenary.nvim",
+		-- Uncomment whichever supported plugin(s) you use
+		-- "nvim-tree/nvim-tree.lua",
+		"nvim-neo-tree/neo-tree.nvim",
+		-- "simonmclean/triptych.nvim"
+	},
+	config = function()
+		require("lsp-file-operations").setup()
+	end,
+}
