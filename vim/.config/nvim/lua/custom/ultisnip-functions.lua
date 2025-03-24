dbf28c8abe17eadfa5dacc025d0fc7412e741ec0
Bartek Laskowski <bartek.laskowski@spglobal.com>
2025-03-24 13:12:28 +0000

    remap kj to jk in ultisnip

--- vim/.config/nvim/lua/custom/ultisnip-functions.lua
+++ vim/.config/nvim/lua/custom/ultisnip-functions.lua
@@ -1,14 +1,20 @@
 -- Function `GetCommentMarker()` is used in ultisnip to auto comment `todo` snippet
 vim.cmd([[
-  function GetCommentMarker()
-  if len(split(&l:commentstring, '%s')) == 1
-  " if 'commentstring' xx%sxx contains no end part
-      return split(&l:commentstring, '%s')[0]
-    elseif match(&l:comments, '\v(,|^):[^,:]*(,|$)')
-      " if 'comments' contains ',:xxx,'
-      return matchstr(&l:comments, '\v(,|^):\zs[^,:]*\ze(,|$)')
-    else
-  echoerr "unable to find line comment marker."
-  endif
-  endfunction
+
+	" Remap jump forward/backward that was upside down
+	let g:UltiSnipsJumpForwardTrigger="<c-k>"
+	let g:UltiSnipsJumpBackwardTrigger="<c-j>"
+
+	function GetCommentMarker()
+		if len(split(&l:commentstring, '%s')) == 1
+			" if 'commentstring' xx%sxx contains no end part
+			return split(&l:commentstring, '%s')[0]
+		elseif match(&l:comments, '\v(,|^):[^,:]*(,|$)')
+			" if 'comments' contains ',:xxx,'
+			return matchstr(&l:comments, '\v(,|^):\zs[^,:]*\ze(,|$)')
+		else
+			echoerr "unable to find line comment marker."
+		endif
+	endfunction
 ]])
+
