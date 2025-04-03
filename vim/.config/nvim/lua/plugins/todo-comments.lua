60d543eb728476b64c9da242e70c045612c286e2
Bartek Laskowski <bartek.laskowski@spglobal.com>
2025-04-03 11:19:08 +0100

    make todo stand out

--- vim/.config/nvim/lua/plugins/todo-comments.lua
+++ vim/.config/nvim/lua/plugins/todo-comments.lua
@@ -5,6 +5,11 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		keywords = {
+			TODO = { icon = " ", color = "warning" },
+			-- 	['@TODO (Bartek Laskowski)'] = { icon = "⚠", color = "error", alt = "no TODO's leftover" },
+			-- 	['@TODO (undg)'] = { icon = "⚠", color = "error", alt = "no TODO's leftover" },
+		},
 		highlight = {
 			pattern = {
 				[[.*<(KEYWORDS)\s*:]],
@@ -13,6 +18,8 @@ return {
 		}
 	}
 }
+-- examples
 -- TODO: with nothing else
 -- TODO (Bartek Laskowski) 2025-04-03: with monkey
 -- @TODO (Bartek Laskowski) 2025-04-03: no monkey here
+-- @TODO (undg) 2025-04-03: no monkey here
