515a60480d93b25db112908a186a6e130384c22b
Bartek Laskowski <bartek.laskowski@spglobal.com>
2025-04-04 11:30:50 +0100

    change todo colors and pattern

--- vim/.config/nvim/lua/plugins/todo-comments.lua
+++ vim/.config/nvim/lua/plugins/todo-comments.lua
@@ -6,14 +6,12 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		keywords = {
-			TODO = { icon = " ", color = "error" },
-			-- 	['@TODO (Bartek Laskowski)'] = { icon = "⚠", color = "error", alt = "no TODO's leftover" },
-			-- 	['@TODO (undg)'] = { icon = "⚠", color = "error", alt = "no TODO's leftover" },
+			-- TODO = { icon = " ", color = "warning" },
 		},
 		highlight = {
 			pattern = {
-				[[.*<(KEYWORDS)\s*:]],
-				[[.*<(KEYWORDS).*:]],
+				[[\s*<(KEYWORDS).*:]],
+				-- [[\s*<(KEYWORDS).*:]],
 			}, -- pattern or table of patterns, used for highlighting (vim regex)
 		}
 	}
