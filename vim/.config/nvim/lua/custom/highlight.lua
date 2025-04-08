954e6adb8ed7c4f904cab339b23766471d4c8719
Bartek Laskowski <bartek.laskowski@spglobal.com>
2025-04-08 12:48:13 +0100

    highlight

--- vim/.config/nvim/lua/custom/highlight.lua
+++ vim/.config/nvim/lua/custom/highlight.lua
@@ -1,9 +1,22 @@
 -- highlight setup
-vim.api.nvim_set_hl(0, "HiglightLInePresent", { bg = "#ffffff" })
+vim.api.nvim_set_hl(0, "HiglightLinePresent", { bg = "#ffffff" })
+
+-- use only in visual
+vim.api.nvim_create_user_command('HiglightLines', function(opts)
+	local lstart = opts.line1
+	local lend = opts.line2
+
+	for line = lstart, lend do
+		vim.fn.matchaddpos("HiglightLinePresent", { line })
+	end
+
+	vim.api.nvim_input("<Esc>")
+end, { range = true }
+)
+
 
 -- mappings
-Keymap.normal("<Leader>1", function()
-	vim.fn.matchadd("HiglightLInePresent", "\\%" .. vim.fn.line(".") .. "l")
-end, { silent = true })
+Keymap.normal("<Leader>1", ":HiglightLines<CR>", { silent = true })
+Keymap.visual("<Leader>1", ":HiglightLines<CR>", { silent = true })
 
 Keymap.normal("<Leader>2", vim.fn.clearmatches, { silent = true })
