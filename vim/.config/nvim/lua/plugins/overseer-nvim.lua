fa2f03d1f2994bf7098e3f51e88851accb0fab57
Bartek Laskowski <bartek.laskowski@spglobal.com>
2025-04-24 13:06:25 +0100

    style overseer

--- vim/.config/nvim/lua/plugins/overseer-nvim.lua
+++ vim/.config/nvim/lua/plugins/overseer-nvim.lua
@@ -12,18 +12,18 @@ return {
 				-- Width dimensions can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
 				-- min_width and max_width can be a single value or a list of mixed integer/float types.
 				-- max_width = {100, 0.2} means "the lesser of 100 columns or 20% of total"
-				max_width = { 20, 0.2 },
+				max_width = { 15, 0.2 },
 				-- min_width = {40, 0.1} means "the greater of 40 columns or 10% of total"
-				min_width = { 10, 0.1 },
+				min_width = { 15, 0.1 },
 				-- optionally define an integer/float for the exact width of the task list
 				width = nil,
-				max_height = { 70, 0.1 },
+				max_height = { 40, 0.1 },
 				min_height = 20,
 				height = nil,
 				-- String that separates tasks
 				separator = "────────────────────────────────────────",
 				-- Default direction. Can be "left", "right", or "bottom"
-				direction = "left",
+				direction = "bottom",
 			},
 		})
 
