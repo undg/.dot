15441898ba708a4f9f636a252bcce7022ae02adc
undg <und3rdg@gmail.com>
2025-05-08 14:56:44 +0100

    center screen after going to mark

--- vim/.config/nvim/lua/custom/only-global-marks.lua
+++ vim/.config/nvim/lua/custom/only-global-marks.lua
@@ -6,8 +6,10 @@ local alphabet = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 
 for _, letter in ipairs(alphabet) do
 	local upper_letter = string.upper(letter)
+	local screen_center = "zz"
+	local screen_top = "zt"
 
 	Keymap.normal("m" .. letter, "m" .. upper_letter)
-	Keymap.normal("'" .. letter, "'" .. upper_letter)
-	Keymap.normal("`" .. letter, "'" .. upper_letter)
+	Keymap.normal("'" .. letter, "'" .. upper_letter .. screen_center)
+	Keymap.normal("`" .. letter, "'" .. upper_letter .. screen_center)
 end
