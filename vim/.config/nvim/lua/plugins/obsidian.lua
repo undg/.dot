8f7794d1d76c0c2e7bf1969c1f18276ffe86c12e
undg <und3rdg@gmail.com>
2025-05-15 12:45:29 +0100

    [vim] wip check if obsidian is installed

--- vim/.config/nvim/lua/plugins/obsidian.lua
+++ vim/.config/nvim/lua/plugins/obsidian.lua
@@ -1,3 +1,19 @@
+local function is_installed(app_name)
+	local os_name = package.config:sub(1, 1)
+	if os_name == "\\" then
+		-- Windows
+		return os.execute("where " .. app_name .. " > nul 2>&1") == 0
+	else
+		-- Unix/Linux/Mac
+		return os.execute("which " .. app_name .. " > /dev/null 2>&1") == 0
+	end
+end
+
+if not is_installed("obsidian") then
+	print("obsidian not installed. SKIP")
+	return {}
+end
+
 return {
 	"epwalsh/obsidian.nvim", -- https://github.com/epwalsh/obsidian.nvim
 	version = "*",
