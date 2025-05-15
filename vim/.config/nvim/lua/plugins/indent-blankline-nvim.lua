b43e041582cc8798b51a4ef9eae98439de3be608
undg <und3rdg@gmail.com>
2025-05-15 16:33:14 +0100

    color deeply nested indentations

--- vim/.config/nvim/lua/plugins/indent-blankline-nvim.lua
+++ vim/.config/nvim/lua/plugins/indent-blankline-nvim.lua
@@ -1,5 +1,5 @@
 local M = {
-	'lukas-reineke/indent-blankline.nvim',
+	'lukas-reineke/indent-blankline.nvim', -- https://github.com/lukas-reineke/indent-blankline.nvim
 	main = 'ibl',
 	opts = {},
 }
@@ -19,6 +19,13 @@ function M.config()
 	-- create the highlight groups in the highlight setup hook, so they are reset
 	-- every time the colorscheme changes
 	hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+		vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
+		vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
+		vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+		vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = '#D19A66' })
+		vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = '#98C379' })
+		vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = '#C678DD' })
+		vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = '#56B6C2' })
 		vim.api.nvim_set_hl(0, 'RainbowRed', { fg = '#E06C75' })
 		vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = '#E5C07B' })
 		vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
