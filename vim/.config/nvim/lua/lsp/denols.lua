07dd8fa93cefb6fd31cc318e596a6a2af54f015f
undg <und3rdg@gmail.com>
2025-05-29 11:56:15 +0100

    helper to check if it's deno project

--- vim/.config/nvim/lua/lsp/denols.lua
+++ vim/.config/nvim/lua/lsp/denols.lua
@@ -1,14 +1,28 @@
 local nvim_lsp = require('lspconfig')
-
 local ok_twoslash, twoslash = pcall(require, 'twoslash-queries')
 if not ok_twoslash then
 	vim.notify('lua/lsp/tsserver.lua: twoslash-queries fail', vim.log.levels.ERROR)
 	return
 end
 
+
+-- Check whether it's a deno project or not
+local function is_deno_project()
+	local deno_files = { 'deno.json', 'deno.jsonc', 'deno.lock' }
+
+	for _, filepath in ipairs(deno_files) do
+		filepath = table.concat({ vim.fn.getcwd(), filepath }, '/')
+
+		if vim.uv.fs_stat(filepath) ~= nil then return true end
+	end
+
+	return false
+end
+
 return {
 	root_dir = nvim_lsp.util.root_pattern('deno.json'),
 	-- autostart = false,
+	enable = is_deno_project(),
 	single_file_support = false,
 	init_options = {
 		lint = true,
