895e24a79f4afa7e9350233fc25a8cad24e491c3
undg <und3rdg@gmail.com>
2025-05-30 10:45:45 +0100

    move keymap.init to init

--- vim/.config/nvim/init.lua
+++ vim/.config/nvim/init.lua
@@ -24,6 +24,12 @@ require("lsp")
 
 require("config")
 
-require("keymap")
+require('keymap.general') -- always first
+require('keymap.lsp')
+require('keymap.spell')
+require('keymap.typescript-nvim')
+require('keymap.tabular')
+require('keymap.fugitive')
+require('keymap.spell-autofixer')
 
 require("autocmd")
