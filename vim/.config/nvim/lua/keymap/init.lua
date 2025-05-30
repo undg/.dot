0cc2301c14a7df2f4e224798f59ed98dfc580bb2
undg <und3rdg@gmail.com>
2025-05-30 10:41:52 +0100

    spell-autofixer

--- vim/.config/nvim/lua/keymap/init.lua
+++ vim/.config/nvim/lua/keymap/init.lua
@@ -7,3 +7,5 @@ require('keymap.spell')
 require('keymap.typescript-nvim')
 require('keymap.tabular')
 require('keymap.fugitive')
+
+require('keymap.spell-autofixer')
---
+++ vim/.config/nvim/lua/keymap/spell-autofixer.lua
@@ -0,0 +1,2 @@
+-- common mistakes to autofix
+Keymap.insert('cosnt ', 'const ', { silent = true })
