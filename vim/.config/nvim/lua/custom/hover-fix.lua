c44c7032e8c21a826045f33f41f397a58393427a
undg <und3rdg@gmail.com>
2025-06-04 16:52:16 +0100

    [vim] tidy

--- vim/.config/nvim/lua/custom/hover-fix.lua
+++ vim/.config/nvim/lua/custom/hover-fix.lua
@@ -5,17 +5,17 @@
 -- https://github.com/neovim/nvim-lspconfig/issues/1931
 
 local function fix_by_custom_hover()
-	local border_ok, border = pcall(require, 'utils.border')
-	local not_ok = not border_ok and 'utils.border' --
+	local border_ok, border = pcall(require, "utils.border")
+	local not_ok = not border_ok and "utils.border" --
 		or false
 
 	if not_ok then
-		vim.notify('plugins/mason.lua: missing required ', not_ok)
+		vim.notify("plugins/mason.lua: missing required ", not_ok)
 	end
 
 	-- Replace hover function with custom implementation
 	-- https://github.com/neovim/neovim/issues/20457#issuecomment-1266782345
-	vim.lsp.handlers['textDocument/hover'] = function(_, result, ctx, config)
+	vim.lsp.handlers["textDocument/hover"] = function(_, result, ctx, config)
 		config = config or { border = border }
 		config.focus_id = ctx.method
 		if not (result and result.contents) then
@@ -26,15 +26,15 @@ local function fix_by_custom_hover()
 		if vim.tbl_isempty(markdown_lines) then
 			return
 		end
-		return vim.lsp.util.open_floating_preview(markdown_lines, 'markdown', config)
+		return vim.lsp.util.open_floating_preview(markdown_lines, "markdown", config)
 	end
 end
 
-local ok_notify, notify = pcall(require, 'notify')
+local ok_notify, notify = pcall(require, "notify")
 
 -- See https://github.com/neovim/nvim-lspconfig/issues/1931#issuecomment-1297599534
 local function fix_by_filter_notify()
-	local banned_messages = { 'No information available' }
+	local banned_messages = { "No information available" }
 	vim.notify = function(msg, ...)
 		for _, banned in ipairs(banned_messages) do
 			if msg == banned then
