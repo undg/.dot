e5332ea86ec4fe2abb7f6005be82343018210526
undg <und3rdg@gmail.com>
2025-06-04 22:42:27 +0100

    [vim] simplify colors, export all methods and data

--- vim/.config/nvim/lua/custom/colors.lua
+++ vim/.config/nvim/lua/custom/colors.lua
@@ -1,173 +1,32 @@
--- #315acd = "SeaShell", HotPink = "#305050", #DCDCDC = "Crimson",
+-- white black
 local M = {}
 
-local colors = {
-	AliceBlue = "#F0F8FF",
-	AntiqueWhite = "#FAEBD7",
-	Aqua = "#00FFFF",
-	Aquamarine = "#7FFFD4",
-	Azure = "#F0FFFF",
-	Beige = "#F5F5DC",
-	Bisque = "#FFE4C4",
-	Black = "#000000",
-	BlanchedAlmond = "#FFEBCD",
-	Blue = "#0000FF",
-	BlueViolet = "#8A2BE2",
-	Brown = "#A52A2A",
-	BurlyWood = "#DEB887",
-	CadetBlue = "#5F9EA0",
-	Chartreuse = "#7FFF00",
-	Chocolate = "#D2691E",
-	Coral = "#FF7F50",
-	CornflowerBlue = "#6495ED",
-	Cornsilk = "#FFF8DC",
-	Crimson = "#DC143C",
-	Cyan = "#00FFFF",
-	DarkBlue = "#00008B",
-	DarkCyan = "#008B8B",
-	DarkGoldenRod = "#B8860B",
-	DarkGray = "#A9A9A9",
-	DarkGrey = "#A9A9A9",
-	DarkGreen = "#006400",
-	DarkKhaki = "#BDB76B",
-	DarkMagenta = "#8B008B",
-	DarkOliveGreen = "#556B2F",
-	DarkOrange = "#FF8C00",
-	DarkOrchid = "#9932CC",
-	DarkRed = "#8B0000",
-	DarkSalmon = "#E9967A",
-	DarkSeaGreen = "#8FBC8F",
-	DarkSlateBlue = "#483D8B",
-	DarkSlateGray = "#2F4F4F",
-	DarkSlateGrey = "#2F4F4F",
-	DarkTurquoise = "#00CED1",
-	DarkViolet = "#9400D3",
-	DeepPink = "#FF1493",
-	DeepSkyBlue = "#00BFFF",
-	DimGray = "#696969",
-	DimGrey = "#696969",
-	DodgerBlue = "#1E90FF",
-	FireBrick = "#B22222",
-	FloralWhite = "#FFFAF0",
-	ForestGreen = "#228B22",
-	Fuchsia = "#FF00FF",
-	Gainsboro = "#DCDCDC",
-	GhostWhite = "#F8F8FF",
-	Gold = "#FFD700",
-	GoldenRod = "#DAA520",
-	Gray = "#808080",
-	Grey = "#808080",
-	Green = "#008000",
-	GreenYellow = "#ADFF2F",
-	HoneyDew = "#F0FFF0",
-	HotPink = "#FF69B4",
-	IndianRed = "#CD5C5C",
-	Indigo = "#4B0082",
-	Ivory = "#FFFFF0",
-	Khaki = "#F0E68C",
-	Lavender = "#E6E6FA",
-	LavenderBlush = "#FFF0F5",
-	LawnGreen = "#7CFC00",
-	LemonChiffon = "#FFFACD",
-	LightBlue = "#ADD8E6",
-	LightCoral = "#F08080",
-	LightCyan = "#E0FFFF",
-	LightGoldenRodYellow = "#FAFAD2",
-	LightGray = "#D3D3D3",
-	LightGrey = "#D3D3D3",
-	LightGreen = "#90EE90",
-	LightPink = "#FFB6C1",
-	LightSalmon = "#FFA07A",
-	LightSeaGreen = "#20B2AA",
-	LightSkyBlue = "#87CEFA",
-	LightSlateGray = "#778899",
-	LightSlateGrey = "#778899",
-	LightSteelBlue = "#B0C4DE",
-	LightYellow = "#FFFFE0",
-	Lime = "#00FF00",
-	LimeGreen = "#32CD32",
-	Linen = "#FAF0E6",
-	Magenta = "#FF00FF",
-	Maroon = "#800000",
-	MediumAquaMarine = "#66CDAA",
-	MediumBlue = "#0000CD",
-	MediumOrchid = "#BA55D3",
-	MediumPurple = "#9370DB",
-	MediumSeaGreen = "#3CB371",
-	MediumSlateBlue = "#7B68EE",
-	MediumSpringGreen = "#00FA9A",
-	MediumTurquoise = "#48D1CC",
-	MediumVioletRed = "#C71585",
-	MidnightBlue = "#191970",
-	MintCream = "#F5FFFA",
-	MistyRose = "#FFE4E1",
-	Moccasin = "#FFE4B5",
-	NavajoWhite = "#FFDEAD",
-	Navy = "#000080",
-	OldLace = "#FDF5E6",
-	Olive = "#808000",
-	OliveDrab = "#6B8E23",
-	Orange = "#FFA500",
-	OrangeRed = "#FF4500",
-	Orchid = "#DA70D6",
-	PaleGoldenRod = "#EEE8AA",
-	PaleGreen = "#98FB98",
-	PaleTurquoise = "#AFEEEE",
-	PaleVioletRed = "#DB7093",
-	PapayaWhip = "#FFEFD5",
-	PeachPuff = "#FFDAB9",
-	Peru = "#CD853F",
-	Pink = "#FFC0CB",
-	Plum = "#DDA0DD",
-	PowderBlue = "#B0E0E6",
-	Purple = "#800080",
-	RebeccaPurple = "#663399",
-	Red = "#FF0000",
-	RosyBrown = "#BC8F8F",
-	RoyalBlue = "#4169E1",
-	SaddleBrown = "#8B4513",
-	Salmon = "#FA8072",
-	SandyBrown = "#F4A460",
-	SeaGreen = "#2E8B57",
-	SeaShell = "#FFF5EE",
-	Sienna = "#A0522D",
-	Silver = "#C0C0C0",
-	SkyBlue = "#87CEEB",
-	SlateBlue = "#6A5ACD",
-	SlateGray = "#708090",
-	SlateGrey = "#708090",
-	Snow = "#FFFAFA",
-	SpringGreen = "#00FF7F",
-	SteelBlue = "#4682B4",
-	Tan = "#D2B48C",
-	Teal = "#008080",
-	Thistle = "#D8BFD8",
-	Tomato = "#FF6347",
-	Turquoise = "#40E0D0",
-	Violet = "#EE82EE",
-	Wheat = "#F5DEB3",
-	White = "#FFFFFF",
-	WhiteSmoke = "#F5F5F5",
-	Yellow = "#FFFF00",
-	YellowGreen = "#9ACD32",
+M.colors = {
+	-- dark
+	"Black",
+	"Navy",
+	"Sienna",
+	"Maroon",
+	"Magenta",
+	"Green",
+	"Red",
+	"Indigo",
+	"Blue",
+	"Olive",
+
+	-- light
+	"White",
+	"Orange",
+	"LimeGreen",
+	"Yellow",
+	"Tomato",
+	"Bisque",
+	"Aqua",
+	"Khaki",
+	"Gray",
 }
 
--- all colors that I want to highlight, including lower case version
-M.toHighlight = {}
-for key, value in pairs(colors) do
-	M.toHighlight[string.lower(key)] = value
-	M.toHighlight[key] = value
-end
-
-local colorsList = {}
-for key, _ in pairs(colors) do
-	table.insert(colorsList, key)
-end
-for _, value in pairs(colors) do
-	table.insert(colorsList, value)
-end
-
-local function getCurrent()
+function M.getCurrent()
 	local word = vim.fn.expand("<cWORD>")
 	word = word:gsub("[\"`'](.*)[\"`']", "%1") -- strip quotes
 	word = word:gsub("[,;()[{}]", "")       -- strip common characters that may be next to cWORD
@@ -175,7 +34,7 @@ local function getCurrent()
 
 	local index
 
-	for i, color in ipairs(colorsList) do
+	for i, color in ipairs(M.colors) do
 		if color:lower() == word:lower() then
 			index = i
 		end
@@ -184,8 +43,8 @@ local function getCurrent()
 	return word, index
 end
 
-local function cycle(direction)
-	local word, currentIndex = getCurrent()
+function M.cycle(direction)
+	local word, currentIndex = M.getCurrent()
 
 	local newColor
 
@@ -194,35 +53,37 @@ local function cycle(direction)
 	end
 
 	if direction == "next" then
-		newColor = colorsList[currentIndex + 1] or colorsList[1]
+		newColor = M.colors[currentIndex + 1] or M.colors[1]
 	else
-		newColor = colorsList[currentIndex - 1] or colorsList[#colorsList]
+		newColor = M.colors[currentIndex - 1] or M.colors[#M.colors]
 	end
 
 	local line = vim.api.nvim_get_current_line()
 	local cursor_col = vim.fn.col(".")
 	local search_pos = vim.fn.searchpos(word, "bcn", vim.fn.line("."))
 	local search_col = search_pos[2]
+	local start_col = search_col - 1
 	local end_col = search_col + #word - 1
 
 	-- Only replace if cursor is within word bounds
-	if cursor_col >= search_col and cursor_col <= end_col then
-		local before = line:sub(1, search_col - 1)
+	if start_col < cursor_col and cursor_col <= end_col then
+		-- if cursor_col >= start_col + 1 and cursor_col <= end_col then
+		local before = line:sub(1, start_col)
 		local after = line:sub(end_col + 1)
 		vim.api.nvim_set_current_line(before .. newColor .. after)
 		-- Move cursor to the beginning or word
-		vim.api.nvim_win_set_cursor(0, { search_pos[1], search_pos[2] - 1 })
+		vim.api.nvim_win_set_cursor(0, { search_pos[1], start_col })
 	end
 end
 
 -- Get next color from colors list
 function M.next()
-	cycle("next")
+	M.cycle("next")
 end
 
 -- Get prev color from colors list
 function M.prev()
-	cycle("prev")
+	M.cycle("prev")
 end
 
 return M
