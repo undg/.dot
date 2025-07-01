e06481ec5485897cac9c174cd3aa5fc1ee2e6c75
undg <bartek@undg.dev>
2025-07-01 12:19:35 +0100

    [vim] fix markdown rendering

--- vim/.config/nvim/lua/custom/conceal-toggle.lua
+++ vim/.config/nvim/lua/custom/conceal-toggle.lua
@@ -1,5 +1,16 @@
+local notify_ok, notify = pcall(require, 'notify')
+
+
+local not_ok = not notify_ok and "notify" --
+	or false
+
+if not_ok then
+	vim.notify("conceal-toggle.lua: requirement's missing - " .. not_ok, vim.log.levels.ERROR)
+end
+
+local renderMarkdown_ok, _ = pcall(require, 'render-markdown')
 local function renderMarkdown(on)
-	if vim.filetype ~= 'markdown' then
+	if vim.bo.filetype ~= 'markdown' or not renderMarkdown_ok then
 		return
 	end
 
@@ -12,31 +23,40 @@ local function renderMarkdown(on)
 	end
 end
 
+local timer_ms = 0
+if renderMarkdown_ok then timer_ms = 100 end
+
 local function toggleConceal()
-	if vim.o.conceallevel == 0 then
-		renderMarkdown(true)
-		vim.fn.timer_start(100, function()
-			vim.o.conceallevel = 3
-		end)
-
-		vim.notify(
-			'Switched to: conceallevel=' .. vim.o.conceallevel .. ' and RenderMarkdown enable',
-			vim.log.levels.INFO,
-			{ title = 'ConcealToggle' }
-		)
-	else
+	notify.dismiss()
+	if vim.o.conceallevel > 0 then
 		renderMarkdown(false)
-		vim.fn.timer_start(100, function()
+		vim.fn.timer_start(timer_ms, function()
 			vim.o.conceallevel = 0
-		end)
 
-		vim.notify(
-			'Switched to: conceallevel=' .. vim.o.conceallevel .. ' and RenderMarkdown disable',
-			vim.log.levels.INFO,
-			{ title = 'ConcealToggle' }
-		)
+			local and_RenderMarkdown = ''
+			if renderMarkdown_ok then and_RenderMarkdown = ' and RenderMarkdown disable' end
+
+			vim.notify(
+				'Switched to: conceallevel=' .. vim.o.conceallevel .. and_RenderMarkdown,
+				vim.log.levels.INFO,
+				{ title = 'ConcealToggle' }
+			)
+		end)
+	else
+		renderMarkdown(true)
+		vim.fn.timer_start(timer_ms, function()
+			vim.o.conceallevel = 3
+
+			local and_RenderMarkdown = ''
+			if renderMarkdown_ok then and_RenderMarkdown = ' and RenderMarkdown enable' end
+
+			vim.notify(
+				'Switched to: conceallevel=' .. vim.o.conceallevel .. and_RenderMarkdown,
+				vim.log.levels.INFO,
+				{ title = 'ConcealToggle' }
+			)
+		end)
 	end
-	print(vim.o.conceallevel)
 end
 
 vim.api.nvim_create_user_command('ConcealToggle', toggleConceal, { desc = 'Toggle render markdown' })
