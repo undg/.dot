cdb7b063d79f44ff5f456723fb92e82c9ba7c00d
undg <bartek@undg.dev>
2025-07-23 22:40:58 +0100

    [vim] improve breadcrumb navigation

--- vim/.config/nvim/lua/plugins/nvim-navic.lua
+++ vim/.config/nvim/lua/plugins/nvim-navic.lua
@@ -1,18 +1,34 @@
 return {
-	'SmiteshP/nvim-navic', -- https://github.com/SmiteshP/nvim-navic
-	dependencies = {
-		'neovim/nvim-lspconfig',
+	{
+		'SmiteshP/nvim-navic', -- https://github.com/SmiteshP/nvim-navic
+		dependencies = {
+			'neovim/nvim-lspconfig',
+		},
+
+		config = function()
+			local navic = require('nvim-navic')
+			navic.setup({
+				lsp = {
+					auto_attach = true,
+				},
+				highlight = true,
+			})
+
+			vim.o.winbar = "  %f > %{%v:lua.require'nvim-navic'.get_location()%}"
+
+			vim.api.nvim_set_hl(0, "WinBar", { bg = "#3c3836", fg = "#d3869b" })
+		end,
 	},
-
-	config = function()
-		local navic = require('nvim-navic')
-		navic.setup({
-			lsp = {
-				auto_attach = true,
-			},
-			highlight = true,
-		})
-
-		vim.o.winbar = "  %f > %{%v:lua.require'nvim-navic'.get_location()%}"
-	end,
+	{
+		"SmiteshP/nvim-navbuddy", -- https://github.com/SmiteshP/nvim-navbuddy
+		dependencies = {
+			'neovim/nvim-lspconfig',
+			"SmiteshP/nvim-navic",
+			"MunifTanjim/nui.nvim"
+		},
+		config = function()
+			require('nvim-navbuddy').setup({ lsp = { auto_attach = true } })
+			Keymap.normal("<leader>fn", ":Navbuddy<cr>", { desc = "Navbuddy" })
+		end
+	}
 }
