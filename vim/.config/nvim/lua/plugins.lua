a46a2e0240761fb867932d2e679930a1d80d82a5
undg <bartek@undg.dev>
2025-07-25 17:27:06 +0100

    [vim] remove ts error translator and improve pretty-ts-errors-markdown

--- vim/.config/nvim/lua/plugins.lua
+++ vim/.config/nvim/lua/plugins.lua
@@ -32,18 +32,18 @@ Spec("plugins.harpoon")                  -- bookmark like buffer storage
 Spec("plugins.nvim-lsp-file-operations") -- adds support for file operations using built-in LSP (update imports)
 
 -- LSP, code and linting
-Spec("plugins.mason")                            -- quickly install any lsp server
-Spec("plugins.lspsaga")                          -- Improves the Neovim built-in LSP experience.lspsaga.lua
-Spec("plugins.typescript")                       -- LSP Typescript utils with performant UI
-Spec("plugins.golang")                           -- LSP golang utils with performant UI
-Spec("plugins.treesitter")                       -- syntax highlight
-Spec("plugins.conform")                          -- better formatting
-Spec("plugins.trouble")                          -- organize errors and warnings
-Spec("plugins.nvim-cmp")                         -- collection of plugins related with autocompletion
-Spec("plugins.overseer-nvim")                    -- A task runner and job management plugin for Neovim
+Spec("plugins.mason")                     -- quickly install any lsp server
+Spec("plugins.lspsaga")                   -- Improves the Neovim built-in LSP experience.lspsaga.lua
+Spec("plugins.typescript")                -- LSP Typescript utils with performant UI
+Spec("plugins.golang")                    -- LSP golang utils with performant UI
+Spec("plugins.treesitter")                -- syntax highlight
+Spec("plugins.conform")                   -- better formatting
+Spec("plugins.trouble")                   -- organize errors and warnings
+Spec("plugins.nvim-cmp")                  -- collection of plugins related with autocompletion
+Spec("plugins.overseer-nvim")             -- A task runner and job management plugin for Neovim
+Spec("plugins.pretty-ts-errors-markdown") -- better formatted TypeScript errors
 -- Spec("plugins.better-ts-errors")                 -- better formatted TypeScript errors
-Spec("plugins.ts-error-translator")              -- better formatted TypeScript errors
-Spec("plugins.pretty-ts-errors-markdown")        -- better formatted TypeScript errors
+-- Spec("plugins.ts-error-translator")              -- better formatted TypeScript errors
 -- Git("https://github.com/cseickel/diagnostic-window.nvim")
 Git("https://github.com/folke/neodev.nvim")      -- Neovim full signature help, docs and completion for the nvim lua API.
 -- Spec("plugins.hover")                            -- General framework for context aware hover providers
--- vim/.config/nvim/lua/plugins/pretty-ts-errors-markdown.lua
+++ vim/.config/nvim/lua/plugins/pretty-ts-errors-markdown.lua
@@ -3,27 +3,38 @@ return {
 		"youyoumu/pretty-ts-errors.nvim", -- https://github.com/youyoumu/pretty-ts-errors.nvim
 
 		config = function()
-			local ts_errors = require 'pretty-ts-errors'
+			local ts_errors_ok, ts_errors = pcall(require, 'pretty-ts-errors')
+
+			local window_ok, window = pcall(require, 'utils.window')
+
+			local not_ok = not window_ok and "utils.window"
+				or not ts_errors_ok and 'pretty-ts-errors'
+				or false
+
+			if not_ok then
+				vim.notify("plugins/pretty-ts-errors-markdown.lua: SKIP, requirement's missing - " .. not_ok,
+					vim.log.levels.ERROR)
+				return
+			end
 
 			ts_errors.setup({
-				{
-					-- Install server with
-					-- npm i -g pretty-ts-errors-markdown
-					executable = "pretty-ts-errors-markdown", -- Path to the executable
-					float_opts = {
-						border = "rounded",    -- Border style for floating windows
-						max_width = 80,        -- Maximum width of floating windows
-						max_height = 20,       -- Maximum height of floating windows
-						wrap = true,           -- Whether to wrap long lines
-					},
-					auto_open = false,         -- Automatically show errors on hover
-				}
+				-- Install server with
+				-- npm i -g pretty-ts-errors-markdown
+				executable = "pretty-ts-errors-markdown", -- Path to the executable
+				float_opts = {
+					border = "rounded",                -- Border style for floating windows
+					max_width = window.width() - 10,   -- Maximum width of floating windowsutilsutils
+					max_height = math.floor(window.height() * 3 / 4), -- Maximum height of floating windows
+					wrap = false,                      -- Whether to wrap long lines
+				},
+				auto_open = false,                     -- Automatically show errors on hover
 			})
 
 			Keymap.normal("<leader>xs", ts_errors.open_all_errors, { desc = "(pretty-ts-errors) open in split" })
 			Keymap.normal("<leader>xt", ts_errors.toggle_auto_open, { desc = "(pretty-ts-errors) toggle auto open" })
 			Keymap.normal("<leader>xx", ts_errors.show_formatted_error,
 				{ desc = "(pretty-ts-errors) show formatted error" })
+			Keymap.normal("<c-x>", ts_errors.show_formatted_error, { desc = "(pretty-ts-errors) show formatted error" })
 		end
 	},
 }
