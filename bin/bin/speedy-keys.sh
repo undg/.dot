8f8b4aec33ac6277fc0e4169781e4b8a324836db
undg <und3rdg@gmail.com>
2025-10-13 13:15:23 +0100

    [bin] speedy keys to fix macOS

--- bin/bin/speedy-keys.sh
+++ bin/bin/speedy-keys.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env sh
 
-#         delay repeatSpeed
-xset r rate 200 100
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # macOS
+    defaults write -g InitialKeyRepeat -int 10  # ~150ms delay
+    defaults write -g KeyRepeat -int 1          # very fast repeat
+elif command -v xset >/dev/null 2>&1; then
+    # Linux/X11
+    xset r rate 200 100
+else
+    echo "Unsupported system"
+fi
