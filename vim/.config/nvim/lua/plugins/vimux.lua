471dc2fa8ec342ba85a27840bb36d5a3ac92f57a
undg <und3rdg@gmail.com>
2025-10-17 21:16:00 +0100

    [vim] small cleanup and <leader>vl run current line in pane

--- vim/.config/nvim/lua/plugins/vimux.lua
+++ vim/.config/nvim/lua/plugins/vimux.lua
@@ -1,42 +1,48 @@
 return {
-	'benmills/vimux', -- https://github.com/benmills/vimux
+	"benmills/vimux", -- https://github.com/benmills/vimux
 	-- cmd = { 'VimuxPromptCommand', 'VimuxRunCommand' },
 	init = function()
 		vim.g.VimuxDebug = false
 		vim.g.VimuxHeight = 40
-		vim.g.VimuxOpenExtraArgs = ''
-		vim.g.VimuxOrientation = 'h'
-		vim.g.VimuxPromptString = 'Run? '
-		vim.g.VimuxResetSequence = 'q C-u'
-		vim.g.VimuxRunnerName = ''
-		vim.g.VimuxRunnerType = 'pane'
+		vim.g.VimuxOpenExtraArgs = ""
+		vim.g.VimuxOrientation = "h"
+		vim.g.VimuxPromptString = "Run? "
+		vim.g.VimuxResetSequence = "q C-u"
+		vim.g.VimuxRunnerName = ""
+		vim.g.VimuxRunnerType = "pane"
 		vim.g.VimuxRunnerQuery = {}
-		vim.g.VimuxTmuxCommand = 'tmux'
+		vim.g.VimuxTmuxCommand = "tmux"
 		vim.g.VimuxUseNearest = true
 		vim.g.VimuxExpandCommand = false
 		vim.g.VimuxCloseOnExit = false
 		vim.g.VimuxCommandShell = false -- enable shell completion (disable arrow up)
 
 		-- Run...
-		Keymap.normal('<Leader>vv', ':call VimuxRunCommand("./" . bufname("%"))<CR>')
-		Keymap.normal('<Leader>vt', ':call VimuxRunCommand("ts-node " . bufname("%"))<CR>')
-		Keymap.normal('<Leader>vn', ':call VimuxRunCommand("node " . bufname("%"))<CR>')
-		Keymap.normal('<Leader>vd', ':call VimuxRunCommand("deno run --allow-all " . bufname("%"))<CR>')
-		Keymap.normal('<Leader>vg', ':call VimuxRunCommand("go run " . bufname("%"))<CR>')
+		Keymap.normal("<Leader>vv", ':call VimuxRunCommand("./" . bufname("%"))<CR>')
+		Keymap.normal("<Leader>vt", ':call VimuxRunCommand("ts-node " . bufname("%"))<CR>')
+		Keymap.normal("<Leader>vn", ':call VimuxRunCommand("node " . bufname("%"))<CR>')
+		Keymap.normal("<Leader>vd", ':call VimuxRunCommand("deno run --allow-all " . bufname("%"))<CR>')
+		Keymap.normal("<Leader>vg", ':call VimuxRunCommand("go run " . bufname("%"))<CR>')
 
 		-- Prompt for a command to run
-		Keymap.normal('<Leader>vp', ':VimuxPromptCommand<CR>')
+		Keymap.normal("<Leader>vp", ":VimuxPromptCommand<CR>")
 
 		-- Interrupt any command running in the runner pane
-		Keymap.normal('<Leader>vc', ':VimuxInterruptRunner<CR>')
+		Keymap.normal("<Leader>vc", ":VimuxInterruptRunner<CR>")
 
 		-- Run last command executed by VimuxRunCommand
-		Keymap.normal('<Leader>vl', ':VimuxRunLastCommand<CR>')
+		-- Keymap.normal("<Leader>vl", ":VimuxRunLastCommand<CR>")
 
 		-- !RESTART LAST COMMAND
 		Keymap.normal(
-			'<Leader>vr',
-			':call VimuxInterruptRunner() <bar> :call VimuxInterruptRunner() <bar> :call VimuxRunLastCommand() <cr><cr>'
+			"<Leader>vr",
+			":call VimuxInterruptRunner() <bar> :call VimuxInterruptRunner() <bar> :call VimuxRunLastCommand() <cr>"
 		)
+
+		-- Send selected text to tmux
+		Keymap.normal("<Leader>vl", function()
+			local line = vim.api.nvim_get_current_line():gsub("^%s+", "")
+			vim.fn.VimuxRunCommand(line)
+		end)
 	end,
 }
