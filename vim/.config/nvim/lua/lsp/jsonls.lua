05d22e962de81f5cd8ebe7abc8d2671b717e375d
undg <und3rdg@gmail.com>
2025-11-11 00:54:17 +0000

    [vim] schema validation with the $schema url

--- vim/.config/nvim/lua/lsp/jsonls.lua
+++ vim/.config/nvim/lua/lsp/jsonls.lua
@@ -1,46 +1,50 @@
 return {
 	settings = {
 		json = {
+			-- Validate JSON files using the $schema key when present
+			validate = { enable = true },
+			-- Format settings
+			format = { enable = true },
 			schemas = {
 				{
-					description = 'TypeScript compiler configuration file',
-					fileMatch = { 'tsconfig.json', 'tsconfig.*.json' },
-					url = 'http://json.schemastore.org/tsconfig',
+					description = "TypeScript compiler configuration file",
+					fileMatch = { "tsconfig.json", "tsconfig.*.json" },
+					url = "http://json.schemastore.org/tsconfig",
 				},
 				{
-					description = 'Lerna config',
-					fileMatch = { 'lerna.json' },
-					url = 'http://json.schemastore.org/lerna',
+					description = "Lerna config",
+					fileMatch = { "lerna.json" },
+					url = "http://json.schemastore.org/lerna",
 				},
 				{
-					description = 'Babel configuration',
-					fileMatch = { '.babelrc.json', '.babelrc', 'babel.config.json' },
-					url = 'http://json.schemastore.org/lerna',
+					description = "Babel configuration",
+					fileMatch = { ".babelrc.json", ".babelrc", "babel.config.json" },
+					url = "http://json.schemastore.org/lerna",
 				},
 				{
-					description = 'ESLint config',
-					fileMatch = { '.eslintrc.json', '.eslintrc' },
-					url = 'http://json.schemastore.org/eslintrc',
+					description = "ESLint config",
+					fileMatch = { ".eslintrc.json", ".eslintrc" },
+					url = "http://json.schemastore.org/eslintrc",
 				},
 				{
-					description = 'Bucklescript config',
-					fileMatch = { 'bsconfig.json' },
-					url = 'https://bucklescript.github.io/bucklescript/docson/build-schema.json',
+					description = "Bucklescript config",
+					fileMatch = { "bsconfig.json" },
+					url = "https://bucklescript.github.io/bucklescript/docson/build-schema.json",
 				},
 				{
-					description = 'Prettier config',
-					fileMatch = { '.prettierrc', '.prettierrc.json', 'prettier.config.json' },
-					url = 'http://json.schemastore.org/prettierrc',
+					description = "Prettier config",
+					fileMatch = { ".prettierrc", ".prettierrc.json", "prettier.config.json" },
+					url = "http://json.schemastore.org/prettierrc",
 				},
 				{
-					description = 'Vercel Now config',
-					fileMatch = { 'now.json' },
-					url = 'http://json.schemastore.org/now',
+					description = "Vercel Now config",
+					fileMatch = { "now.json" },
+					url = "http://json.schemastore.org/now",
 				},
 				{
-					description = 'Stylelint config',
-					fileMatch = { '.stylelintrc', '.stylelintrc.json', 'stylelint.config.json' },
-					url = 'http://json.schemastore.org/stylelintrc',
+					description = "Stylelint config",
+					fileMatch = { ".stylelintrc", ".stylelintrc.json", "stylelint.config.json" },
+					url = "http://json.schemastore.org/stylelintrc",
 				},
 			},
 		},
