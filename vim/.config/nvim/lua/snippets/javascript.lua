47130d7bd16840dc9e9e8fff679dde677f854792
undg <und3rdg@gmail.com>
2025-11-18 11:45:39 +0000

    [vim] [vim] no aliases in lua snip

--- vim/.config/nvim/lua/snippets/javascript.lua
+++ vim/.config/nvim/lua/snippets/javascript.lua
@@ -4,10 +4,6 @@ local t = ls.text_node
 local i = ls.insert_node
 local rep = require("luasnip.extras").rep
 
-local vitest_content = {
-	t({ 'import { describe, expect, it } from "vitest"', "", 'describe(`' }), i(1), t({ '`, () => {', '  it(`' }),
-	i(2), t({ '`, () => {', '    expect(true).toBe(false)', '  })', '})' })
-}
 
 -- stylua: ignore
 return {
@@ -44,7 +40,10 @@ return {
 	s("forof", {
 		t("for(let "), i(1, "value"), t(" = 0 of "), i(2, "list"), t(") {"), t({ "", "\t" }), i(2), t({ "", "}" })
 	}),
-	s("vitestnew", vitest_content),
-	s("newvitest", vitest_content),
-	s("vitest", vitest_content),
+	s("vitestinit",
+		{
+			t({ 'import { describe, expect, it } from "vitest"', "", 'describe(`' }), i(1), t({ '`, () => {', '  it(`' }),
+			i(2), t({ '`, () => {', '    expect(true).toBe(false)', '  })', '})' })
+		}
+	),
 }
