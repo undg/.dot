b4a42ae41ff731330d384125f2c9b0839c7e662d
undg <und3rdg@gmail.com>
2025-12-02 22:32:27 +0000

    [vim] testing new config for md renderer

--- vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
+++ vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -1,33 +1,49 @@
 return {
-	'MeanderingProgrammer/render-markdown.nvim', -- https://github.com/MeanderingProgrammer/render-markdown.nvim
-	dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	"MeanderingProgrammer/render-markdown.nvim",                                      -- https://github.com/MeanderingProgrammer/render-markdown.nvim
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
 	opts = {
-		file_types = { 'markdown', 'Avante' },
-		win_options = {
-			-- See :h 'conceallevel'
-			conceallevel = {
-				-- Used when not being rendered, get user setting
-				default = vim.api.nvim_get_option_value('conceallevel', {}),
-				-- Used when being rendered, concealed text is completely hidden
-				rendered = 3,
-			},
-			-- See :h 'concealcursor'
-			concealcursor = {
-				-- Used when not being rendered, get user setting
-				default = vim.api.nvim_get_option_value('concealcursor', {}),
-				-- Used when being rendered, disable concealing text in all modes
-				rendered = '',
-			},
+		file_types = { "markdown", "Avante" },
+		-- new conf START
+		render_modes = { --
+			"n",
+			-- "i",
+			"v",
+			"V",
+			"c",
+			"t",
 		},
+		anti_conceal = { enabled = false },
+		win_options = {
+			concealcursor = { rendered = "n" },
+		},
+		-- new conf END
+
+		--  @TODO (undg) 2025-12-02: delete old config if new one will survive test of time
+		-- win_options = {
+		-- 	-- See :h 'conceallevel'
+		-- 	conceallevel = {
+		-- 		-- Used when not being rendered, get user setting
+		-- 		default = vim.api.nvim_get_option_value("conceallevel", {}),
+		-- 		-- Used when being rendered, concealed text is completely hidden
+		-- 		rendered = 3,
+		-- 	},
+		-- 	-- See :h 'concealcursor'
+		-- 	concealcursor = {
+		-- 		-- Used when not being rendered, get user setting
+		-- 		default = vim.api.nvim_get_option_value("concealcursor", {}),
+		-- 		-- Used when being rendered, disable concealing text in all modes
+		-- 		rendered = "",
+		-- 	},
+		-- },
 		bullet = {
 			right_pad = 1,
 		},
 		code = {
-			width = 'block',
+			width = "block",
 			right_pad = 4,
 			min_width = 10,
 		},
 	},
 
-	ft = { 'markdown', 'Avante' },
+	ft = { "markdown" },
 }
