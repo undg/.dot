e9ba3ebfcb42016c8b1cf8d34e164c0adea91d95
undg <und3rdg@gmail.com>
2025-12-28 13:57:26 +0000

    [vim] remove padding in markdown toc

--- vim/.config/nvim/lua/plugins/markdown-toc-nvim.lua
+++ vim/.config/nvim/lua/plugins/markdown-toc-nvim.lua
@@ -3,6 +3,9 @@ return {
 	ft = "markdown",          -- Lazy load on markdown filetype
 	cmd = { "Mtoc" },         -- Or, lazy load on "Mtoc" command
 	opts = {
+		toc_list = {
+			padding_lines = 0,
+		},
 		-- Your configuration here (optional)
 	},
 }
