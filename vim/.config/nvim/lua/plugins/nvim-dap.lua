5087a99f0a94293119fe5df4457a1a16cfbc68e5
undg <und3rdg@gmail.com>
2026-01-12 12:41:17 +0000

    [vim] react dap

--- vim/.config/nvim/lua/plugins/nvim-dap.lua
+++ vim/.config/nvim/lua/plugins/nvim-dap.lua
@@ -7,24 +7,163 @@ return {
 		config = function()
 			local dap = require("dap")
 
-			-- require("dap-vscode-js").setup({
-			-- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-			-- debugger_path = "(runtimedir)/site/pack/packer/opt/vscode-js-debug",                                         -- Path to vscode-js-debug installation.
-			-- debugger_cmd = { "extension" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
-			-- adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
-			-- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-			-- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-			-- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
-			-- })
+			local function get_workspace_root()
+				local package_json = vim.fs.find({ "package.json" }, { upward = true, type = "file" })[1]
+				if package_json then
+					return vim.fs.dirname(package_json)
+				end
 
-			Keymap.normal('<leader>dc', require 'dap'.continue)
+				local git_dir = vim.fs.find({ ".git" }, { upward = true, type = "directory" })[1]
+				if git_dir then
+					return vim.fs.dirname(git_dir)
+				end
+
+				return vim.loop.cwd()
+			end
+
+			local function setup_js_debug_adapters()
+				local function configure_from_path(js_debug_path)
+					dap.adapters["pwa-node"] = {
+						type = "server",
+						host = "localhost",
+						port = "${port}",
+						executable = {
+							command = "node",
+							args = { js_debug_path, "${port}" },
+						},
+					}
+
+					dap.adapters["node-terminal"] = dap.adapters["pwa-node"]
+					dap.adapters["pwa-chrome"] = dap.adapters["pwa-node"]
+
+					local skip_files = { "<node_internals>/**", "**/node_modules/**" }
+
+					local function make_web_launch(name, url)
+						return {
+							type = "pwa-chrome",
+							request = "launch",
+							name = name,
+							url = url,
+							webRoot = get_workspace_root,
+							skipFiles = skip_files,
+							sourceMaps = true,
+						}
+					end
+
+					local base_configs = {
+						{
+							type = "pwa-node",
+							request = "launch",
+							name = "Launch file",
+							program = "${file}",
+							cwd = get_workspace_root,
+							runtimeExecutable = "node",
+							sourceMaps = true,
+							skipFiles = skip_files,
+						},
+						{
+							type = "pwa-node",
+							request = "attach",
+							name = "Attach 9229",
+							processId = require("dap.utils").pick_process,
+							cwd = get_workspace_root,
+							sourceMaps = true,
+							port = 9229,
+							skipFiles = skip_files,
+						},
+						make_web_launch("Launch Chrome: 3000", "http://localhost:3000"),
+						make_web_launch("Launch Chrome: 5173", "http://localhost:5173"),
+					}
+
+					for _, language in ipairs({ "javascript", "typescript", "javascriptreact", "typescriptreact" }) do
+						dap.configurations[language] = vim.deepcopy(base_configs)
+					end
+				end
+
+				local function resolve_from_mason()
+					local ok, registry = pcall(require, "mason-registry")
+					if not ok then
+						return
+					end
+
+					local function configure()
+						local ok_pkg, package = pcall(registry.get_package, "js-debug-adapter")
+						if not ok_pkg or not package then
+							return
+						end
+
+						if not package.is_installed or not package:is_installed() then
+							if package.once then
+								package:once("install:success", function()
+									if package.get_install_path then
+										configure_from_path(package:get_install_path() .. "/js-debug/src/dapDebugServer.js")
+									end
+								end)
+							elseif package.on then
+								package:on("install:success", function()
+									if package.get_install_path then
+										configure_from_path(package:get_install_path() .. "/js-debug/src/dapDebugServer.js")
+									end
+								end)
+							end
+
+							if package.install then
+								package:install()
+							end
+
+							return
+						end
+
+						if type(package.get_install_path) ~= "function" then
+							return
+						end
+
+						configure_from_path(package:get_install_path() .. "/js-debug/src/dapDebugServer.js")
+					end
+
+					if registry.refresh then
+						registry.refresh(configure)
+					else
+						configure()
+					end
+
+					if registry.is_installed and registry.is_installed("js-debug-adapter") then
+						configure()
+					end
+				end
+
+				local function resolve_direct_path()
+					local mason_path = vim.fn.stdpath("data") .. "/mason/packages/js-debug-adapter/js-debug/src/dapDebugServer.js"
+					local stat = nil
+					if vim.uv and vim.uv.fs_stat then
+						stat = vim.uv.fs_stat(mason_path)
+					elseif vim.loop and vim.loop.fs_stat then
+						stat = vim.loop.fs_stat(mason_path)
+					end
+
+					if stat then
+						return mason_path
+					end
+				end
+
+				local direct_path = resolve_direct_path()
+				if direct_path then
+					configure_from_path(direct_path)
+				else
+					resolve_from_mason()
+				end
+			end
+
+			setup_js_debug_adapters()
+
+			Keymap.normal("<leader>dc", require("dap").continue)
 			Keymap.normal("<leader>dC", function() require("dap").run_to_cursor() end, { desc = "Run to Cursor" })
-			Keymap.normal('<leader>do', require 'dap'.step_over)
-			Keymap.normal('<leader>di', require 'dap'.step_into)
-			Keymap.normal('<leader>dO', require 'dap'.step_out)
-			Keymap.normal('<leader>db', require 'dap'.toggle_breakpoint)
-			Keymap.normal('<leader>dB',
-				function() require 'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: ')) end)
+			Keymap.normal("<leader>do", require("dap").step_over)
+			Keymap.normal("<leader>di", require("dap").step_into)
+			Keymap.normal("<leader>dO", require("dap").step_out)
+			Keymap.normal("<leader>db", require("dap").toggle_breakpoint)
+			Keymap.normal("<leader>dB",
+				function() require("dap").set_breakpoint(vim.fn.input("Breakpoint condition: ")) end)
 		end
 	},
 	{
@@ -45,13 +184,9 @@ return {
 				"jay-babu/mason-nvim-dap.nvim",
 				---@type MasonNvimDapSettings
 				opts = {
-					-- This line is essential to making automatic installation work
-					-- :exploding-brain
 					handlers = {},
-					automatic_installation = false,
-					-- DAP servers: these will be installed by mason-tool-installer.nvim
-					-- for consistency.
-					ensure_installed = {},
+					automatic_installation = true,
+					ensure_installed = { "js-debug-adapter" },
 				},
 				dependencies = {
 					"mfussenegger/nvim-dap",
