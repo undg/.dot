24d2c42331cc60c83d3a5151673c7efe79e79464
undg <und3rdg@gmail.com>
2026-01-16 12:06:16 +0000

    [vim] change settings for octo-nvim

--- vim/.config/nvim/lua/plugins/octo-nvim.lua
+++ vim/.config/nvim/lua/plugins/octo-nvim.lua
@@ -1,12 +1,18 @@
 return {
-	'pwntester/octo.nvim', -- https://github.com/pwntester/octo.nvim
+	'pwntester/octo.nvim',         -- https://github.com/pwntester/octo.nvim
 	dependencies = {
-		'nvim-lua/plenary.nvim', -- https://github.com/nvim-lua/plenary.nvim,
+		'nvim-lua/plenary.nvim',   -- https://github.com/nvim-lua/plenary.nvim,
 		'nvim-telescope/telescope.nvim', -- https://github.com/nvim-telescope/telescope.nvim,
 		-- OR 'ibhagwan/fzf-lua', https://github.com/ ibhagwan/fzf-lua,
 		'nvim-tree/nvim-web-devicons', -- https://github.com/nvim-tree/nvim-web-devicons,
 	},
 	config = function()
-		require('octo').setup()
+		require('octo').setup({
+			default_merge_method = "squash", -- default merge method which should be used for both `Octo pr merge` and merging from picker, could be `merge`, `rebase` or `squash`
+			notifications = {
+				current_repo_only = true, -- show notifications for current repo only
+			},
+
+		})
 	end,
 }
