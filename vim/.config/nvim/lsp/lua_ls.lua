786f3547ba9d46389d65caaef1baf9ee1621495c
undg <und3rdg@gmail.com>
2026-02-17 01:06:09 +0000

    [vim] speedup init of lsp server

--- vim/.config/nvim/lsp/lua_ls.lua
+++ vim/.config/nvim/lsp/lua_ls.lua
@@ -3,17 +3,24 @@ return {
 		Lua = {
 			runtime = {
 				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-				version = 'LuaJIT',
+				version = "LuaJIT",
 			},
 			diagnostics = {
 				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
+				globals = { "vim" },
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file('', true),
+				-- library = vim.api.nvim_get_runtime_file("", true),
+				-- Make the server faster
+				library = {
+					vim.env.VIMRUNTIME,
+					"${3rd}/luv/library",
+					"${3rd}/busted/library",
+				},
 				checkThirdParty = false, -- THIS IS THE IMPORTANT LINE TO ADD
 			},
+			ignoreDir = { ".git", "node_modules", ".deps" },
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
 				enable = false,
