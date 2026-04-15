e2cc07a3f524da49b96c9a2726f8e977c530c7c3
undg <und3rdg@gmail.com>
2026-04-15 14:37:59 +0100

    [vim] autoinstall treesiter parsers

--- vim/.config/nvim/lua/plugins/tree-sitter-manager.lua
+++ vim/.config/nvim/lua/plugins/tree-sitter-manager.lua
@@ -6,7 +6,7 @@ return {
 			-- Default Options
 			-- ensure_installed = {}, -- list of parsers to install at the start of a neovim session
 			border = "rounded", -- border style for the window (e.g. "rounded", "single", nil), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-			-- auto_install = false, -- if enabled, install missing parsers when editing a new file
+			auto_install = true, -- if enabled, install missing parsers when editing a new file
 			highlight = true, -- treesitter highlighting is enabled by default
 			-- languages = {}, -- override or add new parser sources
 			-- parser_dir = vim.fn.stdpath("data") .. "/site/parser",
