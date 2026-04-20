e523319d22264cccdc123acc4f40ee2a96f2566f
undg <und3rdg@gmail.com>
2026-04-20 12:29:54 +0100

    [shell] add bun to path

--- shell/.config/zsh/path.zsh
+++ shell/.config/zsh/path.zsh
@@ -9,11 +9,12 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 export PATH="$HOME/.deno/bin:$PATH"
+export PATH="$HOME/.bun/bin:$PATH"
 export PATH="$PATH:$(go env GOBIN):$(go env GOPATH)/bin"
 
 # Adds `~/.config/i3/scripts` and all subdirectories to $PATH
 if [[ -d "$HOME/.config/i3/scripts/" ]]; then
-    export PATH="$(du "$HOME/.config/i3/scripts/" | cut -f2 | tr '\n' ':')$PATH"
+	export PATH="$(du "$HOME/.config/i3/scripts/" | cut -f2 | tr '\n' ':')$PATH"
 fi
 
 # debloate WSL from m$ windows paths (fix lagging in terminal)
