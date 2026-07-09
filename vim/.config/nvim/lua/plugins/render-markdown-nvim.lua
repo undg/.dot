d8b12b04b0a7b47d1c8dd74e89dd598a2e830c53
undg <und3rdg@gmail.com>
2026-07-09 09:03:20 +0100

    [vim] changed markdown headers and list's color scheme

--- vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
+++ vim/.config/nvim/lua/plugins/render-markdown-nvim.lua
@@ -1,3 +1,25 @@
+vim.api.nvim_set_hl(0, "RenderMarkdownH1Bg", { bg = "#1e3a5f" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH1", { fg = "#7dd3fc", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownH2Bg", { bg = "#254a70" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH2", { fg = "#a8d8ea", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownH3Bg", { bg = "#2c5a80" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH3", { fg = "#b8e6f0", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownH4Bg", { bg = "#336a90" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH4", { fg = "#c8eef5", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownH5Bg", { bg = "#3a7aa0" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH5", { fg = "#d8f0f8", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownH6Bg", { bg = "#3a7aa0" })
+vim.api.nvim_set_hl(0, "RenderMarkdownH6", { fg = "#d8f0f8", bold = true })
+
+vim.api.nvim_set_hl(0, "RenderMarkdownUnchecked", { fg = "#7dd3fc", bold = true })
+vim.api.nvim_set_hl(0, "RenderMarkdownChecked", { fg = "#8ec07c" })
+vim.api.nvim_set_hl(0, "RenderMarkdownTodo", { fg = "#7dd3fc" })
+
 return {
 	"MeanderingProgrammer/render-markdown.nvim",                                      -- https://github.com/MeanderingProgrammer/render-markdown.nvim
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
@@ -5,30 +27,30 @@ return {
 	---@type render.md.UserConfig
 	opts = {
 		heading = {
-			backgrounds = {
-				"RenderMarkdownH1Bg",
-				"RenderMarkdownH5Bg",
-				"RenderMarkdownH3Bg",
-				"RenderMarkdownH4Bg",
-				"RenderMarkdownH2Bg",
-				"RenderMarkdownH6Bg",
-			},
-			foregrounds = {
-				"RenderMarkdownH1",
-				"RenderMarkdownH5",
-				"RenderMarkdownH3",
-				"RenderMarkdownH4",
-				"RenderMarkdownH2",
-				"RenderMarkdownH6",
-			},
-			icons = { "󰲡 ", "󰲣 ", "󰲥 ", "󰲧 ", "󰲩 ", "󰲫 " },
-			border = true,
-			border_virtual = true,
-			right_pad = 3,
-			left_pad = 3,
-			width = "block",
+			-- backgrounds = {
+			-- 	"RenderMarkdownH1Bg",
+			-- 	"RenderMarkdownH2Bg",
+			-- 	"RenderMarkdownH3Bg",
+			-- 	"RenderMarkdownH4Bg",
+			-- 	"RenderMarkdownH5Bg",
+			-- 	"RenderMarkdownH6Bg",
+			-- },
+			-- foregrounds = {
+			-- 	"RenderMarkdownH1",
+			-- 	"RenderMarkdownH2",
+			-- 	"RenderMarkdownH3",
+			-- 	"RenderMarkdownH4",
+			-- 	"RenderMarkdownH5",
+			-- 	"RenderMarkdownH6",
+			-- },
+			icons = { "󰎥 ", "󰎨 ", "󰎫 ", "󰎲 ", "󰎯 ", "󰎵 " },
+			-- border = true,
+			-- border_virtual = true,
+			-- right_pad = 1,
+			left_pad = 1,
+			-- width = "block",
 		},
-		file_types = { "markdown", "Avante" },
+		file_types = { "markdown" },
 		-- new conf START
 		render_modes = { --
 			"n",
