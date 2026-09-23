abb6e284012dee711b94c697b496c3d815195b3a
undg <und3rdg@gmail.com>
2026-09-23 12:31:18 +0100

    [vim] clear comments after they are send

--- vim/.config/nvim/lua/plugins/herdr-nvim.lua
+++ vim/.config/nvim/lua/plugins/herdr-nvim.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		require("herdr-nvim").setup({
 			prefix = "<leader>a", -- keymap prefix
-			keymaps = true,  -- set false to define your own
-			clear_after_send = false, -- comments are ephemeral by design
+			keymaps = true, -- set false to define your own
+			clear_after_send = true, -- comments are ephemeral by design
 		})
 	end,
 }
